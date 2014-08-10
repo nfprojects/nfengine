caed920b13200ed60af04d0c1a414acd02c60e8c
Witek902 <witek902@gmail.com>
2014-08-10 16:15:14 +0200

    Improve intersection functions
    * The functions has been replaced with templates
    * Tests have been written
    * Missing functionality was added
    
    Change-Id: Ib81a2cd1276fb128ab4db25f150b20b57e0b0397

--- nfEngine/nfCommon/Math/Geometry.hpp
+++ nfEngine/nfCommon/Math/Geometry.hpp
@@ -9,15 +9,17 @@
 namespace NFE {
 namespace Math {
 
-//Axis Aligned Box
+/**
+ * Axis Aligned Box
+ */
 class NFE_ALIGN(16) Box
 {
 public:
     Vector min;
     Vector max;
 
-    NFE_INLINE Box() : min(), max() {};
-    NFE_INLINE Box(const Vector& min_, const Vector& max_) : min(min_), max(max_) {};
+    NFE_INLINE Box() : min(), max() {}
+    NFE_INLINE Box(const Vector& min_, const Vector& max_) : min(min_), max(max_) {}
 
     // merge boxes
     NFE_INLINE Box(const Box& a, const Box& b)
@@ -34,23 +36,58 @@ public:
     NFE_INLINE float Volume() const;
 };
 
-//Frustum
+/**
+ * Frustum
+ */
+NFE_ALIGN(16)
 class NFE_ALIGN(16) Frustum
 {
 public:
+    /// planes IDs
+    enum Plane
+    {
+        Front = 0,
+        Back,
+        Left,
+        Right,
+        Bottom,
+        Top
+    };
+
     Vector planes[6];
     Vector verticies[8];
     Box boundingBox;
 
+    /**
+     * Construct frustum of perspective view.
+     * @param origin   Camera position
+     * @param xAxis    Horizontal extent at distance equal to 1.
+     * @param yAxis    Vertical extent at distance equal to 1.
+     * @param zAxis    Normalized frustum direction.
+     * @param nearDist Near plane distance.
+     * @param farDist  Far plane distance.
+     */
+    NFE_INLINE void Construct(const Vector& origin, const Vector& xAxis, const Vector& yAxis,
+                              const Vector& zAxis, float nearDist, float farDist);
     NFE_INLINE void CalculatePlanes();
-    NFE_INLINE float SupportVertex(const Vector& dir);
+    NFE_INLINE float SupportVertex(const Vector& dir) const;
 };
 
+/**
+ * Sphere
+ */
 class NFE_ALIGN(16) Sphere
 {
 public:
     Vector origin;
     float r;
+
+    NFE_INLINE Sphere() : origin(), r() {}
+    NFE_INLINE Sphere(const Vector& origin, float r)
+    {
+        this->origin = origin;
+        this->r = r;
+    }
 
     NFE_INLINE float SupportVertex(const Vector& dir);
 };
@@ -61,6 +98,10 @@ public:
     Vector v0;
     Vector v1;
     Vector v2;
+
+    NFE_INLINE Triangle() : v0(), v1(), v2() {}
+    NFE_INLINE Triangle(const Vector& v0_, const Vector& v1_, const Vector& v2_)
+        : v0(v0_), v1(v1_), v2(v2_) {}
 };
 
 class NFE_ALIGN(16) Ray
@@ -79,24 +120,41 @@ public:
 };
 
 
+enum class IntersectionResult
+{
+    Outside   = 0, // no intersection
+    Inside    = 1, // shapeA is inside shapeB
+    Intersect = 2, // shapeA and shapeB intersects
+};
 
+/**
+ * Finds the nearest point on a line segment to a @a point.
+ * @param[in]  p1,p2 Line segment ends.
+ * @param[out] out Nearest point on the segment.
+ * @return     Distance to the segment.
+ */
+NFCOMMON_API float ClosestPointOnSegment(const Vector& p, const Vector& p1, const Vector& p2,
+                                         Vector& out);
 
-//----------------------------------------------------------------------
-//intersection tests
-//----------------------------------------------------------------------
-NFCOMMON_API int IntersectBoxBox(const Box& box1, const Box& box2);
-NFCOMMON_API int IntersectBoxFrustum(const Box& box, const Frustum& frustum);
-NFCOMMON_API int IntersectFrustumFrustum(const Frustum& f1, const Frustum& f2);
+/**
+ * Template function for intersection tests.
+ */
+template<typename ShapeTypeA, typename ShapeTypeB>
+bool Intersect(const ShapeTypeA& shapeA, const ShapeTypeB& shapeB);
 
-NFCOMMON_API int InsidePointBox(Box& innerBox, Box& outerBox);
-NFCOMMON_API int IntersectBoxBox(Box& box1, Box& box2);
-NFCOMMON_API int IntersectBoxPoint(const Box& box, const Vector& point);
-NFCOMMON_API int IntersectBoxesPoint(const Box* pBoxes, Vector& point);
-NFCOMMON_API int IntersectTriangleFrustum(const Triangle& tri, const Frustum& frustum);
+/**
+ * Template function for extended intersection tests.
+ * @see IntersectionResult
+ */
+template<typename ShapeTypeA, typename ShapeTypeB>
+IntersectionResult IntersectEx(const ShapeTypeA& shapeA, const ShapeTypeB& shapeB);
 
-NFCOMMON_API int IntersectRayBox(const Box& box, const Ray& ray);
-NFCOMMON_API int IntersectRayBox(const Box& box, const Ray& ray, RayBoxSegment* pSegment);
-NFCOMMON_API int IntersectRayTriangle(const Ray& ray, const Triangle& tri, Vector* pDist);
+/**
+ * Template function for ray intersection tests.
+ * @param dist Distance to the intersection.
+ */
+template<typename ShapeType>
+bool Intersect(const Ray& ray, const ShapeType& shape, Vector& dist);
 
 
 //
@@ -169,9 +227,9 @@ NFE_INLINE Box TransformBox(const Matrix& matrix, const Box& localBox)
 {
     Vector boxCenter = localBox.GetCenter();
 
-    float x = VectorDot3f(localBox.SupportVertex(matrix.r[0]) - boxCenter, matrix.r[0]);
-    float y = VectorDot3f(localBox.SupportVertex(matrix.r[1]) - boxCenter, matrix.r[1]);
-    float z = VectorDot3f(localBox.SupportVertex(matrix.r[2]) - boxCenter, matrix.r[2]);
+    float x = VectorDot3(localBox.SupportVertex(matrix.r[0]) - boxCenter, matrix.r[0])[0];
+    float y = VectorDot3(localBox.SupportVertex(matrix.r[1]) - boxCenter, matrix.r[1])[0];
+    float z = VectorDot3(localBox.SupportVertex(matrix.r[2]) - boxCenter, matrix.r[2])[0];
     Vector boxDim = Vector(x, y, z);
 
     Vector transformedBoxCenter = VectorTransform3(boxCenter, matrix);
@@ -202,17 +260,17 @@ float Sphere::SupportVertex(const Vector& dir)
 
 void Frustum::CalculatePlanes()
 {
-    planes[0] = PlaneFromPoints(verticies[0], verticies[1], verticies[3]); //front
-    planes[1] = PlaneFromPoints(verticies[7], verticies[5], verticies[4]); //back
-    planes[2] = PlaneFromPoints(verticies[4], verticies[0], verticies[6]); //left
-    planes[3] = PlaneFromPoints(verticies[7], verticies[3], verticies[5]); //right
-    planes[4] = PlaneFromPoints(verticies[5], verticies[1], verticies[4]); //bottom
-    planes[5] = PlaneFromPoints(verticies[6], verticies[3], verticies[7]); //top
+    planes[Front]  = PlaneFromPoints(verticies[0], verticies[1], verticies[3]);
+    planes[Back]   = PlaneFromPoints(verticies[7], verticies[5], verticies[4]);
+    planes[Left]   = PlaneFromPoints(verticies[4], verticies[0], verticies[6]);
+    planes[Right]  = PlaneFromPoints(verticies[7], verticies[3], verticies[5]);
+    planes[Bottom] = PlaneFromPoints(verticies[5], verticies[1], verticies[4]);
+    planes[Top]    = PlaneFromPoints(verticies[6], verticies[3], verticies[7]);
 
     boundingBox.MakeFromPoints(verticies, 8);
 }
 
-float Frustum::SupportVertex(const Vector& dir)
+float Frustum::SupportVertex(const Vector& dir) const
 {
     Vector d = VectorDot3(dir, verticies[0]);
 
@@ -226,6 +284,21 @@ float Frustum::SupportVertex(const Vector& dir)
     }
 
     return d.f[0];
+}
+
+void Frustum::Construct(const Vector& origin, const Vector& xAxis, const Vector& yAxis,
+                        const Vector& zAxis, float nearDist, float farDist)
+{
+    verticies[0] = origin + nearDist * (zAxis - xAxis - yAxis);
+    verticies[1] = origin + nearDist * (zAxis + xAxis - yAxis);
+    verticies[2] = origin + nearDist * (zAxis - xAxis + yAxis);
+    verticies[3] = origin + nearDist * (zAxis + xAxis + yAxis);
+    verticies[4] = origin + farDist * (zAxis - xAxis - yAxis);
+    verticies[5] = origin + farDist * (zAxis + xAxis - yAxis);
+    verticies[6] = origin + farDist * (zAxis - xAxis + yAxis);
+    verticies[7] = origin + farDist * (zAxis + xAxis + yAxis);
+
+    CalculatePlanes();
 }
 
 } // namespace Math
