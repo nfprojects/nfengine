d232fbb95a978829f5c940572873f7102258505d
Witek902 <witek902@gmail.com>
2014-11-04 19:59:56 +0100

    Add script running static code analysis
    * cppcheck is required
    
    Verification:
    Run Scripts/cppcheck.bat. Report will be stored to
    Scripts/cppcheck_result.txt
    
    Change-Id: If6f301aa0362e853ce002b12377e3a6238addf07

---
+++ Scripts/cppcheck.bat
@@ -0,0 +1,19 @@
+@echo off
+
+:: make sure that cppcheck is reachable
+echo | set /p=Checking for cppcheck... 
+for %%X in (cppcheck.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND (
+    echo FOUND
+) else (
+    echo NOT FOUND
+    echo     Make sure you download cppcheck before using this script.
+    echo     See README.md for more info. Exiting.
+    exit /b
+)
+
+pushd .
+cd %~dp0\..
+echo Current dir is %cd%
+cppcheck ./nfEngine/ -j 8 --enable=warning 2> Scripts/cppcheck_result.txt
+popd
