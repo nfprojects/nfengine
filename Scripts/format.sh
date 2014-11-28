944cd4a3b881bb5ddf540c2bd8d8c4895fdc8cbb
LKostyra <costyrra.xl@gmail.com>
2014-11-28 13:19:49 +0100

    Adjust Engine for new Deps repository
    Deps were moved to new repository:
        http://github.com/nfprojects/nfenginedeps
    
    This commit removes old nfEngine Deps projects from Engine.sln, since Deps
    now have its own separate repository and Solution file. Additionally, all
    Engine projects are adjusted for new nfEngineDeps structure.
    
    Verification:
        * Use git submodules to download nfEngineDeps
        * Batch build nfEngineDeps
        * Batch build Engine
        * Run tests, check if nfEngineTest works
    
    Change-Id: Ib2ae369fd538f3e9db3a1f9d8713f540c4a1e69b

--- Scripts/format.sh
+++ Scripts/format.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-astyle --options=style.cfg -r -v --suffix=none ../nfEngine/*.cpp ../nfEngine/*.h ../nfEngineTest/*.cpp ../nfEngineTest/*.h
+# Check for astyle
+echo -n "Checking for astyle... "
+type astyle >/dev/null 2>&1 || {
+    echo "NOT FOUND";
+    echo "    Make sure you download astyle before using this script."
+    echo "    See README.md for more info. Exiting."
+    exit 1
+}
+echo FOUND
+echo Current launch directory is pwd
+
+
+#astyle --options=style.cfg -r -v --suffix=none ../nfEngine/*.cpp ../nfEngine/*.h ../nfEngineTest/*.cpp ../nfEngineTest/*.h
