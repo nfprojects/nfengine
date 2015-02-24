a28a4a3928b3bd501ab4680cb89baf50bc8f20f1
Lukasz Kostyra <costyrra.xl@gmail.com>
2015-02-24 18:34:52 +0100

    [Lnx] Implement Timer
    Continuing nfCommon adjustments for Linux, this is a patch implementing
    Timer class for Linux systems. Additionally, Timer's sources were
    slightly altered for consistency with other multi-platform modules.
    
    Verification:
        Build, run tests. All should pass. ThreadPoolPerformance test should
        be executed as well.
    
    Change-Id: I886db4e2d08cf35cf97bb39bfe253c25a5eddd28

--- nfEngine/nfCommon/Linux/Timer.cpp
+++ nfEngine/nfCommon/Linux/Timer.cpp
@@ -1,17 +1,35 @@
 /**
  *  @file   Linux/Timer.cpp
- *  @brief  High resolution timer class definition for Linux system
+ *  @brief  High resolution timer class definition for Linux systems
  */
 
 #include "../stdafx.hpp"
-#include "Timer.hpp"
+#include "../Timer.hpp"
 
 
 namespace NFE {
 namespace Common {
 
-// TODO implement under Linux when nfCommons will be ported to Linux
+Timer::Timer()
+{
+    mStart.tv_sec = 0;
+    mStart.tv_nsec = 0;
+}
 
+void Timer::Start()
+{
+    clock_gettime(CLOCK_MONOTONIC, &mStart);
+}
+
+double Timer::Stop()
+{
+    struct timespec mStop;
+    clock_gettime(CLOCK_MONOTONIC, &mStop);
+
+    double result = (double)(mStop.tv_sec - mStart.tv_sec);
+    result += (double)(mStop.tv_nsec - mStart.tv_nsec) / 1e+9;
+    return result;
+}
 
 } // namespace Common
 } // namespace NFE
