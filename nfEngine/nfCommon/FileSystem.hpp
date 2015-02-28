a808c9ad8f1067b1230fec70fdace9ccb2284a8f
Witek902 <witek902@gmail.com>
2015-02-28 16:52:03 +0100

    Introduce filesystem utilities (only Windows implementation)
    Verification:
    Build, run tests.
    
    Change-Id: Id4b9da9a86e3abdc109374a80192eac6fa3dafec

---
+++ nfEngine/nfCommon/FileSystem.hpp
@@ -0,0 +1,75 @@
+/**
+ * @file   FileSystem.hpp
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Filesystem utilities declarations.
+ */
+
+#pragma once
+#include "nfCommon.hpp"
+
+namespace NFE {
+namespace Common {
+
+enum class PathType
+{
+    Invalid,   // non-existing path
+    File,
+    Directory,
+};
+
+class NFCOMMON_API FileSystem
+{
+public:
+    typedef std::function<bool(const std::string& path, bool isDirectory)> DirListCallback;
+
+    /**
+     * Set current working directory.
+     */
+    static bool ChangeDirectory(const std::string& path);
+
+    /**
+     * Create an empty file if not exists.
+     */
+    static bool TouchFile(const std::string& path);
+
+    /**
+     * Check if a path is a file, directory or does not exist.
+     */
+    static PathType GetPathType(const std::string& path);
+
+    /**
+     * Create a directory.
+     */
+    static bool CreateDir(const std::string& path);
+
+    /**
+     * Remove a file or a directory.
+     * @param recursive Removes all elements in a directory recursively
+     */
+    static bool Remove(const std::string& path, bool recursive = false);
+
+    /**
+     * Copy a file or a directory.
+     * @param srcPath   Source path
+     * @param destPath  Destination path
+     * @param overwrite Overwrite destination path
+     */
+    static bool Copy(const std::string& srcPath, const std::string& destPath,
+                     bool overwrite = false);
+
+    /**
+     * Move a file or a directory to another location.
+     * @param srcPath   Source path
+     * @param destPath  Destination path
+     */
+    static bool Move(const std::string& srcPath, const std::string& destPath);
+
+    /**
+     * List files and directories in a specified path.
+     * @param callback Callback function called for each found object
+     */
+    static bool List(const std::string& path, DirListCallback callback);
+};
+
+} // namespace Common
+} // namespace NFE
