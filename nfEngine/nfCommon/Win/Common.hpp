f76d7f351ddf97799320d74056293afea0c1daa8
Witek902 <witek902@gmail.com>
2015-02-28 16:52:03 +0100

    [Win] nfCommon: Introduce filesystem utilities
    Verification:
    Build, run tests.
    
    Change-Id: Id4b9da9a86e3abdc109374a80192eac6fa3dafec

---
+++ nfEngine/nfCommon/Win/Common.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file   Common.hpp
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Common Windows helpers declarations.
+ */
+
+#include "../stdafx.hpp"
+
+namespace NFE {
+namespace Common {
+
+/**
+ * Convert a string from UTF-8 to Windows' UTF-16.
+ */
+bool UTF8ToUTF16(const std::string& in, std::wstring& out);
+
+/**
+* Convert a string from Windows' UTF-16 to UTF-8.
+*/
+bool UTF16ToUTF8(const std::wstring& in, std::string& out);
+
+/**
+ * Translates GetLastError() code to a string.
+ */
+std::string GetLastErrorString();
+
+} // namespace Common
+} // namespace NFE
