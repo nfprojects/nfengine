93c4531447305567efecda8d4ad390f5fa3e2867
Michal Witanowski <witek902@gmail.com>
2015-03-08 19:22:55 +0100

    nfRendererD3D11: Basic 2D textures support
    Verification:
        Rebuild all, run nfRendererTest.
    
    Change-Id: I04948f5fc930200753d158003887db15ce91a4b0

--- nfEngine/Renderers/Shaders/D3D11/TestVS.hlsl
+++ nfEngine/Renderers/Shaders/D3D11/TestVS.hlsl
@@ -1,12 +1,14 @@
 struct VS_IN
 {
     float3 Pos : POSITION;
-    float4 Color : TEXCOORD;
+    float2 TexCoord : TEXCOORD0;
+    float4 Color : TEXCOORD1;
 };
 
 struct VS_OUTPUT
 {
-    float4 Color : TEXCOORD;
+    float2 TexCoord : TEXCOORD0;
+    float4 Color : TEXCOORD1;
     float4 Pos : SV_POSITION;
 };
 
@@ -14,6 +16,7 @@ VS_OUTPUT main(VS_IN In)
 {
     VS_OUTPUT Out = (VS_OUTPUT)0;
     Out.Pos = float4(In.Pos, 1.0f);
+    Out.TexCoord = In.TexCoord;
     Out.Color = In.Color;
     return Out;
 }
