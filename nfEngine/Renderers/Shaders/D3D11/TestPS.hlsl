bc29c38922e80a59d90d9e8bbeda2218f180e766
Michal Witanowski <witek902@gmail.com>
2015-03-08 19:22:55 +0100

    nfRendererD3D11: Basic 2D textures support
    Verification:
        Rebuild all, run nfRendererTest.
    
    Change-Id: I04948f5fc930200753d158003887db15ce91a4b0

--- nfEngine/Renderers/Shaders/D3D11/TestPS.hlsl
+++ nfEngine/Renderers/Shaders/D3D11/TestPS.hlsl
@@ -1,10 +1,14 @@
+Texture2D<float4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
 struct VS_OUTPUT
 {
-    float4 Color : TEXCOORD;
+    float2 TexCoord : TEXCOORD0;
+    float4 Color : TEXCOORD1;
     float4 Pos : SV_POSITION;
 };
 
 float4 main(VS_OUTPUT In) : SV_TARGET0
 {
-    return In.Color;
+    return In.Color * gTexture.Sample(gSampler, In.TexCoord);
 }
--- nfEngine/Renderers/nfRendererD3D11/Device.cpp
+++ nfEngine/Renderers/nfRendererD3D11/Device.cpp
@@ -78,7 +78,17 @@ IBuffer* Device::CreateBuffer(const BufferDesc& desc)
 
 ITexture* Device::CreateTexture(const TextureDesc& desc)
 {
-    return nullptr;
+    Texture* texture = new (std::nothrow) Texture;
+    if (texture == nullptr)
+        return nullptr;
+
+    if (!texture->Init(desc))
+    {
+        delete texture;
+        return nullptr;
+    }
+
+    return texture;
 }
 
 IRenderTarget* Device::CreateRenderTarget(const RenderTargetDesc& desc)
@@ -131,6 +141,21 @@ IRasterizerState* Device::CreateRasterizerState(const RasterizerStateDesc& desc)
     }
 
     return rs;
+}
+
+ISampler* Device::CreateSampler(const SamplerDesc& desc)
+{
+    Sampler* sampler = new (std::nothrow) Sampler;
+    if (sampler == nullptr)
+        return nullptr;
+
+    if (!sampler->Init(desc))
+    {
+        delete sampler;
+        return nullptr;
+    }
+
+    return sampler;
 }
 
 IShader* Device::CreateShader(const ShaderDesc& desc)
