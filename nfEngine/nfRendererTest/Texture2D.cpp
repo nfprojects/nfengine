54de288df429618bedba631373e62d2c3d88cf2e
Michal Witanowski <witek902@gmail.com>
2015-04-03 00:50:44 +0200

    Introduce unit test project for renderers
    Verification:
        Rebuild all, run nfRendererTest.
    
    Change-Id: I812cc4b569d5b4311423f53018b91aca639a2d35

---
+++ nfEngine/nfRendererTest/Texture2D.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.hpp"
+#include "RendererTest.hpp"
+
+TEST_F(nfRendererTest, Texture2DCreation)
+{
+    std::unique_ptr<ITexture> texture;
+
+    uint32_t bitmap[] = { 0xFFFFFFFF, 0, 0, 0xFFFFFFFF };
+
+    TextureDataDesc textureDataDesc;
+    textureDataDesc.data = bitmap;
+    textureDataDesc.lineSize = 2 * sizeof(uint32_t);
+    textureDataDesc.sliceSize = 4 * sizeof(uint32_t);
+
+    // default (good) texture descriptor
+    TextureDesc defTextureDesc;
+    defTextureDesc.binding = NFE_RENDERER_TEXTURE_BIND_SHADER;
+    defTextureDesc.format = ElementFormat::Uint_8_norm;
+    defTextureDesc.texelSize = 4;
+    defTextureDesc.width = 2;
+    defTextureDesc.height = 2;
+    defTextureDesc.mipmaps = 1;
+    defTextureDesc.dataDesc = &textureDataDesc;
+    defTextureDesc.layers = 1;
+
+    TextureDesc textureDesc;
+
+    // no texture data suplied
+    textureDesc = defTextureDesc;
+    textureDesc.dataDesc = nullptr;
+    texture.reset(gRendererDevice->CreateTexture(textureDesc));
+    EXPECT_TRUE(texture.get() == nullptr);
+
+    // invalid width
+    textureDesc = defTextureDesc;
+    textureDesc.width = 0;
+    texture.reset(gRendererDevice->CreateTexture(textureDesc));
+    EXPECT_TRUE(texture.get() == nullptr);
+
+    // invalid height
+    textureDesc = defTextureDesc;
+    textureDesc.height = 0;
+    texture.reset(gRendererDevice->CreateTexture(textureDesc));
+    EXPECT_TRUE(texture.get() == nullptr);
+
+    // TODO: write more cases
+
+    textureDesc = defTextureDesc;
+    texture.reset(gRendererDevice->CreateTexture(textureDesc));
+    EXPECT_TRUE(texture.get() != nullptr);
+}
---
+++ nfEngine/nfRendererTest/stdafx.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+// TODO: remove
+#define NOMINMAX
+#include <Windows.h>
+
+#include <memory>
+
+#include "gtest\gtest.h"
