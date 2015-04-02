40e5ca01a8f10897c61dbbc14f6620a9a4d5488e
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-02 13:03:40 +0200

    Implement Linux version of nfCommon Window module
    Verification:
    Compile and run test. Test should pass.
    
    Change-Id: I1c6f93e605f2bcef263030286e51da6a37edecda

---
+++ nfEngine/nfCommonTest/WindowTest.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.hpp"
+#include "../nfCommon/Window.hpp"
+
+
+class WindowTest : public testing::Test
+{
+protected:
+    NFE::Common::Window mWindow;
+};
+
+
+TEST_F(WindowTest, CreateTest)
+{
+    void* result;
+    result = mWindow.GetHandle();
+    EXPECT_EQ(result, nullptr);
+    bool openResult = mWindow.Open();
+    ASSERT_TRUE(openResult);
+    openResult = mWindow.Open();
+    ASSERT_FALSE(openResult);
+    result = mWindow.GetHandle();
+    ASSERT_NE(result, nullptr);
+}
