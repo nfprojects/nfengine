5f9a4096d7585a68da9704fcdf64f64369458056
Michal Witanowski <witek902@gmail.com>
2015-04-03 17:13:01 +0200

    nfRendererTest: Simple framework for rendering verification
    * BeginTestFrame/EndTestFrame added in nfRendererTest class
    * Added missing code in D3D11 renderer and interface
    
    Verification:
        Rebuild all, run nfRendererTest and nfRendererDemo.
    
    Change-Id: Iff70ec79b6b3b25a2ae99ebd3a09c8d2a43c723a

--- nfEngine/nfRendererTest/RendererTest.cpp
+++ nfEngine/nfRendererTest/RendererTest.cpp
@@ -6,6 +6,7 @@ HMODULE nfRendererTest::gRendererModule = NULL;
 IDevice* nfRendererTest::gRendererDevice = nullptr;
 ICommandBuffer* nfRendererTest::gCommandBuffer = nullptr;
 
+
 void nfRendererTest::SetUpTestCase()
 {
     /*
@@ -36,4 +37,50 @@ void nfRendererTest::TearDownTestCase()
         ASSERT_TRUE(proc != nullptr);
         proc();
     }
+}
+
+bool nfRendererTest::BeginTestFrame(int width, int height, ElementFormat format, int texelSize)
+{
+    TextureDesc texDesc;
+    texDesc.binding = NFE_RENDERER_TEXTURE_BIND_RENDERTARGET;
+    texDesc.access = BufferAccess::GPU_ReadWrite;
+    texDesc.format = format;
+    texDesc.texelSize = texelSize;
+    texDesc.width = width;
+    texDesc.height = height;
+    mTestTexture.reset(gRendererDevice->CreateTexture(texDesc));
+    if (!mTestTexture.get())
+        return false;
+
+    RenderTargetElement rtTarget;
+    rtTarget.texture = mTestTexture.get();
+    RenderTargetDesc rtDesc;
+    rtDesc.numTargets = 1;
+    rtDesc.targets = &rtTarget;
+    mTestRenderTarget.reset(gRendererDevice->CreateRenderTarget(rtDesc));
+    if (!mTestRenderTarget.get())
+        return false;
+
+    texDesc.binding = 0;
+    texDesc.access = BufferAccess::CPU_Read;
+    mTestTextureRead.reset(gRendererDevice->CreateTexture(texDesc));
+    if (!mTestTextureRead.get())
+        return false;
+
+    gCommandBuffer->SetRenderTarget(mTestRenderTarget.get());
+
+    return true;
+}
+
+bool nfRendererTest::EndTestFrame(void* data)
+{
+    gCommandBuffer->SetRenderTarget(nullptr);
+    gCommandBuffer->CopyTexture(mTestTexture.get(), mTestTextureRead.get());
+    bool success = gCommandBuffer->ReadTexture(mTestTextureRead.get(), data);
+
+    mTestRenderTarget.reset();
+    mTestTexture.reset();
+    mTestTextureRead.reset();
+
+    return success;
 }
