2eba15ad08602c663f832fab613c70b25374c5d0
Michal Witanowski <witek902@gmail.com>
2015-04-03 17:13:01 +0200

    nfRendererTest: Simple framework for rendering verification
    * BeginTestFrame/EndTestFrame added in RendererTest class
    * Added missing code in D3D11 renderer and interface
    
    Verification:
        Rebuild all, run nfRendererTest and nfRendererDemo.
    
    Change-Id: Iff70ec79b6b3b25a2ae99ebd3a09c8d2a43c723a

--- nfEngine/nfRendererTest/RendererTest.hpp
+++ nfEngine/nfRendererTest/RendererTest.hpp
@@ -15,6 +15,29 @@ protected:
     // preapre test case enviroment - release the renderer
     static void TearDownTestCase();
 
+    // texture used as render target
+    std::unique_ptr<ITexture> mTestTexture;
+    std::unique_ptr<IRenderTarget> mTestRenderTarget;
+
+    // texture used for GPU -> CPU data transfer
+    std::unique_ptr<ITexture> mTestTextureRead;
+
+    /**
+     * Start rendering into test render target texture.
+     * width,height      Texture dimensions
+     * format, texelSize Texture format
+     * @see EndFrame
+     */
+    bool BeginTestFrame(int width, int height, ElementFormat format, int texelSize);
+
+    /**
+     * Finish rendering into test render target texture and return rendered pixels buffer.
+     * data[out] Buffer, where renderer pixels will be written into. Note that the buffer
+                 size must be big enough to store all the pixels.
+     * @see BeginFrame
+     */
+    bool EndTestFrame(void* data);
+
     static Common::Library gRendererLib;
     static IDevice* gRendererDevice;
     static ICommandBuffer* gCommandBuffer;
