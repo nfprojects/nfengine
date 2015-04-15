1d7442aa6f2d2b2116e0fa8da1ae5c46137f037f
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-15 08:28:00 +0200

    nfCommonTest: Implement Input/Output Stream tests
    Verification:
    Compile and run nfCommonTest.
    
    Change-Id: Ieedfaf43cd9ce70d1fed784e5f78332d8567bec2

---
+++ nfEngine/nfCommonTest/BufferInputStreamTest.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.hpp"
+#include "../nfCommon/InputStream.hpp"
+
+#include "test.hpp"
+
+using namespace NFE::Common;
+
+
+class BufferInputStreamTest : public testing::Test
+{
+protected:
+    std::unique_ptr<char[]> mBuffer;
+
+    void SetUp()
+    {
+        mBuffer.reset(new char[TEXTSIZE + 1]);
+        mBuffer.get()[TEXTSIZE] = '\0';
+    }
+};
+
+TEST_F(BufferInputStreamTest, NullBufferTest)
+{
+    // Create stream pointing to nullptr
+    BufferInputStream stream(nullptr, TEXTSIZE);
+
+    // Size should be as in constructor, methods should all fail
+    ASSERT_EQ(stream.GetSize(), TEXTSIZE);
+    ASSERT_EQ(stream.Seek(1), false);
+    ASSERT_EQ(stream.Read(1, nullptr), false);
+}
+
+TEST_F(BufferInputStreamTest, StringBufferTest)
+{
+    // Create stream pointing to our TEXT
+    BufferInputStream stream(TEXT.data(), TEXTSIZE);
+
+    // Write stream to our buffer and compare it with TEXT
+    ASSERT_EQ(stream.Read(TEXTSIZE, mBuffer.get()), TEXTSIZE);
+    ASSERT_EQ(memcmp(mBuffer.get(), TEXT.data(), TEXTSIZE), 0);
+}
+
+TEST_F(BufferInputStreamTest, StringBufferSeekTest)
+{
+    // Create stream pointing to our TEXT
+    BufferInputStream stream(TEXT.data(), TEXTSIZE);
+
+    // Seek to the middle of the stream (TEXT/2) and compare it with 2nd half of TEXT
+    size_t halfSize = TEXTSIZE / 2;
+    bool seekResult = stream.Seek(halfSize);
+    ASSERT_EQ(seekResult, true);
+    size_t readResult = stream.Read(halfSize, mBuffer.get());
+    ASSERT_EQ(readResult, halfSize);
+    ASSERT_EQ(memcmp(mBuffer.get(), TEXT.data() + halfSize, halfSize), 0);
+}
--- nfEngine/nfCommonTest/test.hpp
+++ nfEngine/nfCommonTest/test.hpp
@@ -6,3 +6,7 @@
 const std::string TEST_PACK_PATH = "testfile.nfp";
 const std::string TEST_SAMPLE_FILE_DIR = "./TestDir/";
 const std::string TEST_SAMPLE_FILE_PREFIX = "samplefile_";
+
+// BufferOutputStream and BufferInputStream constants
+const std::string TEXT = "The quick brown fox jumps over the lazy dog";
+const int TEXTSIZE = TEXT.length();
