7793678c7ebc35f0b896d0e1835c9a65c19a986e
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-21 11:24:46 +0200

    Relocate utility functions from nfCore to nfCommon
    Moved ReadHardwareInfo()@Engine.cpp and MemoryCheck()@Memory.cpp functions
    from nfCore to nfCommon.
    
    Change-Id: I06106e63b594f2a924672dc647c0ee13ff57518d

---
+++ nfEngine/nfCommon/Memory.hpp
@@ -0,0 +1,39 @@
+/**
+ * @file   Memory.hpp
+ * @author mkulagowski (mkkulagowski@gmail.com)
+ * @brief  Memory utilities declarations.
+ */
+
+#pragma once
+
+#include "Common.hpp"
+
+namespace NFE {
+namespace Common {
+
+//memory access rights
+#define ACCESS_READ (1<<0)
+#define ACCESS_WRITE (1<<1)
+#define ACCESS_EXECUTE (1<<2)
+#define ACCESS_READWRITE (ACCESS_READ|ACCESS_WRITE)
+
+/**
+ * Check memory block access privileges. This function is mainly used to verify pointers
+ *        passed as input parameters to engine API to avoid access violations (segmentation faults).
+ * @param ptr Pointer to the beginning of a block
+ * @param size Block size
+ * @return Bitfield describing access privileges to the memory block
+ */
+COMMON_API uint32 MemoryCheck(const void* ptr, size_t size);
+
+/**
+ * Template version of MemoryCheck(). Useful to verify pointer to a single object.
+ */
+template <typename T>
+uint32 MemoryCheck(const T* ptr)
+{
+    return MemoryCheck(ptr, sizeof(T));
+}
+
+} // namespace Common
+} // namespace NFE
