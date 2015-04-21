034b8728cdaac3cb850f4f8360a5af22be2e8eb6
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-21 11:24:46 +0200

    Relocate utility functions from nfCore to nfCommon
    Moved ReadHardwareInfo()@Engine.cpp and MemoryCheck()@Memory.cpp functions
    from nfCore to nfCommon.
    
    Change-Id: I06106e63b594f2a924672dc647c0ee13ff57518d

---
+++ nfEngine/nfCommonTest/MemoryTest.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.hpp"
+#include "../nfCommon/Memory.hpp"
+
+using namespace NFE::Common;
+
+TEST(MemoryTest, AccessReadWrite)
+{
+    int* x = new int;
+    uint32 resultAccess = MemoryCheck(x);
+    ASSERT_EQ(ACCESS_READWRITE, resultAccess);
+    delete x;
+}
+
+TEST(MemoryTest, AccessNone)
+{
+    int* x = nullptr;
+    uint32 resultAccess = MemoryCheck(x);
+    ASSERT_EQ(0, resultAccess);
+}
