3a8392a58571a19638186aa68f234fb98ad61356
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-21 11:24:46 +0200

    Relocate utility functions from nfCore to nfCommon
    Moved ReadHardwareInfo()@Engine.cpp and MemoryCheck()@Memory.cpp functions
    from nfCore to nfCommon.
    
    Change-Id: I06106e63b594f2a924672dc647c0ee13ff57518d

---
+++ nfEngine/nfCommon/Memory.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file   Memory.cpp
+ * @author mkulagowski (mkkulagowski@gmail.com)
+ * @brief  Memory utilities definitions
+ */
+
+#include "stdafx.hpp"
+#include "Memory.hpp"
+
+namespace NFE {
+namespace Common {
+
+//check if memory block can be read and/or written
+uint32 MemoryCheck(const void* ptr, size_t size)
+{
+    MEMORY_BASIC_INFORMATION info;
+    VirtualQuery(ptr, &info, sizeof(info));
+
+    if (((size_t)info.BaseAddress + info.RegionSize) <= ((size_t)ptr + size))
+        return false;
+
+    UINT flags = 0;
+    switch (info.Protect & 0xFF)
+    {
+        case PAGE_EXECUTE_READ:
+            flags = ACCESS_READ | ACCESS_EXECUTE;
+            break;
+
+        case PAGE_EXECUTE:
+            flags = ACCESS_EXECUTE;
+            break;
+
+        case PAGE_EXECUTE_READWRITE:
+            flags = ACCESS_WRITE | ACCESS_READ | ACCESS_EXECUTE;
+            break;
+
+        case PAGE_READONLY:
+            flags = ACCESS_READ;
+            break;
+
+        case PAGE_READWRITE:
+            flags = ACCESS_WRITE | ACCESS_READ;
+            break;
+    }
+
+    return flags;
+}
+
+} // namespace Common
+} // namespace NFE
