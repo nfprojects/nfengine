9a7e30b8b32bd0cf5e6dd3e1aef2a8bfba81a564
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-27 14:26:12 +0200

    Implement SystemInfo class
    Implementation of Windows and Linux version of SystemInfo class in
    nfCommon. Single test in nfCommonTest added.
    
    Verification:
    Compile and run test with '-v' option. Check if whole output is logically
    and well printed out.
    
    Change-Id: I2acc617fc5683e0a6f6d8431adea2f36e6b59530

---
+++ nfEngine/nfCommon/Win/SystemInfoPlatform.cpp
@@ -0,0 +1,57 @@
+/**
+ * @file   Win/SystemInfoPlatform.cpp
+ * @author mkulagowski (mkkulagowski@gmail.com)
+ * @brief  System information API implementation for Windows system
+ */
+
+#include "../stdafx.hpp"
+#include "../SystemInfo.hpp"
+
+namespace NFE {
+namespace Common {
+
+void SystemInfo::InitCPUInfoPlatform()
+{
+    //get CPU information
+    SYSTEM_INFO sysInfo;
+    GetSystemInfo(&sysInfo);
+    mCPUCoreNo = sysInfo.dwNumberOfProcessors;
+    mPageSize = sysInfo.dwPageSize;
+}
+
+void SystemInfo::InitMemoryInfo()
+{
+    // get memory information
+    MEMORYSTATUSEX memStatusEx;
+    memStatusEx.dwLength = sizeof(memStatusEx);
+    GlobalMemoryStatusEx(&memStatusEx);
+    mMemTotalPhysKb = memStatusEx.ullTotalPhys / 1024;
+    mMemFreePhysKb = memStatusEx.ullAvailPhys / 1024;
+    mMemTotalVirtKb = memStatusEx.ullTotalPageFile / 1024; // PageFile is swap + RAM
+    mMemFreeVirtKb = memStatusEx.ullAvailPageFile / 1024; // PageFile is swap + RAM
+}
+
+void SystemInfo::Cpuid(int cpuInfo[4], int function_id)
+{
+    // MSVC provides a __cpuid function
+    __cpuid(cpuInfo, function_id);
+}
+
+uint64_t SystemInfo::Rdtsc()
+{
+    return __rdtsc();
+}
+
+uint64_t const SystemInfo::GetFreeMemoryKb()
+{
+    // get RAM info
+    MEMORYSTATUSEX memStatusEx;
+    memStatusEx.dwLength = sizeof(memStatusEx);
+    GlobalMemoryStatusEx(&memStatusEx);
+    mMemFreePhysKb = memStatusEx.ullAvailPhys / 1024;
+    mMemFreeVirtKb = memStatusEx.ullAvailPageFile / 1024; // PageFile is swap + RAM
+
+    return mMemFreeVirtKb + mMemFreePhysKb;
+}
+} // namespace Common
+} // namespace NFE
