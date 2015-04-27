5f118ccd0e6e1df4d8e61ea4666607a1f6f7828d
mkkulagowski <mk.kulagowski@gmail.com>
2015-04-27 14:26:12 +0200

    Implement SystemInfo class
    Implementation of Windows and Linux version of SystemInfo class in
    nfCommon. Tests added.
    
    Verification:
    Compile and run tests with '-v' option. Check if whole output is logically
    and well printed out.
    
    Change-Id: I2acc617fc5683e0a6f6d8431adea2f36e6b59530

---
+++ nfEngine/nfCommon/Linux/SystemInfoPlatform.cpp
@@ -0,0 +1,148 @@
+/**
+ * @file   Linux/SystemInfoPlatform.cpp
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  System information API implementation for Linux system
+ */
+
+#include "../PCH.hpp"
+#include "../SystemInfo.hpp"
+#include "../Math/Math.hpp"
+#include <stdio.h>
+
+namespace NFE {
+namespace Common {
+
+void SystemInfo::InitCPUInfoPlatform()
+{
+    //get CPU information
+    mCPUCoreNo = sysconf(_SC_NPROCESSORS_ONLN); // 'ONLN' are currently available,
+                                                // 'CONF' are configured
+    mPageSize = sysconf(_SC_PAGESIZE); // may be 'PAGE_SIZE' on some systems
+}
+
+void SystemInfo::InitMemoryInfo()
+{
+    // get memory information
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+
+    std:string line;
+    uint64_t memUsedVirtKb;
+
+    std::ifstream statmStream("/proc/self/statm");
+    std::getline(statmStream, line);
+    sscanf(line.c_str(), "%*s %ul", memUsedVirtKb);
+    memstatmStreamInfo.close();
+
+    /* TODO Implement checking total virtual memory for the current process
+     * then we will be able to calculate free virtual memory
+     *     mMemTotalVirtKb =
+     *     mMemFreeVirtKb = mMemTotalVirtKb;
+     *     mMemFreeVirtKb -= memUsedVirtKb;
+    */
+    // 2 line "hotfix" waiting for the to-do above
+    mMemTotalVirtKb = 0;
+    mMemFreeVirtKb = 0;
+
+    uint64_t memUnit = sysInfo.mem_unit;
+
+    mMemTotalSwapKb = sysInfo.totalswap * memUnit;
+    mMemTotalSwapKb /= MEMORY_BYTES_DIVISOR;
+
+    mMemFreeSwapKb = sysInfo.freeswap * memUnit;
+    mMemFreeSwapKb /= MEMORY_BYTES_DIVISOR;
+
+    mMemTotalPhysKb = sysInfo.totalram * memUnit;
+    mMemTotalPhysKb /= MEMORY_BYTES_DIVISOR;
+
+    mMemFreePhysKb = sysInfo.freeram * memUnit;
+    mMemFreePhysKb /= MEMORY_BYTES_DIVISOR;
+}
+
+void SystemInfo::Cpuid(int cpuInfo[4], int function_id)
+{
+#if defined(__i386__) && defined(__PIC__)
+/* %ebx may be the PIC register.  */
+#if __GNUC__ >= 3
+    __asm__ ("xchg{l}\t{%%}ebx, %1\n\t"
+            "cpuid\n\t"
+            "xchg{l}\t{%%}ebx, %1\n\t"
+            : "=a" (cpuInfo[0]), "=r" (cpuInfo[1]), "=c" (cpuInfo[2]), "=d" (cpuInfo[3])
+            : "0" (function_id));
+#else
+/* Host GCCs older than 3.0 weren't supporting Intel asm syntax
+   nor alternatives in i386 code.  */
+    __asm__ ("xchgl\t%%ebx, %1\n\t"
+            "cpuid\n\t"
+            "xchgl\t%%ebx, %1\n\t"
+            : "=a" (cpuInfo[0]), "=r" (cpuInfo[1]), "=c" (cpuInfo[2]), "=d" (cpuInfo[3])
+            : "0" (function_id));
+#endif
+#else
+    __asm__ ("cpuid\n\t"
+            : "=a" (cpuInfo[0]), "=b" (cpuInfo[1]), "=c" (cpuInfo[2]), "=d" (cpuInfo[3])
+            : "0" (function_id));
+#endif
+}
+
+uint64_t SystemInfo::Rdtsc()
+{
+    unsigned int hi, lo;
+    __asm__ volatile("rdtsc" : "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+uint64_t SystemInfo::GetFreeMemoryKb()
+{
+    //get RAM info
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+
+    uint64_t memUnit = sysInfo.mem_unit;
+
+    mMemFreeSwapKb = sysInfo.freeswap * memUnit;
+    mMemFreeSwapKb /= MEMORY_BYTES_DIVISOR;
+
+    mMemFreePhysKb = sysInfo.freeram * memUnit;
+    mMemFreePhysKb /= MEMORY_BYTES_DIVISOR;
+
+    /* TODO Implement checking total virtual memory for the current process
+     * then we will be able to calculate free virtual memory
+     *     mMemTotalVirtKb =
+     *     mMemFreeVirtKb = mMemTotalVirtKb;
+     *     mMemFreeVirtKb -= memUsedVirtKb;
+    */
+
+    return Math::Min(mMemFreeSwapKb + mMemFreePhysKb, mMemFreeVirtKb);
+}
+
+uint64_t SystemInfo::GetMemFreePhysKb()
+{
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+
+    uint64_t memUnit = sysInfo.mem_unit;
+    return mMemFreePhysKb;
+}
+
+uint64_t SystemInfo::GetMemFreeVirtKb()
+{
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+
+    uint64_t memUnit = sysInfo.mem_unit;
+    return mMemFreeVirtKb;
+}
+
+uint64_t SystemInfo::GetMemFreeSwapKb()
+{
+    struct sysinfo sysInfo;
+    sysinfo(&sysInfo);
+
+    uint64_t memUnit = sysInfo.mem_unit;
+    mMemFreeSwapKb = sysInfo.freeswap * memUnit;
+    mMemFreeSwapKb /= MEMORY_BYTES_DIVISOR;
+    return mMemFreeSwapKb;
+}
+} // namespace Common
+} // namespace NFE
--- nfEngine/nfCommon/SystemInfoConstants.hpp
+++ nfEngine/nfCommon/SystemInfoConstants.hpp
@@ -1,13 +1,18 @@
 /**
  * @file   SystemInfoConstants.hpp
- * @author mkulagowski (mkkulagowski@gmail.com)
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
  * @brief  Constants for SystemInfo class
  */
 
 #pragma once
 
-const int EXTENDED_ID_MAX_VALUE = 0x80000000;
-const int CPU_BRAND_STRING_LENGTH = 0x40;
-const int CPU_BRAND_STRING_1 = 0x80000002;
-const int CPU_BRAND_STRING_2 = 0x80000003;
-const int CPU_BRAND_STRING_3 = 0x80000004;
+const unsigned int EXTENDED_ID_MAX_VALUE = 0x80000000;
+const unsigned int CPU_BRAND_STRING_LENGTH = 0x40;
+const unsigned int CPU_BRAND_STRING_1 = 0x80000002;
+const unsigned int CPU_BRAND_STRING_2 = 0x80000003;
+const unsigned int CPU_BRAND_STRING_3 = 0x80000004;
+const unsigned int CPU_CACHE_LINE_SIZE = 0x80000006;
+
+const unsigned int CPUID_FEATURES_1_2 = 0x1;
+const unsigned int CPUID_FEATURES_3 = 0x7;
+const unsigned int CPUID_FEATURES_4_5 = 0x80000001;
