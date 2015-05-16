d5518b6de74ad5ee732d458b1db106364a6d237f
Michal Witanowski <witek902@gmail.com>
2015-05-17 00:06:30 +0200

    nfRendererD3D11: More shader features
    * added a function getting information about shader I/Os
    * added a function getting a shader disassembly
    * simple shader test written
    
    Change-Id: I5cabe5907ad5ee807dc1a79bfdf5bfc90e265ef7

--- nfEngine/Renderers/nfRendererD3D11/Shader.cpp
+++ nfEngine/Renderers/nfRendererD3D11/Shader.cpp
@@ -6,7 +6,8 @@
 
 #include "PCH.hpp"
 #include "RendererD3D11.hpp"
-#include "../../nfCommon/Logger.hpp"
+#include "Logger.hpp"
+#include "File.hpp"
 
 namespace NFE {
 namespace Renderer {
@@ -44,7 +45,33 @@ bool Shader::Init(const ShaderDesc& desc)
     mType = desc.type;
 
     HRESULT hr;
-    size_t shaderSize = strlen(desc.code);
+    std::vector<char> str;
+    size_t shaderSize = 0;
+    const char* code = nullptr;
+
+    if (desc.code == nullptr)
+    {
+        if (desc.path == nullptr)
+        {
+            LOG_ERROR("Shader code or path must be suplied");
+            return false;
+        }
+
+        using namespace Common;
+        File file(desc.path, AccessMode::Read);
+        shaderSize = static_cast<size_t>(file.GetSize());
+        str.resize(shaderSize + 1);
+
+        if (file.Read(str.data(), shaderSize) != shaderSize)
+            return false;
+        str[shaderSize] = '\0';
+        code = str.data();
+    }
+    else
+    {
+        code = desc.code;
+        shaderSize = strlen(code);
+    }
 
     DWORD flags = D3DCOMPILE_ENABLE_STRICTNESS;
 #ifdef _DEBUG //shaders debugging
@@ -78,19 +105,19 @@ bool Shader::Init(const ShaderDesc& desc)
     }
 
     ID3DBlob* errorsBuffer = nullptr;
-    hr = D3DCompile(desc.code, shaderSize, desc.name, NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+    hr = D3DCompile(code, shaderSize, desc.path, NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
                     "main", profileName, flags, 0, &mBytecode, &errorsBuffer);
 
     if (errorsBuffer)
     {
-        LOG_ERROR("Shader '%s' compilation output:\n%s", desc.name,
+        LOG_ERROR("Shader '%s' compilation output:\n%s", desc.path,
                   (char*)errorsBuffer->GetBufferPointer());
         errorsBuffer->Release();
     }
 
     if (FAILED(hr))
     {
-        LOG_ERROR("Compilation of shader '%s' failed", desc.name);
+        LOG_ERROR("Compilation of shader '%s' failed", desc.path);
         return false;
     }
 
@@ -129,7 +156,33 @@ bool Shader::Init(const ShaderDesc& desc)
         return false;
     }
 
-    LOG_SUCCESS("Shader '%s' compiled successfully", desc.name)
+    LOG_SUCCESS("Shader '%s' compiled successfully", desc.path)
+    return true;
+}
+
+bool Shader::Disassemble(std::string& output, bool html)
+{
+    ID3DBlob* bytecode = mBytecode.get();
+    if (bytecode == nullptr)
+    {
+        LOG_ERROR("Shader is not compiled");
+        return false;
+    }
+
+    UINT flags = 0;
+    if (html)
+        flags = D3D_DISASM_ENABLE_COLOR_CODE | D3D_DISASM_ENABLE_INSTRUCTION_NUMBERING;
+
+    HRESULT hr;
+    D3DPtr<ID3DBlob> disassembly;
+    hr = D3D_CALL_CHECK(D3DDisassemble(bytecode->GetBufferPointer(), bytecode->GetBufferSize(),
+                                       flags, 0, &disassembly));
+    if (FAILED(hr))
+        return false;
+
+    const char* str = static_cast<const char*>(disassembly->GetBufferPointer());
+    output = std::string(str, disassembly->GetBufferSize());
+
     return true;
 }
 
@@ -143,6 +196,69 @@ ID3DBlob* Shader::GetBytecode() const
 {
     return mBytecode.get();
 }
+
+bool Shader::GetIODesc(ShaderIODesc& result)
+{
+    HRESULT hr;
+    result.resBinding.clear();
+
+    ID3DBlob* bytecode = mBytecode.get();
+    if (bytecode == nullptr)
+    {
+        LOG_ERROR("Shader is not compiled");
+        return false;
+    }
+
+    // get reflector object
+    D3DPtr<ID3D11ShaderReflection> reflection;
+    hr = D3D_CALL_CHECK(D3DReflect(bytecode->GetBufferPointer(), bytecode->GetBufferSize(),
+                                   IID_ID3D11ShaderReflection, (void**)&reflection));
+    if (FAILED(hr))
+        return false;
+
+    // get shader descriptior via reflector
+    D3D11_SHADER_DESC desc;
+    hr = D3D_CALL_CHECK(reflection->GetDesc(&desc));
+    if (FAILED(hr))
+        return false;
+
+    for (UINT i = 0; i < desc.BoundResources; ++i)
+    {
+        D3D11_SHADER_INPUT_BIND_DESC d3dBindingDesc;
+        hr = D3D_CALL_CHECK(reflection->GetResourceBindingDesc(i, &d3dBindingDesc));
+        if (FAILED(hr))
+        {
+            LOG_ERROR("Failed to parse resource binding, i = %d", i);
+            return false;
+        }
+
+        ShaderResBindingDesc bindingDesc;
+        bindingDesc.slot = d3dBindingDesc.BindPoint;
+
+        switch (d3dBindingDesc.Type)
+        {
+        case D3D_SIT_CBUFFER:
+            bindingDesc.type = ShaderResourceType::CBuffer;
+            break;
+        case D3D_SIT_TEXTURE:
+            bindingDesc.type = ShaderResourceType::Texture;
+            break;
+        case D3D_SIT_SAMPLER:
+            bindingDesc.type = ShaderResourceType::Sampler;
+            break;
+        default:
+            LOG_WARNING("Unsupported shader resource type (%d) at slot %d (name: '%s')",
+                        d3dBindingDesc.Type, i, d3dBindingDesc.Name);
+            continue;
+        }
+
+        std::string name = d3dBindingDesc.Name;
+        result.resBinding[name] = bindingDesc;
+    }
+
+    return true;
+}
+
 
 } // namespace Renderer
 } // namespace NFE
