36901b6aedef815459b9c1e4417a288de6c6da1a
mkkulagowski <mk.kulagowski@gmail.com>
2015-05-20 15:18:12 +0200

    nfCommon: Implement Library module on linux
    Implement Library module, so it works on linux platform.
    Add tests and custom test-dynamic-library.
    
    Verification: Compile and run tests.
    
    Change-Id: Ibfc3b501355fc3e7f243691d05aca82a4142a27f

--- nfEngine/nfCommon/Win/Library.cpp
+++ nfEngine/nfCommon/Win/Library.cpp
@@ -14,7 +14,7 @@ namespace Common {
 
 Library::Library()
 {
-    mModule = NULL;
+    mModule = nullptr;
 }
 
 Library::Library(const std::string& path)
@@ -25,8 +25,9 @@ Library::Library(const std::string& path)
 Library::Library(Library&& other)
 {
     Close();
+
     mModule = other.mModule;
-    other.mModule = NULL;
+    other.mModule = nullptr;
 }
 
 Library::~Library()
@@ -36,22 +37,27 @@ Library::~Library()
 
 bool Library::IsOpened() const
 {
-    return mModule != NULL;
+    return mModule != nullptr;
 }
 
 bool Library::Open(const std::string& path)
 {
     Close();
 
+    std::string pathExt(path);
+    std::string libExt = ".dll";
+    if (libExt.compare(pathExt.substr(pathExt.size() - libExt.size())) != 0)
+        pathExt.append(libExt);
+
     std::wstring widePath;
-    if (!UTF8ToUTF16(path, widePath))
+    if (!UTF8ToUTF16(pathExt, widePath))
         return false;
 
     mModule = ::LoadLibrary(widePath.c_str());
 
-    if (mModule == NULL)
+    if (mModule == nullptr)
     {
-        LOG_ERROR("Failed to load library '%s': %s", path.c_str(), GetLastErrorString().c_str());
+        LOG_ERROR("Failed to load library '%s': %s", pathExt.c_str(), GetLastErrorString().c_str());
         return false;
     }
 
@@ -60,20 +66,20 @@ bool Library::Open(const std::string& path)
 
 void Library::Close()
 {
-    if (mModule == NULL)
+    if (mModule != nullptr)
     {
         ::FreeLibrary(mModule);
-        mModule = NULL;
+        mModule = nullptr;
     }
 }
 
 void* Library::GetSymbol(const std::string& name)
 {
-    if (mModule == NULL)
+    if (mModule == nullptr)
         return nullptr;
 
     FARPROC ptr = ::GetProcAddress(mModule, name.c_str());
-    if (ptr == NULL)
+    if (ptr == nullptr)
     {
         LOG_ERROR("Failed to get pointer to symbol '%s': %s", name.c_str(),
                   GetLastErrorString().c_str());
