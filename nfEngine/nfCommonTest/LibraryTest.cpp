a6815b37ebacf5a19cfc4bc94380cb50eb5fbbf8
mkkulagowski <mk.kulagowski@gmail.com>
2015-05-20 15:18:12 +0200

    nfCommon: Implement Library module on linux
    Implement Library module, so it works on linux platform.
    Add tests and custom test-dynamic-library.
    
    Verification: Compile and run tests.
    
    Change-Id: Ibfc3b501355fc3e7f243691d05aca82a4142a27f

---
+++ nfEngine/nfCommonTest/LibraryTest.cpp
@@ -0,0 +1,81 @@
+#include "PCH.hpp"
+#include "../nfCommon/Library.hpp"
+
+using namespace NFE::Common;
+
+typedef int(*FuncType)(int, int);
+
+class LibraryTest : public testing::Test
+{
+protected:
+    std::unique_ptr<Library> mLib;
+
+    void SetUp()
+    {
+        mLib.reset(new Library);
+    }
+
+    void TearDown()
+    {
+        mLib->Close();
+    }
+};
+
+TEST_F(LibraryTest, NotExistingLib)
+{
+    ASSERT_FALSE(mLib->Open("this_file_does_not_exist"));
+}
+
+TEST_F(LibraryTest, GetSymbolErrors)
+{
+    // Loading function before opening library results in nullptr
+    
+    FuncType WinLife;
+    ASSERT_FALSE(mLib->GetSymbol("Add", WinLife));
+
+    // Loading nonexistent function also results in nulltr
+    ASSERT_TRUE(mLib->Open("testCalcLib"));
+    ASSERT_FALSE(mLib->GetSymbol("WinLife", WinLife));
+}
+
+TEST_F(LibraryTest, LibUsage)
+{
+    // Open test library
+    ASSERT_TRUE(mLib->Open("testCalcLib"));
+
+    // Load functions from the library
+    FuncType Add, Subtract;
+    ASSERT_TRUE(mLib->GetSymbol("Add", Add));
+    ASSERT_TRUE(mLib->GetSymbol("Subtract", Subtract));
+    // Check if functions were loaded successfully
+    ASSERT_TRUE(NULL != Add);
+    ASSERT_TRUE(NULL != Subtract);
+
+    // Check if loaded functions work properly
+    int number1 = 12345;
+    int number2 = 54321;
+
+    int result = number1 + number2;
+    int funcResult = Add(number1, number2);
+    ASSERT_EQ(result, funcResult);
+
+    result = number1 - number2;
+    funcResult = Subtract(number1, number2);
+    ASSERT_EQ(result, funcResult);
+}
+
+TEST_F(LibraryTest, IsOpened)
+{
+    ASSERT_FALSE(mLib->IsOpened());
+    ASSERT_TRUE(mLib->Open("testCalcLib"));
+    ASSERT_TRUE(mLib->IsOpened());
+}
+
+TEST_F(LibraryTest, CopyConstructor)
+{
+    ASSERT_TRUE(mLib->Open("testCalcLib"));
+    Library lib(*mLib.get());
+    ASSERT_TRUE(lib.IsOpened());
+    ASSERT_FALSE(mLib->IsOpened());
+    lib.Close();
+}
