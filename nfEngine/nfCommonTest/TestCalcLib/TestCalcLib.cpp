62acb0975d8c1b9c690c0824b4bbde36e8929ecc
mkkulagowski <mk.kulagowski@gmail.com>
2015-05-20 15:18:12 +0200

    nfCommon: Implement Library module on linux
    Implement Library module, so it works on linux platform.
    Add tests and custom test-dynamic-library.
    
    Verification: Compile and run tests.
    
    Change-Id: Ibfc3b501355fc3e7f243691d05aca82a4142a27f

---
+++ nfEngine/nfCommonTest/TestCalcLib/TestCalcLib.cpp
@@ -0,0 +1,18 @@
+/**
+* @file
+* @author mkkulagowski (mkulagowski(at)gmail.com)
+* @brief Definition of test library for LibraryTest
+*/
+
+#include "TestCalcLib.hpp"
+
+
+int Add(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int Subtract(int num1, int num2)
+{
+    return num1 - num2;
+}
