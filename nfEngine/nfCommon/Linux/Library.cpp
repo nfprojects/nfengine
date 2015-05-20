490dadf6a86c0810f5f968ac754ae1036deafd1f
mkkulagowski <mk.kulagowski@gmail.com>
2015-05-20 15:18:12 +0200

    nfCommon: Implement Library module on linux
    Implement Library module, so it works on linux platform.
    Add tests and custom test-dynamic-library.
    
    Verification: Compile and run tests.
    
    Change-Id: Ibfc3b501355fc3e7f243691d05aca82a4142a27f

--- nfEngine/nfCommon/Linux/Library.cpp
+++ nfEngine/nfCommon/Linux/Library.cpp
@@ -1,13 +1,12 @@
 /**
  * @file
- * @author Witek902 (witek902@gmail.com)
- * @brief  Windows implementation of Library class.
+ * @author Mkkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Linux implementation of Library class.
  */
 
 #include "../PCH.hpp"
 #include "../Library.hpp"
-#include "../Logger.hpp"
-#include "Common.hpp"
+#include <dlfcn.h>
 
 namespace NFE {
 namespace Common {
@@ -43,20 +42,17 @@ bool Library::Open(const std::string& path)
 {
     Close();
 
-    std::string pathExt(path);
-    std::string libExt = ".dll";
+    std::string pathExt = "lib";
+    pathExt += path;
+    std::string libExt = ".so";
     if (libExt.compare(pathExt.substr(pathExt.size() - libExt.size())) != 0)
         pathExt.append(libExt);
 
-    std::wstring widePath;
-    if (!UTF8ToUTF16(pathExt, widePath))
-        return false;
-
-    mModule = ::LoadLibrary(widePath.c_str());
+    mModule = dlopen(pathExt.c_str(), RTLD_LAZY);
 
     if (mModule == NULL)
     {
-        LOG_ERROR("Failed to load library '%s': %s", pathExt.c_str(), GetLastErrorString().c_str());
+        std::cout << "Failed to load library " << pathExt.c_str() << ": " << dlerror() << std::endl;
         return false;
     }
 
@@ -65,9 +61,10 @@ bool Library::Open(const std::string& path)
 
 void Library::Close()
 {
-    if (mModule == NULL)
+    if (mModule != NULL)
     {
-        ::FreeLibrary(mModule);
+        if (dlclose(mModule))
+            std::cout << "Failed to close library: " << dlerror() << std::endl;
         mModule = NULL;
     }
 }
@@ -77,15 +74,17 @@ void* Library::GetSymbol(const std::string& name)
     if (mModule == NULL)
         return nullptr;
 
-    FARPROC ptr = ::GetProcAddress(mModule, name.c_str());
-    if (ptr == NULL)
+    // it is recommended to clear dlerror first, because NULL value returned CAN be valid
+    dlerror();
+    void* ptr = dlsym(mModule, name.c_str());
+    char* errorMsg = dlerror();
+    if (errorMsg != NULL)
     {
-        LOG_ERROR("Failed to get pointer to symbol '%s': %s", name.c_str(),
-                  GetLastErrorString().c_str());
+        std::cout << "Failed to get pointer to symbol " << name.c_str() << ": " << errorMsg << std::endl;
         return nullptr;
     }
 
-    return static_cast<void*>(ptr);
+    return ptr;
 }
 
 } // namespace Common
