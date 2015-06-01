9909abebea37d75984802870bc6dd58c034e0328
Michal Witanowski <witek902@gmail.com>
2015-06-01 23:45:25 +0200

    Fix for GCC 5.1.1
    Builing the project with newest version of GCC failed with error:
    "nfCommonTest/Test.hpp:12:14: error: ‘TEXTSIZE’ defined but not used".
    TEXSIZE and other constants definitions have been moved to separate cpp file.
    
    Verification:
        Rebuild all, run tests.
    
    Change-Id: Ibe2d150b72409856aba15537b1cbac6e2d12e997

--- nfEngine/nfCommonTest/Constants.cpp
+++ nfEngine/nfCommonTest/Constants.cpp
@@ -1,12 +1,9 @@
-#pragma once
+#include "PCH.hpp"
+#include "Constants.hpp"
 
-#include <string>
-
-// libpacker-related constants
 const std::string TEST_PACK_PATH = "testfile.nfp";
 const std::string TEST_SAMPLE_FILE_DIR = "./TestDir/";
 const std::string TEST_SAMPLE_FILE_PREFIX = "samplefile_";
 
-// BufferOutputStream and BufferInputStream constants
 const std::string TEXT = "The quick brown fox jumps over the lazy dog";
 const size_t TEXTSIZE = TEXT.length();
