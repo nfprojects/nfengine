c4dabf068f265c684246f1a54e14dc1fb9ed210a
mkkulagowski <mk.kulagowski@gmail.com>
2015-06-09 12:05:07 +0200

    Remove SetUpCurrentDirectory functions
    Remove SetUpCurrentDirectory functions from tests and demos. Reimplement
    this functionality using FileSystem class from nfCommon.
    
    Verification: Compile and run tests and demos.
    
    Change-Id: I4a4e2cd174c55a8c80dc71f9c0b7749b0e956e39

--- nfEngine/nfCoreTest/Main.cpp
+++ nfEngine/nfCoreTest/Main.cpp
@@ -1,21 +1,12 @@
 #include "PCH.hpp"
-
-// temporary
-void SetUpCurrentDirectory()
-{
-    char szFileName[MAX_PATH];
-    GetModuleFileNameA(NULL, szFileName, MAX_PATH);
-    std::string rootDir = std::string(szFileName);
-    size_t found = rootDir.find_last_of("/\\");
-    rootDir = rootDir.substr(0, found) + "/../../../";
-    SetCurrentDirectoryA(rootDir.c_str());
-}
+#include "../nfCommon/FileSystem.hpp"
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
 
-    SetUpCurrentDirectory();
+    std::string execPath = NFE::Common::FileSystem::GetExecutablePath();
+    NFE::Common::FileSystem::ChangeDirectory(execPath + "/../../..");
     int result = RUN_ALL_TESTS();
 
     _CrtDumpMemoryLeaks();
