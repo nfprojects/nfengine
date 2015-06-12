dcc0dc3e9a07471d15eb0c491f1040ecc7af68c0
mkkulagowski <mk.kulagowski@gmail.com>
2015-06-12 17:04:11 +0200

    [nfCommonTest] Implement Image class tests
    Unit tests for Image class from nfCommon.
    
    Verification: Compile and run tests.
    
    Change-Id: I4676b600549b8f046b7b87ddb385b2d3e05a099a

---
+++ nfEngine/nfCommonTest/ImageTest.cpp
@@ -0,0 +1,239 @@
+#include "PCH.hpp"
+#include "../nfCommon/Image.hpp"
+#include "../nfCommon/InputStream.hpp"
+#include "Test.hpp"
+
+using namespace NFE::Common;
+
+class ImageTest : public testing::Test
+{
+protected:
+    std::unique_ptr<Image> mImage;
+
+    void SetUp()
+    {
+        EXPECT_NO_THROW(mImage.reset(new Image()));
+    }
+};
+
+TEST_F(ImageTest, BasicConstructor)
+{
+    // For uninitialized mImage nothing should work...
+    ASSERT_EQ(0, mImage->GetWidth());
+    ASSERT_EQ(0, mImage->GetHeight());
+    ASSERT_EQ(0, mImage->GetMipmapsNum());
+    ASSERT_TRUE(mImage->GetData() == nullptr);
+    //ASSERT_TRUE(mImage->GetMipmap(0).data == nullptr);
+    ASSERT_EQ(ImageFormat::Unknown, mImage->GetFormat());
+}
+
+TEST_F(ImageTest, CopyConstructor)
+{
+    Image mImage_copy1;
+    mImage_copy1.SetData(const_cast<char*>(TEXT.data()), 5, 5, ImageFormat::BC1);
+    Image mImage_copy2(mImage_copy1);
+
+    ASSERT_EQ(1, mImage_copy2.GetMipmapsNum());
+    ASSERT_EQ(const_cast<char*>(TEXT.data()), mImage_copy2.GetData());
+    ASSERT_EQ(ImageFormat::BC1, mImage_copy2.GetFormat());
+}
+
+TEST_F(ImageTest, SetData)
+{
+    // Setting dummy data and additional information
+    mImage->SetData(const_cast<char*>(TEXT.data()), 5, 5, ImageFormat::BC1);
+    ASSERT_EQ(1, mImage->GetMipmapsNum());
+    ASSERT_EQ(const_cast<char*>(TEXT.data()), mImage->GetData());
+    ASSERT_EQ(ImageFormat::BC1, mImage->GetFormat());
+}
+
+TEST_F(ImageTest, GenerateMipmaps)
+{
+    // No data
+    ASSERT_EQ(1, mImage->GenerateMipmaps(5));
+
+    // Unknown mImage format
+    mImage->SetData(const_cast<char*>(TEXT.data()), 5, 5, ImageFormat::Unknown);
+    ASSERT_EQ(1, mImage->GenerateMipmaps(5));
+
+    // block-coding not supported
+    mImage->SetData(const_cast<char*>(TEXT.data()), 5, 5, ImageFormat::BC1);
+    ASSERT_EQ(1, mImage->GenerateMipmaps(5));
+
+    // successfully loaded picture
+    FileInputStream texture(textureJPG.data());
+    ASSERT_EQ(0, mImage->Load(&texture));
+    ASSERT_EQ(0, mImage->GenerateMipmaps(5));
+    ASSERT_EQ(5, mImage->GetMipmapsNum());
+}
+
+TEST_F(ImageTest, Release)
+{
+    FileInputStream texture(textureJPG.data());
+    EXPECT_EQ(0, mImage->Load(&texture));
+    EXPECT_EQ(0, mImage->GenerateMipmaps(5));
+    EXPECT_EQ(5, mImage->GetMipmapsNum());
+    mImage->Release();
+    ASSERT_EQ(0, mImage->GetMipmapsNum());
+    ASSERT_EQ(0, mImage->GetWidth());
+    ASSERT_EQ(0, mImage->GetHeight());
+}
+
+TEST_F(ImageTest, Load)
+{
+    FileInputStream* texture;
+    // lambda to load and assert images
+    auto LoadAssert = [&]()
+    {
+        ASSERT_EQ(0, mImage->Load(texture));
+        ASSERT_EQ(240, mImage->GetWidth());
+        ASSERT_EQ(212, mImage->GetHeight());
+    };
+
+    // Testing Load() for different types of mImage files
+    // JPEG
+    texture = new FileInputStream(textureJPG.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::RGBA_UByte, mImage->GetFormat());
+    delete texture;
+
+    // PNG
+    texture = new FileInputStream(texturePNG.data());
+    LoadAssert();
+    ASSERT_TRUE(ImageFormat::RGBA_UByte == mImage->GetFormat() ||
+                ImageFormat::A_UByte == mImage->GetFormat() ||
+                ImageFormat::RGB_UByte == mImage->GetFormat());
+    delete texture;
+
+    // BMP
+    texture = new FileInputStream(textureBMP.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::RGBA_UByte, mImage->GetFormat());
+    delete texture;
+
+    // DDS
+    //   BC1
+    texture = new FileInputStream(textureDDS_BC1.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::BC1, mImage->GetFormat());
+    delete texture;
+
+    //   BC2
+    texture = new FileInputStream(textureDDS_BC2.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::BC2, mImage->GetFormat());
+    delete texture;
+
+    //   BC3
+    texture = new FileInputStream(textureDDS_BC3.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::BC3, mImage->GetFormat());
+    delete texture;
+
+    //   BC4
+    texture = new FileInputStream(textureDDS_BC4.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::BC4, mImage->GetFormat());
+    delete texture;
+
+    //   BC5
+    texture = new FileInputStream(textureDDS_BC5.data());
+    LoadAssert();
+    ASSERT_EQ(ImageFormat::BC5, mImage->GetFormat());
+    delete texture;
+}
+
+TEST_F(ImageTest, FormatToString)
+{
+    std::string format = Image::FormatToStr(ImageFormat::R_UByte);
+    ASSERT_EQ(0, format.compare("R_uchar"));
+    format = Image::FormatToStr(ImageFormat::Unknown);
+    ASSERT_EQ(0, format.compare("Unknown"));
+}
+
+TEST_F(ImageTest, BitsPerPixel)
+{
+    ASSERT_EQ(8, Image::BitsPerPixel(ImageFormat::R_UByte));
+    ASSERT_EQ(0, Image::BitsPerPixel(ImageFormat::Unknown));
+}
+
+TEST_F(ImageTest, Convert)
+{
+    FileInputStream* texture;
+    // lambda to load, convert and assert images
+    auto LoadAndConvertAssert = [&]()
+    {
+        ASSERT_EQ(0, mImage->Load(texture));
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::A_UByte));
+        ASSERT_EQ(ImageFormat::A_UByte, mImage->GetFormat());
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::R_UByte));
+        ASSERT_EQ(ImageFormat::R_UByte, mImage->GetFormat());
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::RGB_UByte));
+        ASSERT_EQ(ImageFormat::RGB_UByte, mImage->GetFormat());
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::RGBA_UByte));
+        ASSERT_EQ(ImageFormat::RGBA_UByte, mImage->GetFormat());
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::R_Float));
+        ASSERT_EQ(ImageFormat::R_Float, mImage->GetFormat());
+
+        ASSERT_EQ(0, mImage->Convert(ImageFormat::RGBA_Float));
+        ASSERT_EQ(ImageFormat::RGBA_Float, mImage->GetFormat());
+    };
+
+    // Testing Convert() for different types of mImage files
+    // JPEG
+    texture = new FileInputStream(textureJPG.data());
+    LoadAndConvertAssert();
+    delete texture;
+
+    // PNG
+    texture = new FileInputStream(texturePNG.data());
+    LoadAndConvertAssert();
+    delete texture;
+
+    // BMP
+    texture = new FileInputStream(textureBMP.data());
+    LoadAndConvertAssert();
+    delete texture;
+
+    // to Unknown format
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::Unknown));
+
+    // DDS - currently no conversion for dds
+    //   BC1
+    texture = new FileInputStream(textureDDS_BC1.data());
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+    delete texture;
+
+    //   BC2
+    texture = new FileInputStream(textureDDS_BC2.data());
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+    delete texture;
+
+    //   BC3
+    texture = new FileInputStream(textureDDS_BC3.data());
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+    delete texture;
+
+    //   BC4
+    texture = new FileInputStream(textureDDS_BC4.data());
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+    delete texture;
+
+    //   BC5
+    texture = new FileInputStream(textureDDS_BC5.data());
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+    delete texture;
+
+    // NO DATA - nothing to convert
+    mImage->Release();
+    ASSERT_EQ(0, mImage->Convert(ImageFormat::RGBA_UByte));
+
+    // from Unknown format
+    mImage->SetData(const_cast<char*>(TEXT.data()), 5, 5, ImageFormat::Unknown);
+    ASSERT_EQ(1, mImage->Convert(ImageFormat::RGBA_UByte));
+}
--- nfEngine/nfCommonTest/Test.hpp
+++ nfEngine/nfCommonTest/Test.hpp
@@ -8,5 +8,16 @@ const std::string TEST_SAMPLE_FILE_DIR = "./TestDir/";
 const std::string TEST_SAMPLE_FILE_PREFIX = "samplefile_";
 
 // BufferOutputStream and BufferInputStream constants
+// TEXT const used in ImageTest as well
 const std::string TEXT = "The quick brown fox jumps over the lazy dog";
 const size_t TEXTSIZE = TEXT.length();
+
+// Image constants
+const std::string textureDDS_BC1 = "../TestResources/ImageSamples/textureBC1.dds";
+const std::string textureDDS_BC2 = "../TestResources/ImageSamples/textureBC2.dds";
+const std::string textureDDS_BC3 = "../TestResources/ImageSamples/textureBC3.dds";
+const std::string textureDDS_BC4 = "../TestResources/ImageSamples/textureBC4.dds";
+const std::string textureDDS_BC5 = "../TestResources/ImageSamples/textureBC5.dds";
+const std::string textureJPG = "../TestResources/ImageSamples/textureJPG.jpg";
+const std::string textureBMP = "../TestResources/ImageSamples/textureBMP.bmp";
+const std::string texturePNG = "../TestResources/ImageSamples/texturePNG.png";
