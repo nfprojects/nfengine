9c495eccbf18a311a6d778a7d8a9c0922ea76210
Lukasz Kostyra <costyrra.xl@gmail.com>
2015-08-02 01:33:17 +0200

    nfRendererOGL4: Add basic Shader support
    First version of shaders implemented in OpenGL 4.
    
    To keep compatible with D3D11 Renderer, Separate Shader Objects ARB
    extension, introduced in OpenGL 4.1, is used. The extension skips linking
    checks in Shader Programs and introduces Program Pipeline Object, which
    joins together multiple Shader Programs. One OGL Shader Program Object
    contains only one Shader Object. The names might be a bit misleading,
    in particular:
        * Shader Program from OGL is a part of our IShader interface.
        * Program Pipeline from OGL is a part of our IShaderProgram interface.
    
    With this commit the basic shader workflow is implemented - multiple
    IShaders are bound to one IShaderProgram, which is then set by Renderer's
    Command Buffer. Further functionalities will be developed in the future.
    
    Please note - shaders already have input/output layout specified. It will
    be used in next commit with Vertex Buffer Object implementation.
    
    Verification:
        Build, run nfRendererDemo with "nfRendererOGL4" argument. The Demo
        should launch on the only available Scene 0:0 with no errors.
    
    Change-Id: Ib55c07100d06e450326ecfe017b6055d02ad281c

---
+++ nfEngine/Renderers/nfRendererOGL4/Shader.hpp
@@ -0,0 +1,54 @@
+/**
+ * @file
+ * @author  LKostyra (costyrra.xl@gmail.com)
+ * @brief   OGL4 Shader declarations
+ */
+
+#include "../RendererInterface/Shader.hpp"
+#include "Defines.hpp"
+
+namespace NFE {
+namespace Renderer {
+
+class ShaderProgram: public IShaderProgram
+{
+    friend class CommandBuffer;
+
+    GLuint mProgramPipeline;
+
+public:
+    ShaderProgram();
+    ~ShaderProgram();
+    bool Init(const ShaderProgramDesc& desc);
+};
+
+class Shader: public IShader
+{
+    friend class CommandBuffer;
+    friend class ShaderProgram;
+
+    ShaderType mType;
+
+    /**
+     * The naming convention is a bit different in OpenGL with SSO, comparing to D3D.
+     *
+     * To implement SSO, OpenGL allows creation of a ShaderProgram with only one Shader attached.
+     * Thus, D3D's Shader is OGL's ShaderProgram, and the equivalent of D3D's ShaderProgram is a
+     * ProgramPipeline, newly added by SSO.
+     *
+     * Refer to ARB_separate_shader_objects description on opengl.org for more info about SSO.
+     */
+    GLuint mShaderProgram;
+
+public:
+    Shader();
+    ~Shader();
+    bool Init(const ShaderDesc& desc);
+    GLuint GetShaderProgram();
+
+    bool GetIODesc(ShaderIODesc& result);
+    bool Disassemble(bool html, std::string& output);
+};
+
+} // namespace Renderer
+} // namespace NFE
