9bb7257f2d3b9950fe1030dec8e889468df2819b
Michal Witanowski <witek902@gmail.com>
2015-08-17 18:23:43 +0200

    nfCommon: Refactor logger
    Changes:
    * Multiple logger backends support (for now, console and HTML backends
      are implemented).
    * Unlimitted log message length.
    * "Debug" log level.
    * PrintColored() function now accepts formatted string.
    * "Use Full Path" (/FC) flag has been added to all Visual Studio project files.
    
    Verification:
        Rebuild all. Run tests, verify Logs/log.html file.
        Perform verification both on Windows and Linux.
    
    Change-Id: Ife1088c36a42634ef80ac6a2b53d800833471957

---
+++ nfEngine/nfCommon/LoggerBackends/BackendHTML.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Declaration of HTML logger backend
+ */
+
+#pragma once
+
+#include "../Logger.hpp"
+#include "../File.hpp"
+
+namespace NFE {
+namespace Common {
+
+/**
+ * HTML logger backend implementation.
+ */
+class NFCOMMON_API LoggerBackendHTML : public LoggerBackend
+{
+    File mFile;
+    std::vector<char> mBuffer;
+
+public:
+    LoggerBackendHTML();
+    ~LoggerBackendHTML();
+    void Log(LogType type, const char* srcFile, int line, const char* str, double timeElapsed);
+};
+
+} // namespace Common
+} // namespace NFE
