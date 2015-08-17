7bfcaca3ffb09ca0a56224353e4ffdf45331ad10
Michal Witanowski <witek902@gmail.com>
2015-08-17 18:23:43 +0200

    nfCommon: Refactor logger
    Changes:
    * Multiple logger backends support (for now, console and HTML backends
      are implemented).
    * Unlimitted log message length.
    * "Debug" log level.
    * PrintColored() function now accepts formatted string.
    
    Verification:
        Rebuild all. Run tests, verify Logs/log.html file.
    
    Change-Id: Ife1088c36a42634ef80ac6a2b53d800833471957

---
+++ nfEngine/nfCommon/LoggerBackends/BackendConsole.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Declaration of console (standard output) logger backend
+ */
+
+#pragma once
+
+#include "../Logger.hpp"
+
+namespace NFE {
+namespace Common {
+
+/**
+ * Console logger backend implementation.
+ */
+class NFCOMMON_API LoggerBackendConsole : public LoggerBackend
+{
+public:
+    void Log(LogType type, const char* srcFile, int line, const char* str, double timeElapsed);
+};
+
+} // namespace Common
+} // namespace NFE
---
+++ nfEngine/nfCommonTest/LoggerTest.cpp
@@ -0,0 +1,23 @@
+#include "PCH.hpp"
+#include "../nfCommon/Logger.hpp"
+#include "../nfCommon/Timer.hpp"
+
+using namespace NFE::Common;
+
+TEST(LoggerTest, Simple)
+{
+    LOG_DEBUG("Test log - debug");
+    LOG_INFO("Test log - info");
+    LOG_SUCCESS("Test log - success");
+    LOG_WARNING("Test log - warning");
+    LOG_ERROR("Test log - error");
+    LOG_FATAL("Test log - fatal");
+}
+
+TEST(LoggerTest, Long)
+{
+    std::string longMessage;
+    for (int i = 0; i < 100; ++i)
+        longMessage += "This is a long message... ";
+    LOG_INFO(longMessage.c_str());
+}
