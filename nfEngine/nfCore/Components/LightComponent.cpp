e17b3d6f07a15d733bf827c9206a1331054c5393
Michal Witanowski <witek902@gmail.com>
2015-09-08 21:11:25 +0200

    nfCore: Support for spot lights
    * Shaders were added
    * LightsRenderer::DrawSpotLight() was filled
    * RendererSystem was reorganized - now, temporary data, such as light
      Frustum are hold in RendererSystem, LightComponents holds only
      light properties.
    
    Verification:
        Rebuild, run nfEngineTest, place some spot light with "T" key.
    
    Change-Id: Id407445958429f45293015badc493798cbea867e

--- nfEngine/nfCore/Components/LightComponent.cpp
+++ nfEngine/nfCore/Components/LightComponent.cpp
@@ -24,16 +24,41 @@ NFE_REGISTER_COMPONENT(LightComponent);
 
 LightComponent::LightComponent()
     : mColor(Vector(1.0f, 1.0f, 1.0f, 1.0f))
-    , mLightType(LightType::Unknown)
     , mShadowMap(nullptr)
     , mLightMap(nullptr)
-    , mDrawShadow(false)
+    , mLightType(LightType::Unknown)
 {
 }
 
 LightComponent::~LightComponent()
 {
     Release();
+}
+
+LightComponent::LightComponent(const LightComponent& other)
+{
+    mLightType = other.mLightType;
+    mColor = other.mColor;
+
+    switch (mLightType)
+    {
+    case LightType::Omni:
+        mOmniLight = other.mOmniLight;
+        break;
+    case LightType::Spot:
+        mSpotLight = other.mSpotLight;
+        break;
+    case LightType::Dir:
+        mDirLight = other.mDirLight;
+        break;
+    default:
+        LOG_ERROR("Invalid light type");
+    }
+
+    mShadowMap = nullptr;
+    mLightMap = other.mLightMap;
+    if (mLightMap)
+        mLightMap->AddRef();
 }
 
 void LightComponent::SetColor(const Float3& color)
@@ -122,17 +147,17 @@ Result LightComponent::SetShadowMap(uint32 resolution)
 
     switch (mLightType)
     {
-        case LightType::Omni:
-            mShadowMap->Resize(resolution, ShadowMap::Type::Cube);
-            break;
-
-        case LightType::Spot:
-            mShadowMap->Resize(resolution, ShadowMap::Type::Flat);
-            break;
-
-        case LightType::Dir:
-            mShadowMap->Resize(resolution, ShadowMap::Type::Cascaded, mDirLight.splits);
-            break;
+    case LightType::Omni:
+        mShadowMap->Resize(resolution, ShadowMap::Type::Cube);
+        break;
+    case LightType::Spot:
+        mShadowMap->Resize(resolution, ShadowMap::Type::Flat);
+        break;
+    case LightType::Dir:
+        mShadowMap->Resize(resolution, ShadowMap::Type::Cascaded, mDirLight.splits);
+        break;
+    default:
+        LOG_ERROR("Invalid light type");
     }
 
     return Result::OK;
@@ -158,149 +183,17 @@ void LightComponent::SetLightMap(const char* pName)
     }
 
     ResManager* rm = Engine::GetInstance()->GetResManager();
-    Texture* pNewTexture = static_cast<Texture*>(rm->GetResource(pName, ResourceType::Texture));
+    Texture* newTexture = static_cast<Texture*>(rm->GetResource(pName, ResourceType::Texture));
 
-    if (pNewTexture != mLightMap)
+    if (newTexture != mLightMap)
     {
-        if (mLightMap) mLightMap->DelRef();
-        pNewTexture->AddRef();
-        mLightMap = pNewTexture;
+        if (mLightMap)
+            mLightMap->DelRef();
+
+        mLightMap = newTexture;
+        if (mLightMap)
+            mLightMap->AddRef();
     }
-}
-
-void LightComponent::Update(CameraComponent* pCamera)
-{
-    /*
-    if (mLightType == LightType::Spot)
-    {
-        //set perspective projection
-        Perspective perspectiveDesc;
-        perspectiveDesc.aspectRatio = 1.0f;
-        perspectiveDesc.farDist = 100.0f;
-        perspectiveDesc.nearDist = 0.1f;
-        perspectiveDesc.FoV = mSpotLight.cutoff;
-        mCameras[0]->SetPerspective(&perspectiveDesc);
-
-        //set view the same as light
-        //  mCameras[0]->mOwner->mMatrix = mOwner->mMatrix;
-
-        //update camera
-        mCameras[0]->Update(0.0f);
-    }
-
-    if (mShadowMap && mShadowMap->GetSize() > 0)
-    {
-        if (mLightType == LightType::Omni)
-        {
-            // +X
-            mCameras[0]->mParentOffset.r[0] = Vector(0, 0, -1);
-            mCameras[0]->mParentOffset.r[1] = Vector(0, 1, 0);
-            mCameras[0]->mParentOffset.r[2] = Vector(1, 0, 0);
-
-            // -X
-            mCameras[1]->mParentOffset.r[0] = Vector(0, 0, 1);
-            mCameras[1]->mParentOffset.r[1] = Vector(0, 1, 0);
-            mCameras[1]->mParentOffset.r[2] = Vector(-1, 0, 0);
-
-            // +Y
-            mCameras[2]->mParentOffset.r[0] = Vector(1, 0, 0);
-            mCameras[2]->mParentOffset.r[1] = Vector(0, 0, -1);
-            mCameras[2]->mParentOffset.r[2] = Vector(0, 1, 0);
-
-            // -Y
-            mCameras[3]->mParentOffset.r[0] = Vector(1, 0, 0);
-            mCameras[3]->mParentOffset.r[1] = Vector(0, 0, 1);
-            mCameras[3]->mParentOffset.r[2] = Vector(0, -1, 0);
-
-
-            // +Z
-            mCameras[4]->mParentOffset.r[0] = Vector(1, 0, 0);
-            mCameras[4]->mParentOffset.r[1] = Vector(0, 1, 0);
-            mCameras[4]->mParentOffset.r[2] = Vector(0, 0, 1);
-
-            // -Z
-            mCameras[5]->mParentOffset.r[0] = Vector(-1, 0, 0);
-            mCameras[5]->mParentOffset.r[1] = Vector(0, 1, 0);
-            mCameras[5]->mParentOffset.r[2] = Vector(0, 0, -1);
-
-            // set perspective projection
-            Perspective perspectiveDesc;
-            perspectiveDesc.aspectRatio = 1.0f;
-            perspectiveDesc.farDist = mOmniLight.radius;
-            perspectiveDesc.nearDist = mOmniLight.radius * 0.001f;
-            perspectiveDesc.FoV = NFE_MATH_PI * 0.5f;
-
-            //update camera
-            for (int i = 0; i < 6; i++)
-            {
-                mCameras[i]->SetPerspective(&perspectiveDesc);
-                //mCameras[i]->mOwner->SetPosition(mOwner->GetPosition());
-                mCameras[i]->OnUpdate(0.0f);
-            }
-        }
-
-        if (mLightType == LightType::Dir)
-        {
-            Frustum frustum;
-
-            float camNear = 5.0f;
-            float camFar = mDirLight.farDist;
-            const float blend = 0.2f;
-            float linLogFactor = 0.8f;
-
-            float cascadeCount = (float)mDirLight.splits;
-            for (int32 i = 0; i < mDirLight.splits - 1; i++)
-            {
-                float logFactor = camNear * powf(camFar / camNear, ((float)i + blend) / cascadeCount);
-                float linFactor = camNear + (((float)i + blend) / cascadeCount) * (camFar - camNear);
-
-                mCascadeRanges[i].f[1] = Lerp(linFactor, logFactor, linLogFactor);
-            }
-
-            for (int32 i = 1; i < mDirLight.splits; i++)
-            {
-                float logFactor = camNear * powf(camFar / camNear, ((float)i - 1.0f - blend) / cascadeCount);
-                float linFactor = camNear + (((float)i - 1.0f - blend) / cascadeCount) * (camFar - camNear);
-
-                mCascadeRanges[i].f[0] = Lerp(linFactor, logFactor, linLogFactor);
-            }
-
-            mCascadeRanges[mDirLight.splits - 1].f[1] = camFar;
-            mCascadeRanges[0].f[0] = 0.0f;
-
-
-            for (int i = 0; i < mDirLight.splits; i++)
-            {
-                pCamera->SplitFrustum(mCascadeRanges[i].f[0], mCascadeRanges[i].f[1], &frustum);
-                Sphere boundingSphere(frustum.verticies[6], frustum.verticies[7],
-                                      frustum.verticies[5], frustum.verticies[3]);
-
-                Ortho ortho;
-                ortho.right =    boundingSphere.SupportVertex(   mOwner->mMatrix.GetRow(0));
-                ortho.left =     -boundingSphere.SupportVertex(  -mOwner->mMatrix.GetRow(0));
-                ortho.top =      boundingSphere.SupportVertex(   mOwner->mMatrix.GetRow(1));
-                ortho.bottom =  -boundingSphere.SupportVertex(  -mOwner->mMatrix.GetRow(1));
-                ortho.farDist =  boundingSphere.SupportVertex(   mOwner->mMatrix.GetRow(2));
-                ortho.nearDist = -boundingSphere.SupportVertex(  -mOwner->mMatrix.GetRow(2)) - 50.0f;
-
-
-                float worldUnitsPerTexel = 2.0f * boundingSphere.r / 1024.0f;
-                ortho.left = Quantize(ortho.left, worldUnitsPerTexel);
-                ortho.right = Quantize(ortho.right, worldUnitsPerTexel);
-                ortho.bottom = Quantize(ortho.bottom, worldUnitsPerTexel);
-                ortho.top = Quantize(ortho.top, worldUnitsPerTexel);
-                mCameras[i]->SetOrtho(&ortho);
-
-
-                Orientation orient;
-                mOwner->GetOrientation(&orient);
-                mCameras[i]->mOwner->SetOrientation(&orient);
-                mCameras[i]->mOwner->SetPosition(Vector());
-                mCameras[i]->OnUpdate(0.0f);
-            }
-        }
-    }
-    */
 }
 
 bool LightComponent::CanBeTiled()
--- nfEngine/nfCore/Systems/RendererSystem.hpp
+++ nfEngine/nfCore/Systems/RendererSystem.hpp
@@ -14,6 +14,16 @@
 namespace NFE {
 namespace Scene {
 
+/**
+ * Temporary data of active spot light.
+ */
+struct NFE_ALIGN16 SpotLightData
+{
+    Math::Frustum frustum;
+    Math::Matrix viewMatrix;
+    Math::Matrix projMatrix;
+};
+
 NFE_ALIGN16
 class RendererSystem : public Util::Aligned
 {
@@ -21,21 +31,26 @@ class RendererSystem : public Util::Aligned
     typedef std::tuple<TransformComponent*, LightComponent*> LightEntry;
     typedef std::tuple<TransformComponent*, CameraComponent*, BodyComponent*> CameraEntry;
 
+    std::vector<LightEntry> mOmniLights;
+    std::vector<LightEntry> mSpotLights;
+    std::vector<LightEntry> mDirLights;
+    std::vector<SpotLightData, Util::AlignedAllocator<SpotLightData, 16>> mSpotLightsData;
+
     std::vector<MeshEntry> mMeshes;
     std::vector<MeshEntry> mActiveMeshEntities;
-    std::vector<LightEntry> mLights;
     std::vector<CameraEntry> mCameras;
 
     SceneManager* mScene;
 
-    void RenderLight(Renderer::RenderContext* ctx, LightComponent* light, TransformComponent* transform);
-    void RenderLightDebug(Renderer::RenderContext* ctx, LightComponent* light, TransformComponent* transform);
+    void RenderLights(Renderer::RenderContext* ctx) const;
+    void RenderLightsDebug(Renderer::RenderContext* ctx) const;
 
     void RenderGBuffer(Renderer::RenderContext* ctx, CameraComponent* camera,
                        TransformComponent* cameraTransform);
     void RenderShadow(Renderer::RenderContext * ctx, LightComponent* pLight, uint32 faceID = 0);
     void FindActiveMeshEntities();
     void FindVisibleMeshEntities(const Math::Frustum & frustum, std::vector<MeshEntry>& list);
+    void UpdateLights();
 
 public:
     RendererSystem(SceneManager* scene);
