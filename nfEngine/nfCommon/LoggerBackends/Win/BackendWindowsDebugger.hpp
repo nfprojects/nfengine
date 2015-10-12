6104900a79dfd0b71d7beacdb0418fa8c2cb16bd
Michal Witanowski <witek902@gmail.com>
2015-10-12 19:52:12 +0200

    nfCommon: Add Windows debugger logger backend
    Now, when running on Windows under debugger, logger messages are
    also passed to WinAPI's OutputDebugString(). Moreover, log message
    is printed in a way, that it's "clickable" - double clicking on
    a messages jumps exactly to LOG_XXX() in the code.
    
    Verification:
        Rebuild all, run nfRendererDemo/nfEngineDemo/etc., verify
        Visual Studio debugger output.
    
    Change-Id: Ic8d9db8904ed9c5b1f59b9f0259a2da823eee59a

---
+++ nfEngine/nfCommon/LoggerBackends/Win/BackendWindowsDebugger.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Declaration of Windows debugger logger backend
+ */
+
+#pragma once
+
+#include "../../Logger.hpp"
+
+namespace NFE {
+namespace Common {
+
+/**
+ * Implementation of Windows debugger logger backend.
+ */
+class NFCOMMON_API LoggerBackendWinDebugger : public LoggerBackend
+{
+    std::vector<char> mBuffer;
+
+    std::string mDebugString;
+    std::wstring mWideDebugString;
+
+public:
+    LoggerBackendWinDebugger();
+    void Log(LogType type, const char* srcFile, int line, const char* str, double timeElapsed);
+};
+
+} // namespace Common
+} // namespace NFE
