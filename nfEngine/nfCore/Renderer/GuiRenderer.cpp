c055163da08edf8864f3adae945d29ce0ab9baa0
Michal Witanowski <witek902@gmail.com>
2015-10-18 20:20:12 +0200

    nfCore: Simple text printing.
    * New class "Font" has been introduced.
    * Printing functions in GuiRenderer have been added.
    * Vertical alignment support.
    * Time delta printing in nfEngineDemo.
    
    Verification:
        Rebuild nfEngineDemo, launch it and check if "dt = ..." is printed
        in top left corner.
    
    Change-Id: I86ea1b56b4506f1b0ca73b759ecff0d0eab264b3

--- nfEngine/nfCore/Renderer/GuiRenderer.cpp
+++ nfEngine/nfCore/Renderer/GuiRenderer.cpp
@@ -203,5 +203,104 @@ void GuiRenderer::DrawTexturedQuad(RenderContext* context, const Rectf& rect,
     PushQuad(context, GuiQuadData(texture, alpha), GuiQuadVertex(rect, texCoords, color));
 }
 
+bool GuiRenderer::PrintText(RenderContext* context, Font* font, const char* text,
+                            const Recti& rect, uint32 color,
+                            VerticalAlignment vAlign, HorizontalAlignment hAlign)
+{
+    if (!font || !font->mTexture)
+        return false;
+
+    const float interline = 1.5f;
+    float texInvWidth = 1.0f / (float)font->mTexWidth;
+    float texInvHeight = 1.0f / (float)font->mTexHeight;
+
+    int offsetX = rect.Xmin;
+    int offsetY = rect.Ymax;
+
+    int textWidth, textLines;
+    font->GetTextSize(text, textWidth, textLines);
+    int lineHeight = static_cast<int>(interline * font->mSize);
+
+    (void)hAlign; // TODO
+
+    switch (vAlign)
+    {
+    case VerticalAlignment::Top:
+        offsetY = rect.Ymax;
+        break;
+    case VerticalAlignment::Center:
+        offsetY = rect.Ymax - (rect.Ymax - rect.Ymin - textLines * lineHeight) / 2;
+        break;
+    case VerticalAlignment::Bottom:
+        offsetY = rect.Ymax - (rect.Ymax - rect.Ymin - textLines * lineHeight);
+        break;
+    default:
+        return false;
+    }
+    offsetY -= lineHeight;
+
+    int defaultOffsetX = offsetX;
+
+    GuiQuadData quadData(font->mTexture.get(), true);
+    for (int i = 0; text[i]; i++)
+    {
+        // TODO: UTF-8 support
+        size_t charId = static_cast<unsigned char>(text[i]);
+        if (charId > 127)
+            charId = '?';
+
+        if (charId == '\n')  // carriage return
+        {
+            offsetY -= lineHeight;
+            offsetX = defaultOffsetX;
+            continue;
+        }
+
+        const CharacterInfo& charInfo = font->mChars[charId];
+        if (charInfo.width < 0)
+            continue;
+
+        Rectf quadRect;
+        quadRect.Xmin = static_cast<float>(offsetX + charInfo.left);
+        quadRect.Xmax = static_cast<float>(quadRect.Xmin + charInfo.width);
+        quadRect.Ymin = static_cast<float>(offsetY + charInfo.top - charInfo.height);
+        quadRect.Ymax = static_cast<float>(quadRect.Ymin + charInfo.height);
+
+        Rectf texRect;
+        texRect.Xmin = static_cast<float>(charInfo.u) * texInvWidth;
+        texRect.Ymax = static_cast<float>(charInfo.v) * texInvHeight;
+        texRect.Xmax = static_cast<float>(charInfo.u + charInfo.width) * texInvWidth;
+        texRect.Ymin = static_cast<float>(charInfo.v + charInfo.height) * texInvHeight;
+
+        PushQuad(context, quadData, GuiQuadVertex(quadRect, texRect, color));
+        offsetX += charInfo.spacing;
+    }
+
+    return true;
+}
+
+bool GuiRenderer::PrintTextWithBorder(RenderContext* context, Font* font, const char* text,
+                                      const Recti& rect, uint32 color, uint32 borderColor,
+                                      VerticalAlignment vAlign, HorizontalAlignment hAlign)
+{
+    Recti tempRect;
+    for (int i = -1; i <= 1; ++i)
+    {
+        for (int j = -1; j <= 1; ++j)
+        {
+            if (i == 0 && j == 0)
+                continue;
+
+            tempRect.Ymin = rect.Ymin + j;
+            tempRect.Ymax = rect.Ymax + j;
+            tempRect.Xmin = rect.Xmin + i;
+            tempRect.Xmax = rect.Xmax + i;
+            PrintText(context, font, text, tempRect, borderColor, vAlign, hAlign);
+        }
+    }
+
+    return PrintText(context, font, text, rect, color, vAlign, hAlign);
+}
+
 } // namespace Renderer
 } // namespace NFE
