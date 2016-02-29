8270a3a4726cfeaf1256e3e983493b7d8cacb59a
Michal Witanowski <witek902@gmail.com>
2016-02-29 18:18:58 +0100

    Move Aligned.cpp from nfCore/Utils to nfCommon
    * Tests for Aligned and AlignedAllocator have been added.
    
    Change-Id: I42da2094691d6e792078ffea6848c26ab3a8f2db
    Verification: Rebuild all, run tests and nfEngineDemo.

---
+++ nfEngine/nfCommonTest/AlignedTest.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Unit tests for Aligned and AlignedAllocator classes.
+ */
+
+#include "PCH.hpp"
+#include "../nfCommon/Aligned.hpp"
+
+using namespace NFE::Common;
+
+namespace {
+
+class TestClass16 : public Aligned<16>
+{
+    int a;
+};
+
+class TestClass64 : public Aligned<64>
+{
+    int a;
+};
+
+class TestClass4096 : public Aligned<4096>
+{
+    int a;
+};
+
+} // namespace
+
+TEST(Aligned, AlignedObject)
+{
+    std::unique_ptr<TestClass16> testObject16(new TestClass16);
+    std::unique_ptr<TestClass64> testObject64(new TestClass64);
+    std::unique_ptr<TestClass4096> testObject4096(new TestClass4096);
+
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject16.get()) % 16);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject64.get()) % 64);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject4096.get()) % 4096);
+}
+
+TEST(Aligned, AlignedArray)
+{
+    const size_t ARRAY_SIZE = 10;
+
+    std::unique_ptr<TestClass16[]> testArray16(new TestClass16[ARRAY_SIZE]);
+    std::unique_ptr<TestClass64[]> testArray64(new TestClass64[ARRAY_SIZE]);
+    std::unique_ptr<TestClass4096[]> testArray4096(new TestClass4096[ARRAY_SIZE]);
+
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testArray16.get()) % 16);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testArray64.get()) % 64);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testArray4096.get()) % 4096);
+}
+
+TEST(Aligned, AlignedContainer)
+{
+    std::vector<int, AlignedAllocator<int, 16>> vector16;
+    std::vector<int, AlignedAllocator<int, 64>> vector64;
+    std::vector<int, AlignedAllocator<int, 4096>> vector4096;
+
+    vector16.push_back(1);
+    vector64.push_back(2);
+    vector4096.push_back(3);
+
+    EXPECT_EQ(0, reinterpret_cast<size_t>(vector16.data()) % 16);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(vector64.data()) % 64);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(vector4096.data()) % 4096);
+}
