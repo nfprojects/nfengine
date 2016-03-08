a9051f7020d5c3e0eced996c2ea6bb963e6971d8
Michal Witanowski <witek902@gmail.com>
2016-03-08 23:07:42 +0100

    nfCommon: Optimize BVH
    * Added BVH tree rebalancing.
    * BVHNode structure was optimized so it takes less memory.
    * Querying was simplified - there is only now one template method -
      IntersectEx(Box, <Shape>) function must be implemented for query shape.
    * Added prefetching in BVH query.
    
    Verification: Run nfCommonTest and nfCommonPerfTest.
    
    Change-Id: I9d6bd5af2864e04a2188d6e62464813da1fcbbc3

--- nfEngine/nfCommon/Math/SSE/GeometrySSE.cpp
+++ nfEngine/nfCommon/Math/SSE/GeometrySSE.cpp
@@ -31,6 +31,22 @@ bool Intersect(const Box& box, const Vector& point)
     return (_mm_movemask_ps(_mm_and_ps(cmpMax, cmpMin)) & 7) == 7;
 }
 
+template<> NFCOMMON_API
+IntersectionResult IntersectEx(const Box& box1, const Box& box2)
+{
+    Vector temp0 = _mm_cmpge_ps(box1.min, box2.max);
+    Vector temp1 = _mm_cmpge_ps(box2.min, box1.max);
+    if (_mm_movemask_ps(_mm_or_ps(temp0, temp1)) & 7)
+        return IntersectionResult::Outside;
+
+    temp0 = _mm_cmpge_ps(box1.min, box2.min);
+    temp1 = _mm_cmpge_ps(box2.max, box1.max);
+    if ((_mm_movemask_ps(_mm_and_ps(temp0, temp1)) & 7) == 7)
+        return IntersectionResult::Inside;
+
+    return IntersectionResult::Intersect;
+}
+
 } // namespace Math
 } // namespace NFE
 
