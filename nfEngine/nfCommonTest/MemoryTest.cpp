e6abd3e49890c57b7c9843855e1329d43cb1bf00
Michal Witanowski <witek902@gmail.com>
2016-03-10 18:36:54 +0100

    nfCommon: Change MemoryCheck function
    * Function MemoryCheck now only checks if a memory region can be read
    * Linux version has been implemented
    * Tests have been improved
    
    Verification: Run tests and nfEngineDemo.
    
    Change-Id: I40f5a76187db2a9abc8b42bc053b003be0f04317

--- nfEngine/nfCommonTest/MemoryTest.cpp
+++ nfEngine/nfCommonTest/MemoryTest.cpp
@@ -3,17 +3,25 @@
 
 using namespace NFE::Common;
 
-TEST(MemoryTest, AccessReadWrite)
+TEST(MemoryTest, AccessValid)
 {
-    int* x = new int;
-    uint32 resultAccess = MemoryCheck(x);
-    ASSERT_EQ(ACCESS_READWRITE, resultAccess);
-    delete x;
+    const size_t ARRAY_SIZE = 4096;
+
+    std::unique_ptr<char[]> array(new char [ARRAY_SIZE]);
+    std::unique_ptr<char> singleValue(new char);
+    char arrayOnStack[ARRAY_SIZE];
+    char singleValueOnStack;
+
+    EXPECT_TRUE(MemoryCheck(array.get(), ARRAY_SIZE));
+    EXPECT_TRUE(MemoryCheck(singleValue.get(), 1));
+    EXPECT_TRUE(MemoryCheck(arrayOnStack, ARRAY_SIZE));
+    EXPECT_TRUE(MemoryCheck(&singleValueOnStack));
 }
 
-TEST(MemoryTest, AccessNone)
+TEST(MemoryTest, AccessInvalid)
 {
     int* x = nullptr;
-    uint32 resultAccess = MemoryCheck(x);
-    ASSERT_EQ(0, resultAccess);
+    int validValue = 123;
+    EXPECT_FALSE(MemoryCheck(x));
+    EXPECT_FALSE(MemoryCheck(&validValue, 1024 * 1024 * 64)); // stack won't have 64MB
 }
