ee24aa34e52cdf0c842fa08e2e0932a2b3813602
Michal Witanowski <witek902@gmail.com>
2016-04-14 23:08:15 +0200

    nfRendererDemo: GPU information logging at renderer init
    Verification: Run nfRendererDemo with D3D11 backend and find
                  GPU info in logs.
    
    Change-Id: I0927a35b8102da260be853de503522401f6320f7

--- nfEngine/nfRendererDemo/Main.cpp
+++ nfEngine/nfRendererDemo/Main.cpp
@@ -18,6 +18,7 @@
 #include "../nfCommon/FileSystem.hpp"
 #include "../nfCommon/Window.hpp"
 #include "../nfCommon/Timer.hpp"
+#include "../nfCommon/Logger.hpp"
 
 #include <algorithm>
 
@@ -141,6 +142,24 @@ public:
         mRendererDevice = proc();
         if (mRendererDevice == nullptr)
             return false;
+
+        DeviceInfo deviceInfo;
+        if (!mRendererDevice->GetDeviceInfo(deviceInfo))
+            LOG_ERROR("Failed to get rendering device information");
+        else
+        {
+            LOG_INFO("GPU name: %s", deviceInfo.description.c_str());
+            LOG_INFO("GPU info: %s", deviceInfo.misc.c_str());
+
+            std::string features;
+            for (size_t i = 0; i < deviceInfo.features.size(); ++i)
+            {
+                if (i > 0)
+                    features += ", ";
+                features += deviceInfo.features[i];
+            }
+            LOG_INFO("GPU features: %s", features.c_str());
+        }
 
         return true;
     }
