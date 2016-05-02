aa5c19f4ea69ec19c2ee158cf5f4e31fa1adf973
mkkulagowski <mk.kulagowski@gmail.com>
2016-05-02 14:54:03 +0200

    nfCommon: Introduce XML logger backend
    Introduces XML logger backend.
    
    Verification: Build and run tests, then check Logs directory for file
    log.xml. It should be a valid XML file.
    
    Change-Id: If624e15b35a9922f2cd8b401eae7f554a65ac455

--- nfEngine/nfCommon/LoggerBackends/BackendTxt.cpp
+++ nfEngine/nfCommon/LoggerBackends/BackendTxt.cpp
@@ -7,15 +7,7 @@
 #include "../PCH.hpp"
 #include "BackendTxt.hpp"
 #include "../Logger.hpp"
-
-#define NFE_MAX_LOG_MESSAGE_LENGTH 1024
-
-/// snprintf was not implemented until Visual Studio 2015
-#if defined(WIN32) && defined(_MSC_VER)
-#if _MSC_VER <= 1800
-#define snprintf sprintf_s
-#endif
-#endif
+#include "BackendCommon.hpp"
 
 namespace NFE {
 namespace Common {
@@ -35,8 +27,7 @@ void LoggerBackendTxt::Reset()
                                          "[SourceFile]:[LineOfCode]: [Message]\n";
 
     const std::string logFileName = "log.txt";
-    const size_t initialBufferSize = 1024;
-    mBuffer.resize(initialBufferSize);
+    mBuffer.resize(NFE_MAX_LOG_MESSAGE_LENGTH);
 
     const std::string logFilePath = Logger::GetInstance()->GetLogsDirectory() + '/' + logFileName;
     if (!mFile.Open(logFilePath, AccessMode::Write, true))
