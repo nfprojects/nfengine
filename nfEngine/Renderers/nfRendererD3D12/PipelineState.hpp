0879c0792316645ba68b55b21ce5028ca22126f4
Michal Witanowski <witek902@gmail.com>
2016-05-06 22:39:18 +0200

    nfRendererD3D12: Shader resources binding
    * Extracting info about shader resource bindings on shader compilation.
    * Creation of root signature in ResourceBindingLayout object.
    * Simple static descriptor heap allocation.
    * Added missing support for CBuffer creation and updating.
    * A helper class  HeapAllocator  has been introduced to help descriptor
      heaps management.
    
    Verification:
        Run nfRendererDemo with D3D12 backend. First scene and 4 first
        subscenes (with CBuffer subscene) should work.
    
    Change-Id: I3a9f26d7bdd0d119cdfc56860adbae3651c74008

--- nfEngine/Renderers/nfRendererD3D12/PipelineState.hpp
+++ nfEngine/Renderers/nfRendererD3D12/PipelineState.hpp
@@ -8,6 +8,7 @@
 
 #include "../RendererInterface/PipelineState.hpp"
 #include "Common.hpp"
+#include "ResourceBinding.hpp"
 
 namespace NFE {
 namespace Renderer {
@@ -17,11 +18,8 @@ typedef std::tuple<IPipelineState*, IShaderProgram*> FullPipelineStateParts;
 class PipelineState : public IPipelineState
 {
     friend class CommandBuffer;
-    friend class FullPipelineState;
 
-    // TODO this should be obtained from separate structure describing resource bindings
-    D3DPtr<ID3D12RootSignature> mRootSignature;
-
+    ResourceBindingLayout* mBindingLayout;
     D3D12_INPUT_LAYOUT_DESC mInputLayoutDesc;
     D3D12_RASTERIZER_DESC mRasterizerDesc;
     D3D12_DEPTH_STENCIL_DESC mDepthStencilDesc;
@@ -31,18 +29,10 @@ class PipelineState : public IPipelineState
     uint32 mNumControlPoints;
 
 public:
+    ~PipelineState();
     bool Init(const PipelineStateDesc& desc);
-};
 
-class FullPipelineState
-{
-    friend class CommandBuffer;
-    friend class Device;
-
-    D3DPtr<ID3D12PipelineState> mPipelineState;
-
-public:
-    bool Init(const FullPipelineStateParts& parts);
+    static ID3D12PipelineState* CreateFullPipelineState(const FullPipelineStateParts& parts);
 };
 
 } // namespace Renderer
