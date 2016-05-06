7e2fe28b370471adbc73b866899fe9ec8b448f03
mkkulagowski <mk.kulagowski@gmail.com>
2016-05-06 14:52:29 +0200

    nfCommon: Performance tests update
    * Introduces new CommonPerfTest class, that enables logging to file and
    console with a streaming operator. It's a base class for all performance
    test classes.
    * Adds performance tests for Logger module.
    
    Verification: Run nfCommonPerfTest and check files in Logs/PerfTests.
    
    Change-Id: I84a7c5bac936df9cbcf7554a5837e7c1c1842339

--- nfEngine/nfCommonPerfTest/Main.cpp
+++ nfEngine/nfCommonPerfTest/Main.cpp
@@ -5,9 +5,17 @@
  */
 
 #include "PCH.hpp"
+#include "FileSystem.hpp"
 
 int main(int argc, char* argv[])
 {
     testing::InitGoogleTest(&argc, argv);
+
+    std::string execPath = NFE::Common::FileSystem::GetExecutablePath();
+    std::string execDir = NFE::Common::FileSystem::GetParentDir(execPath);
+    NFE::Common::FileSystem::ChangeDirectory(execDir + "/../../../Logs");
+    NFE::Common::FileSystem::CreateDir("PerfTests");
+    NFE::Common::FileSystem::ChangeDirectory("PerfTests");
+
     return RUN_ALL_TESTS();
 }
--- nfEngine/nfCommonPerfTest/Test.hpp
+++ nfEngine/nfCommonPerfTest/Test.hpp
@@ -7,3 +7,79 @@
 #pragma once
 
 #include <string>
+#include <typeinfo>
+#include "../nfCommon/Logger.hpp"
+
+namespace
+{
+std::string getTime()
+{
+    time_t rawTime;
+    time(&rawTime);
+    struct tm* timeInfo = localtime(&rawTime);
+    if (timeInfo != nullptr)
+    {
+        /// print current date and time, in format
+        const int MAX_DATE_LENGTH = 32;
+        char dateTimeStr[MAX_DATE_LENGTH];
+        strftime(dateTimeStr, MAX_DATE_LENGTH, "%m-%d-%Y_%H.%M.%S", timeInfo);
+        return std::string(dateTimeStr);
+    }
+    return "";
+}
+} // namespace
+
+class CommonPerfTest : public testing::Test
+{
+public:
+    std::fstream mFile;
+
+    void SetUp()
+    {
+        static const std::string gLogFilePath = getName() + "_"
+            + ::getTime() + ".txt";
+        mFile.open(gLogFilePath, std::fstream::out | std::fstream::app);
+    }
+
+    void TearDown()
+    {
+        if (mFile.is_open())
+            mFile.close();
+
+        NFE::Common::Logger::GetInstance()->Reset();
+    }
+
+    void TestBody()
+    {
+    }
+
+    virtual std::string getName()
+    {
+        return std::string("CommonPerfTest");
+    }
+
+    CommonPerfTest& castBase()
+    {
+        return *reinterpret_cast<CommonPerfTest*>(this);
+    }
+
+    template<typename T>
+    CommonPerfTest& operator<<(T obj)
+    {
+        std::cout << obj;
+        if (mFile.is_open())
+            mFile << obj;
+
+        return castBase();
+    }
+
+    // Overload to resolve std::endl's template type
+    CommonPerfTest& operator<<(std::ostream&(*pManip)(std::ostream&))
+    {
+        std::cout << (*pManip);
+        if (mFile.is_open())
+            mFile << (*pManip);
+
+        return castBase();
+    }
+};
