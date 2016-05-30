26bc93a8bf5e5265678b2e5b69c6494171c33738
mkkulagowski <mk.kulagowski@gmail.com>
2016-05-30 15:34:38 +0200

    nfCommon: Implement enum iterator class
    Adds EnumIterator template class that exposes begin() and end() methods of
    EnumIterator<*some enum class*>(), along with ++ and * operators, so that
    range-based for is able to iterate over all enum class values and !=/==
    operators for testing.
    
    Simple test to nfCommonTest has been added as well.
    
    Verification: Build and run tests.
    
    Change-Id: I4545de9950b73f1dae8fec5ea0319e317cbc1e18

---
+++ nfEngine/nfCommonTest/EnumIteratorTest.cpp
@@ -0,0 +1,72 @@
+#include "PCH.hpp"
+#include "../nfCommon/EnumIterator.hpp"
+#include <iostream>
+
+using namespace NFE::Common;
+
+
+namespace {
+
+enum class testEnum
+{
+    None,
+    value1,
+    value2,
+    value3,
+    value4,
+    value5,
+
+    First = value1,
+    Last = value4,
+};
+
+const char* testEnumToStr(testEnum obj)
+{
+    switch (obj)
+    {
+        case testEnum::value1:
+            return "val1";
+            break;
+        case testEnum::value2:
+            return "val2";
+            break;
+        case testEnum::value3:
+            return "val3";
+            break;
+        case testEnum::value4:
+            return "val4";
+            break;
+        case testEnum::value5:
+            return "val5";
+            break;
+        default:
+            return "None";
+    }
+}
+
+} //namespace
+
+TEST(EnumIteratorTest, SimpleTest)
+{
+    auto iter = EnumIterator<testEnum>().begin();
+    auto endIter = EnumIterator<testEnum>().end();
+
+    ASSERT_EQ(*iter, testEnum::value1);
+    ASSERT_EQ(iter, EnumIterator<testEnum>().begin());
+    ASSERT_NE(iter, endIter);
+
+    ++iter;
+    ASSERT_EQ(*iter, testEnum::value2);
+    ASSERT_NE(iter, endIter);
+
+    ++iter;
+    ASSERT_EQ(*iter, testEnum::value3);
+    ASSERT_NE(iter, endIter);
+
+    ++iter;
+    ASSERT_EQ(*iter, testEnum::value4);
+    ASSERT_NE(iter, endIter);
+
+    ++iter;
+    ASSERT_EQ(iter, endIter);
+}
