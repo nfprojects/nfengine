b6c7ada2671da8554ae6c0b5b1dd0ae2c0da8465
mkkulagowski <mk.kulagowski@gmail.com>
2016-05-30 15:34:38 +0200

    nfCommon: Implement enum iterator class
    Adds EnumIterator template class that exposes begin() and end() methods of
    EnumIterator<*some enum class*>(), along with ++ and * operators, so that
    range-based for is able to iterate over all enum class values and !=/==
    operators for testing.
    
    Few SFINAE templates were added for static_asserts, to ensure that using
    wrong type of object or even incorrectly designed enum would result in
    easily readable compile-time errors.
    
    Simple test to nfCommonTest has been added as well.
    
    Verification: Build and run tests, also try to compile code while using:
                  * not-enum type;
                  * enum type without First element;
                  * enum type without Last element;
                  * enum type with First and Last elements in the wrong order.
    
    Change-Id: I4545de9950b73f1dae8fec5ea0319e317cbc1e18

---
+++ nfEngine/nfCommon/EnumIterator.hpp
@@ -0,0 +1,131 @@
+/**
+ * @file
+ * @author Mkkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  EnumIterator template class definition.
+ */
+
+#pragma once
+
+namespace {
+
+/**
+ * SFINAE struct to assure, that used enum contains 'First' value
+ */
+template<typename T>
+struct contains_First
+{
+    typedef char yes[1];
+    typedef char no [2];
+
+    template<typename U> static yes& test(decltype(U::First)*);
+    template<typename U> static no&  test(...);
+
+    static bool const value = sizeof(test<T>(0)) == sizeof(yes);
+};
+
+/**
+ * SFINAE struct to assure, that used enum contains 'Last' value
+ */
+template<typename T>
+struct contains_Last
+{
+    typedef char yes[1];
+    typedef char no [2];
+
+    template<typename U> static yes& test(decltype(U::Last)*);
+    template<typename U> static no&  test(...);
+
+    static bool const value = sizeof(test<T>(0)) == sizeof(yes);
+};
+
+/**
+ * SFINAE functions to assure, that 'First' and 'Last' enum values are in the right order.
+ * If enum does not contain either 'First' or 'Last' value - returns true, to hide errors.
+ */
+template <typename T>
+typename std::enable_if<contains_First<T>::value && contains_Last<T>::value, bool>::type
+constexpr right_FL_order()
+{
+    return T::First <= T::Last;
+};
+
+template <typename T>
+typename std::enable_if<!(contains_First<T>::value && contains_Last<T>::value), bool>::type
+constexpr right_FL_order()
+{
+    return true;
+};
+
+} // namespace
+
+namespace NFE {
+namespace Common {
+
+/**
+ * Helper class to allow iteration through enum via ranged-based for loops
+ * Enums should contain First and Last elements that points to chosen values, like so:
+ * enum class Enum {
+ *    None,
+ *    value_1,
+ *    value_2,
+ *    ...,
+ *    value_n,
+ *
+ *    First = value_1,
+ *    Last = value_n };
+ */
+template<typename T>
+class EnumIterator
+{
+    // Asserts to assure, that T is an enumeration with First and Last elements in the right order
+    static_assert(std::is_enum<T>::value,     "Type must be an enumeration");
+    static_assert(::contains_First<T>::value, "Enumeration must contain an element called First");
+    static_assert(::contains_Last<T>::value,  "Enumeration must contain an element called Last");
+    static_assert(::right_FL_order<T>(),         "'First' element cannot be greater then the 'Last'");
+
+public:
+    class Iterator
+    {
+    public:
+        Iterator(int value)
+            : mValue(value)
+        {
+        }
+
+        T operator*() const
+        {
+            return static_cast<T>(mValue);
+        }
+
+        void operator++()
+        {
+            ++mValue;
+        }
+
+        bool operator!=(const Iterator& rhs) const
+        {
+            return mValue != rhs.mValue;
+        }
+
+        bool operator==(const Iterator& rhs) const
+        {
+            return mValue == rhs.mValue;
+        }
+
+    private:
+        int mValue;
+    };
+
+    typename EnumIterator<T>::Iterator begin() const
+    {
+        return typename EnumIterator<T>::Iterator(static_cast<int>(T::First));
+    }
+
+    typename EnumIterator<T>::Iterator end() const
+    {
+        return typename EnumIterator<T>::Iterator((static_cast<int>(T::Last)) + 1);
+    }
+};
+
+} // namespace Common
+} // namespace NFE
