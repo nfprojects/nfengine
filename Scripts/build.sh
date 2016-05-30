47c2a0ec6bd56545291d71eefec4b3d68569dbb2
Lukasz Kostyra <costyrra.xl@gmail.com>
2016-05-30 12:30:23 +0200

    [Lnx] Scripts: Rework building scripts
    As a small improvement to Scripts, this commit moves code from
    rebuild-all.sh to one generic build.sh script. This will have some
    benefits in the future:
    * Easier to trigger single configuration build
    * Easier to add more configurations (if new will appear)
    * Easier to switch between configurations
    
    NOTE: To make rebuild-all.sh work correctly, build.sh DOES NOT clean
    previous build results (only CMake files are removed to re-create them
    in new configuration). On the other hand, rebuild-all.sh's behavior
    is unmodified.
    
    NOTE2: Building method on Linux has changed to recommended out-of-source
    build. Build scripts now produce build-<target> directory.
    
    Verification:
        * Scripts/rebuild-all.sh should work as before.
        * Scripts/build.sh can be ran without an argument, or with build
          type arg (Debug/Release), which will be supplied to CMake.
    
    Change-Id: I61ef9ea1c689967f7d7664365469af103857bc84

---
+++ Scripts/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CPU_COUNT=`grep -c ^processor /proc/cpuinfo`
+echo "Available CPU cores: ${CPU_COUNT}"
+echo
+
+if [ "$#" -gt 1 ];
+then
+    echo "Incorrect number of parameters."
+    echo "Usage: build.sh [BUILD_TYPE]"
+    echo
+    echo "Where:"
+    echo "    BUILD_TYPE - type of build (debug/release)"
+    echo
+fi
+
+if [ "$#" -eq 0 ];
+then
+    echo "No BUILD_TYPE argument - assuming Release build"
+    BUILD_TYPE="Release"
+else
+    BUILD_TYPE=$1
+fi
+
+
+pushd . > /dev/null
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}/..
+echo -n "Current directory is "; pwd
+
+echo
+
+# ensure case-sensitiveness
+BUILD_TYPE="$(tr '[:lower:]' '[:upper:]' <<< ${BUILD_TYPE:0:1})${BUILD_TYPE:1}"
+
+mkdir -p build-$BUILD_TYPE
+
+# Build with given configuration
+cd build-$BUILD_TYPE
+cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+make -j ${CPU_COUNT}
+echo
+
+popd > /dev/null
--- Scripts/clean.sh
+++ Scripts/clean.sh
@@ -21,8 +21,8 @@ rm -rf ShaderCache
 rm -rf ShaderCache_Debug
 echo "DONE"
 
-echo -n "Removing CMake files... "
-Scripts/clean-cmake.sh > /dev/null
+echo -n "Removing build-produced files... "
+rm -rf build-*
 echo "DONE"
 
 echo -n "Removing other files... "
