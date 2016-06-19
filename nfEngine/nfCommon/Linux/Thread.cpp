371dca3e0ce0b775be277147ab6f8fedcdfa2ea9
mkkulagowski <mk.kulagowski@gmail.com>
2016-06-19 02:14:47 +0200

    nfCommon: Implement Thread helper class
    Implements Thread helper class used for getting threads Id, setting
    threads priority and getting threads debug name.
    
    Verification: Build and run tests.
    
    Change-Id: I0291cd8cbcd38f59cf951e9af0ba6d4f3048f41b

---
+++ nfEngine/nfCommon/Linux/Thread.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Thread class definition.
+ */
+
+#include "PCH.hpp"
+#include "../Thread.hpp"
+#include <thread>
+#include <pthread.h>
+
+
+namespace NFE {
+namespace Common {
+
+thread_local const unsigned int threadId = static_cast<unsigned int>(pthread_self());
+
+bool Thread::SetThreadPriority(std::thread& thread, ThreadPriority priority)
+{
+    sched_param schParam;
+    schParam.sched_priority = static_cast<int>(priority);
+    auto res = pthread_setschedparam(threads.native_handle(), SCHED_RR, &schParam);
+    return (res == 0) ? true : false;
+}
+
+bool Thread::SetCurrentThreadName(const char* name)
+{
+	if (std::char_traits<char>::length(name) < 16)
+		if(pthread_setname_np(static_cast<pthread_t>(threadId), name) == 0);
+			return true;
+
+	return false;
+}
+
+} // namespace Common
+} // namespace NFE
