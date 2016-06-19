4552ece7a11226fd7bbf6a24cc441c5789a0af5d
mkkulagowski <mk.kulagowski@gmail.com>
2016-06-19 02:14:47 +0200

    nfCommon: Implement Thread helper class
    Implements Thread helper class used for getting threads Id, setting
    threads priority and getting threads debug name.
    
    Verification: Build and run nfCommonTest.
    
    Change-Id: I0291cd8cbcd38f59cf951e9af0ba6d4f3048f41b

---
+++ nfEngine/nfCommon/Thread.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Thread class declaration.
+ */
+
+#pragma once
+#include "nfCommon.hpp"
+
+namespace NFE {
+namespace Common {
+
+#if defined(__LINUX__) | defined(__linux__)
+enum class ThreadPriority
+{
+    Idle = 1,
+    Lowest = 16,
+    BelowNormal = 33,
+    Normal = 50,
+    AboveNormal = 66,
+    Highest = 83,
+    RealTime = 99,
+};
+#elif defined(WIN32)
+enum class ThreadPriority
+{
+    Idle = THREAD_PRIORITY_IDLE,
+    Lowest = THREAD_PRIORITY_LOWEST,
+    BelowNormal = THREAD_PRIORITY_BELOW_NORMAL,
+    Normal = THREAD_PRIORITY_NORMAL,
+    AboveNormal = THREAD_PRIORITY_ABOVE_NORMAL,
+    Highest = THREAD_PRIORITY_HIGHEST,
+    RealTime = THREAD_PRIORITY_TIME_CRITICAL,
+};
+#endif
+
+// Threads' id (implementation per thread)
+extern thread_local const unsigned int threadId;
+
+/**
+ * Helper methods for thread interaction
+ */
+class NFCOMMON_API Thread final
+{
+public:
+    static bool SetThreadPriority(std::thread& thread, ThreadPriority priority);
+    static bool SetCurrentThreadName(const char* name);
+
+    NFE_INLINE static const unsigned int GetCurrentThreadId()
+    {
+        return threadId;
+    }
+};
+
+} // namespace Common
+} // namespace NFE
---
+++ nfEngine/nfCommon/Win/Thread.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Thread class definition.
+ */
+
+#include "../PCH.hpp"
+#include "../Thread.hpp"
+#include <thread>
+
+
+namespace
+{
+
+const DWORD MS_VC_EXCEPTION = 0x406D1388;
+
+struct ThreadnameInfo
+{
+    DWORD dwType;     // Must be 0x1000.
+    LPCSTR szName;    // Pointer to name (in user addr space).
+    DWORD dwThreadID; // Thread ID (-1=caller thread).
+    DWORD dwFlags;    // Reserved for future use, must be zero.
+};
+
+} // namespace
+
+namespace NFE {
+namespace Common {
+
+thread_local const unsigned int threadId = ::GetCurrentThreadId();
+
+bool Thread::SetThreadPriority(std::thread& thread, ThreadPriority priority)
+{
+    auto result = ::SetThreadPriority(thread.native_handle(), static_cast<int>(priority));
+    return (result != 0);
+}
+
+bool Thread::SetCurrentThreadName(const char* name)
+{
+    ::ThreadnameInfo info;
+    info.dwType = 0x1000;
+    info.szName = name;
+    info.dwThreadID = static_cast<DWORD>(-1);
+    info.dwFlags = 0;
+
+    __try
+    {
+        RaiseException(::MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR),
+                       reinterpret_cast<ULONG_PTR*>(&info));
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER)
+    {
+    }
+
+    return true;
+}
+
+} // namespace Common
+} // namespace NFE
