d82fbb27eacbce3a92dbf62fcbcf4378d51bc9e5
mkkulagowski <mk.kulagowski@gmail.com>
2016-06-19 02:14:47 +0200

    nfCommon: Implement Thread helper class
    Implements Thread helper class used for getting threads Id, setting
    threads priority and getting threads debug name.
    
    Verification: Build and run tests.
    
    Change-Id: I0291cd8cbcd38f59cf951e9af0ba6d4f3048f41b

---
+++ nfEngine/nfCommonTest/ThreadTest.cpp
@@ -0,0 +1,101 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Unit tests for Thread class.
+ */
+
+#include "PCH.hpp"
+#include "../nfCommon/Thread.hpp"
+#include <algorithm>
+#include <array>
+
+using namespace NFE::Common;
+
+// Global variables for the tests
+const int testThreadNumber = 100;
+
+// Simple function to test Thread::SetPriority
+void simpleFunc()
+{
+    size_t size = 10000;
+    const char value = 123;
+
+    std::vector<char> vectDup(size, value);
+
+    std::unique(vectDup.begin(), vectDup.end());
+};
+
+// Simple function to test differences in thread Id
+void idTestFunc(unsigned int* id)
+{
+    *id = Thread::GetCurrentThreadId();
+};
+
+// Simple function that checks if thread name is constructed as expected
+void nameTestFunc()
+{
+    const std::string name = Thread::GetCurrentThreadName();
+    const unsigned int id = Thread::GetCurrentThreadId();
+
+    const std::string expectedName = 'T' + std::to_string(id);
+
+    ASSERT_EQ(expectedName, name);
+};
+
+
+TEST(ThreadTest, SetPriority)
+{
+    auto threadTestLambda = [](ThreadPriority pr) {
+        std::thread testThread(nameTestFunc);
+        ASSERT_TRUE(Thread::SetThreadPriority(testThread, pr));
+        testThread.join();
+    };
+
+    threadTestLambda(ThreadPriority::Idle);
+    threadTestLambda(ThreadPriority::Lowest);
+    threadTestLambda(ThreadPriority::BelowNormal);
+    threadTestLambda(ThreadPriority::Normal);
+    threadTestLambda(ThreadPriority::AboveNormal);
+    threadTestLambda(ThreadPriority::Highest);
+    threadTestLambda(ThreadPriority::RealTime);
+}
+
+TEST(ThreadTest, ThreadId)
+{
+    std::array<unsigned int, testThreadNumber> testIds;
+    std::array<std::thread, testThreadNumber> testThreads;
+
+    for (int i = 0; i < testThreadNumber; i++)
+        testThreads[i] = std::thread(idTestFunc, &testIds[i]);
+
+    // Wait till all threads do their work
+    for (int i = 0; i < testThreadNumber; i++)
+        testThreads[i].join();
+
+    // Create and resize container for unique values
+    std::vector<unsigned int> idsUniqueCopy;
+    idsUniqueCopy.reserve(testThreadNumber);
+
+    // Sort in place and then create a unique-values copy
+    std::sort(testIds.begin(), testIds.end());
+    std::unique_copy(testIds.cbegin(), testIds.cend(), std::back_inserter(idsUniqueCopy));
+
+    // Check for duplicate values - should be none
+    std::vector<unsigned int> difference;
+    std::set_difference(testIds.cbegin(), testIds.cend(),
+                        idsUniqueCopy.cbegin(), idsUniqueCopy.cend(),
+                        std::back_inserter(difference));
+
+    ASSERT_TRUE(difference.empty());
+}
+
+TEST(ThreadTest, ThreadName)
+{
+    std::array<std::thread, testThreadNumber> testThreads;
+
+    for (int i = 0; i < testThreadNumber; i++)
+        testThreads[i] = std::thread(nameTestFunc);
+
+    for (int i = 0; i < testThreadNumber; i++)
+        testThreads[i].join();
+}
