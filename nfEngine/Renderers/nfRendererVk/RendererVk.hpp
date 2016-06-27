0b156a99a8f4d1a6177da744e21bded83a15fa38
Lukasz Kostyra <costyrra.xl@gmail.com>
2016-06-27 13:30:24 +0200

    Renderers: nfRendererVk skeleton
    Change-Id: I26f2fafcd968d8e7e126b51ed8b09bfa7af72fc4

---
+++ nfEngine/Renderers/nfRendererVk/RendererVk.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * @author  LKostyra (costyrra.xl@gmail.com)
+ * @brief   Declarations of Vulkan rendering backend.
+ */
+
+#pragma once
+
+#include "Defines.hpp"
+
+// modules
+#include "Device.hpp"
--- nfEngine/nfRendererDemo/Common.cpp
+++ nfEngine/nfRendererDemo/Common.cpp
@@ -17,6 +17,7 @@ std::string gShaderPathExt;
 const std::string D3D11_BACKEND("nfRendererD3D11");
 const std::string D3D12_BACKEND("nfRendererD3D12");
 const std::string OGL4_BACKEND("nfRendererOGL4");
+const std::string VK_BACKEND("nfRendererVk");
 
 const std::string HLSL5_SHADER_PATH_PREFIX("nfEngine/nfRendererDemo/Shaders/HLSL5/");
 const std::string HLSL5_SHADER_EXTENSION(".hlsl");
@@ -29,7 +30,7 @@ std::vector<std::string> GetDefaultBackend()
 #ifdef WIN32
     return { D3D11_BACKEND, HLSL5_SHADER_PATH_PREFIX, HLSL5_SHADER_EXTENSION };
 #elif defined(__linux__) | defined(__LINUX__)
-    return { OGL4_BACKEND, GLSL_SHADER_PATH_PREFIX, GLSL_SHADER_EXTENSION };
+    return { VK_BACKEND, GLSL_SHADER_PATH_PREFIX, GLSL_SHADER_EXTENSION };
 #else
 #error "Target platform not supported!"
 #endif
