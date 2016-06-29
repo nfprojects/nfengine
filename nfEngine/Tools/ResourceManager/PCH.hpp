47b5e204174a1cf269d9bfb71434c146f2348dc0
Michal Witanowski <witek902@gmail.com>
2016-06-29 20:09:04 +0200

    Mesh resources refactor
    * FastMeshConverter was converted into new ResourceManager tool
    * Mesh binary file handling implemented in new project - nfResources
    * Code cleanup
    * CMake files added
    
    Verification:
    1. Update nfEngineDemo/Data directory with syncher.
    2. Make sure that nfEngineDemo/Data/Materials is gone.
    3. Remove all NFM files from nfEngineDemo/Data/Meshes
    4. Run ResourceManager tool and check if materials and meshes files
       are created in nfEngineDemo/CookedData.
    5. Run nfEngineDemo and make sure that all meshes and materials
       load properly.
    
    Change-Id: Id27d89f0e1251e4c71353e20a4f985c86eab5c8e

---
+++ nfEngine/Tools/ResourceManager/PCH.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <limits.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <algorithm>
+#include <vector>
+
+#include "model_obj/model_obj.h"
--- nfEngineDemo/Test.hpp
+++ nfEngineDemo/Test.hpp
@@ -20,44 +20,4 @@
 #include "nfCommon/Timer.hpp"
 #include "nfCommon/Logger.hpp"
 
-/*
-    This function creates an example mesh.
-
-void CreateCustomFloorMesh()
-{
-    CustomMeshVertex verticies[4];
-    verticies[0].Position = Float3(-50.0f, 0.0f, -50.0f);
-    verticies[1].Position = Float3(50.0f, 0.0f, -50.0f);
-    verticies[2].Position = Float3(-50.0f, 0.0f, 50.0f);
-    verticies[3].Position = Float3(50.0f, 0.0f, 50.0f);
-
-    verticies[0].TexCoord = Float2(-20.0f, 20.0f);
-    verticies[1].TexCoord = Float2(20.0f,  20.0f);
-    verticies[2].TexCoord = Float2(-20.0f, -20.0f);
-    verticies[3].TexCoord = Float2(20.0f, -20.0f);
-
-    for (int i = 0; i<4; i++)
-    {
-        verticies[i].Normal = Float3(0.0f, 1.0f, 0.0f);
-        verticies[i].Tangent = Float3(1.0f, 0.0f, 0.0f);
-    }
-
-    uint32 indices[] = {0, 1, 2,  1, 3, 2};
-
-    CustomMeshSubMesh submeshes[2];
-    submeshes[0].m_IndexOffset = 0;
-    submeshes[0].m_TrianglesCount = 1;
-    submeshes[0].pMaterial = 0; //ENGINE_GET_MATERIAL("floor");
-
-    submeshes[1].m_IndexOffset = 3;
-    submeshes[1].m_TrianglesCount = 1;
-    submeshes[1].pMaterial = ENGINE_GET_MATERIAL("bricks");
-
-
-    Mesh* pCustomMesh = ENGINE_GET_MESH("floor_mesh");
-    pCustomMesh->Create(verticies, 4,  indices, 6,  submeshes, 2);
-    //pCustomMesh->AddRef();
-}
-*/
-
 extern NFE::Engine* gEngine;
