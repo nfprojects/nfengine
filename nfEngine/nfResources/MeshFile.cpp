3dba7f3f7e9b529cc50a5e9f62c41327f8682412
Michal Witanowski <witek902@gmail.com>
2016-06-29 20:09:04 +0200

    Mesh resources refactor
    * FastMeshConverter was converted into new ResourceManager tool
    * Mesh binary file handling implemented in new project - nfResources
    * Code cleanup
    * CMake files added
    
    Verification:
    1. Update nfEngineDemo/Data directory with syncher.
    2. Make sure that nfEngineDemo/Data/Materials is gone.
    3. Remove all NFM files from nfEngineDemo/Data/Meshes
    4. Run ResourceManager tool and check if materials and meshes files
       are created in nfEngineDemo/CookedData.
    5. Run nfEngineDemo and make sure that all meshes and materials
       load properly.
    
    Change-Id: Id27d89f0e1251e4c71353e20a4f985c86eab5c8e

---
+++ nfEngine/nfResources/MeshFile.cpp
@@ -0,0 +1,92 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Mesh resource file definitions
+ */
+
+#include "PCH.hpp"
+#include "MeshFile.hpp"
+
+#include "nfCommon/Logger.hpp"
+#include "nfCommon/FileSystem.hpp"
+#include "nfCommon/File.hpp"
+
+
+namespace NFE {
+namespace Resource {
+
+using namespace Math;
+
+bool MeshFile::Load(Common::InputStream& stream)
+{
+    MeshHeader header;
+    if (stream.Read(&header, sizeof(header)) != sizeof(header))
+    {
+        LOG_ERROR("Failed to read mesh signature");
+        return false;
+    }
+
+    if (header.magic != NFE_MESH_FILE_MAGIC)
+    {
+        LOG_ERROR("Invalid signature");
+        return false;
+    }
+
+    // sanity checks
+    if (header.indicesCount == 0 || header.subMeshesCount == 0 || header.verticesCount == 0)
+    {
+        LOG_ERROR("Corrupted mesh file");
+        return false;
+    }
+
+    const size_t verticesSize = header.verticesCount * sizeof(MeshVertex);
+    const size_t indicesSize = header.indicesCount * sizeof(uint32);
+    const size_t subMeshesSize = header.subMeshesCount * sizeof(MeshSubMesh);
+
+    mVertices.resize(header.verticesCount);
+    mIndices.resize(header.indicesCount);
+    mSubMeshes.resize(header.subMeshesCount);
+
+    if (stream.Read(mVertices.data(), verticesSize) != verticesSize)
+    {
+        LOG_ERROR("Failed to load vertices data");
+        return false;
+    }
+
+    if (stream.Read(mIndices.data(), indicesSize) != indicesSize)
+    {
+        LOG_ERROR("Failed to load indices data");
+        return false;
+    }
+
+    if (stream.Read(mSubMeshes.data(), subMeshesSize) != subMeshesSize)
+    {
+        LOG_ERROR("Failed to load submeshes data");
+        return false;
+    }
+
+    return true;
+}
+
+bool MeshFile::Save(Common::OutputStream& stream) const
+{
+    MeshHeader header;
+    header.magic = NFE_MESH_FILE_MAGIC;
+    header.verticesCount = static_cast<uint32>(mVertices.size());
+    header.indicesCount = static_cast<uint32>(mIndices.size());
+    header.subMeshesCount = static_cast<uint32>(mSubMeshes.size());
+
+    if (!stream.Write(&header, sizeof(header)))
+        return false;
+    if (!stream.Write(mVertices.data(), sizeof(MeshVertex) * header.verticesCount))
+        return false;
+    if (!stream.Write(mIndices.data(), sizeof(uint32) * header.indicesCount))
+        return false;
+    if (!stream.Write(mSubMeshes.data(), sizeof(MeshSubMesh) * header.subMeshesCount))
+        return false;
+
+    return true;
+}
+
+} // namespace Resource
+} // namespace NFE
