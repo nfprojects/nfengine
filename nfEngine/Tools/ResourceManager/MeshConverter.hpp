bd503e7f3085bd0a77d02041d3bcc2a631f9a575
Michal Witanowski <witek902@gmail.com>
2016-06-29 20:09:04 +0200

    Tools: ResourceManager base
    * FastMeshConverter was converted into new ResourceManager tool
    * Code cleanup
    * CMake files added
    
    Verification:
    1. Update nfEngineDemo/Data directory with syncher.
    2. Make sure that nfEngineDemo/Data/Materials is gone.
    3. Remove all NFM files from nfEngineDemo/Data/Meshes
    4. Run ResourceManager tool and check if materials and meshes files
       are created in nfEngineDemo\CookedData.
    5. Run nfEngineDemo and make sure that all meshes and materials
       load properly.
    
    Change-Id: Id27d89f0e1251e4c71353e20a4f985c86eab5c8e

---
+++ nfEngine/Tools/ResourceManager/MeshConverter.hpp
@@ -0,0 +1,80 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Mesh converter declarations
+ */
+
+#pragma once
+
+#include "nfCommon/Math/Math.hpp"
+
+#include <vector>
+
+
+// NFM file structures
+
+#define MAT_NAME_MAX_LENGTH (120)
+
+namespace NFE {
+
+/**
+ * Helper class allowing for mesh file conversion.
+ */
+class MeshFile
+{
+    // TODO
+    // * support for other files
+    // * support for loading meshes from cooked file
+
+    // TODO - file format
+    // * Level of Detail
+    // * 32-bit or 16-bit indices (to save space)
+    // * bones information
+    // * animations information
+    // * internal materials
+
+public:
+    struct Vertex
+    {
+        Math::Float3 pos;       // position
+        Math::Float2 texCoord;  // texture coordinate
+        int8 normal[4];         // normal vector (encoded as normalized 8-bit value)
+        int8 tangent[4];        // tangent vector (encoded as normalized 8-bit value)
+    };
+
+    // submesh is a set of triangles that use the same material
+    struct SubMesh
+    {
+        int indexOffset;
+        int triangleCount;
+
+        // TODO use resource ID instead when it's supported
+        char materialName[MAT_NAME_MAX_LENGTH];
+    };
+
+    // internal material (TEMPORARY)
+    struct Material
+    {
+        std::string name;
+        std::string diffuseTextureFileName;
+        std::string normalTextureFileName;
+    };
+
+    /**
+     * Load mesh form OBJ file.
+     */
+    bool LoadFromObj(const std::string& path);
+
+    /**
+     * Save mesh to binary (cooked) format.
+     */
+    bool Save(const std::string& path) const;
+
+private:
+    std::vector<Vertex> mVertices;
+    std::vector<unsigned int> mIndices;
+    std::vector<SubMesh> mSubMeshes;
+    std::vector<Material> mMaterials;
+};
+
+} // namespace NFE
