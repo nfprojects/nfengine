fc1772cb0a28468664c22e631ace80febe80ee12
mkkulagowski <mk.kulagowski@gmail.com>
2016-06-30 18:40:53 +0200

    nfCommon: Implement scope logs
    Implements LogScope class, that allows for logging while entering and
    exiting a certain scope.
    
    Verification: Build and run nfCommonTest.
    
    Change-Id: Ib615cc831b1c4b27f2f7de62e64accd856ee094a

---
+++ nfEngine/nfCommon/LogScope.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  LogScope class declarations.
+ */
+
+#pragma once
+
+#include "nfCommon.hpp"
+
+namespace NFE {
+namespace Common {
+
+class NFCOMMON_API LogScope
+{
+private:
+    const char* mScopeName;
+
+public:
+    LogScope();
+    LogScope(const char* name);
+    LogScope(const char* name, const char* file, int line);
+    ~LogScope();
+};
+
+// Macro for easy calling LogScope's constructor with __FILE__ and __LINE__.
+#define LOG_SCOPE(scopeName) LogScope scopeName(#scopeName, __FILE__, __LINE__)
+
+} // namespace Common
+} // namespace NFE
