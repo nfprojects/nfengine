15422772fa6b2cc3142d0430bb71548bbad17532
Lukasz Kostyra <costyrra.xl@gmail.com>
2016-07-07 16:44:36 +0200

    nfRendererVk: Enable scene 0:0
    Backbuffer, RenderTarget and Shaders are implemented.
    
    Verification:
        Build, run nfRendererDemo. Scene 0:0 should work.
    
    Change-Id: If11930b697139fbe8d47ab7fb65f163e60cd4a88

---
+++ nfEngine/Renderers/nfRendererVk/Backbuffer.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file
+ * @author  LKostyra (costyrra.xl@gmail.com)
+ * @brief   Definition of renderer's Backbuffer and its common functions
+ */
+
+#include "PCH.hpp"
+#include "Backbuffer.hpp"
+
+
+namespace NFE {
+namespace Renderer {
+
+Backbuffer::Backbuffer()
+{
+}
+
+Backbuffer::~Backbuffer()
+{
+}
+
+bool Backbuffer::Resize(int newWidth, int newHeight)
+{
+}
+
+bool Backbuffer::Init(const BackbufferDesc& desc)
+{
+    // XCB connection
+
+}
+
+bool Present()
+{
+}
+
+} // namespace Renderer
+} // namespace NFE
---
+++ nfEngine/Renderers/nfRendererVk/Backbuffer.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file
+ * @author  LKostyra (costyrra.xl@gmail.com)
+ * @brief   Declaration of renderer's backbuffer
+ */
+
+#include "../RendererInterface/Backbuffer.hpp"
+#include "Texture.hpp"
+
+namespace NFE {
+namespace Renderer {
+
+
+class Backbuffer : public IBackbuffer, public Texture
+{
+    friend class CommandBuffer;
+
+public:
+    Backbuffer();
+    ~Backbuffer();
+
+    bool Resize(int newWidth, int newHeight);
+    bool Init(const BackbufferDesc& desc);
+    bool Present();
+};
+
+} // namespace Renderer
+} // namespace NFE
