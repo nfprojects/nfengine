2eb6b7543ea2aea8c7754c05081aac7ff7405f32
Lukasz Kostyra <costyrra.xl@gmail.com>
2016-07-08 21:18:02 +0200

    nfRendererVk: Extension system v1
    First version of the extension system for Vulkan renderer.
    
    For simplicity sake, it almost completely mimics the OpenGL Renderer
    Extension subsystem. As the Renderer progresses, this will be replaced
    with something more robust and clean.
    
    Verification:
        Build, run nfRendererDemo with nfRendererVk argument. Should work
        as before (print information about GPU devices, then exit).
    
    Change-Id: Ie4cbbc1781f3f28631729dc27a1f39421809e1d5

---
+++ nfEngine/Renderers/nfRendererVk/Linux/XcbExtensions.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * @author  LKostyra (costyrra.xl@gmail.com)
+ * @brief   Declarations of XCB-specific Vulkan extensions
+ */
+
+#pragma once
+
+#include "../Defines.hpp"
+
+namespace NFE {
+namespace Renderer {
+
+extern PFN_vkCreateXcbSurfaceKHR vkCreateXcbSurfaceKHR;
+
+/**
+ * Initializes Xcb Surface-related device extensions.
+ */
+bool nfvkXcbInstanceExtensionsInit(VkInstance instance);
+
+} // namespace Renderer
+} // namespace NFE
