a2cc46c2acafb8ad4882b62205ebf8d30a10cf29
Michal Witanowski <witek902@gmail.com>
2016-07-17 21:57:55 +0200

    nfRendererD3D12: Support for prefered video card selection
    It is now possible to specify prefered video card in nfRendererTest
    and nfRendererDemo:
    * in nfRendererTest, to use card 1, pass arguments: "--card_id 1",
    * in nfRendererDemo, to use card 1 with D3D12 renderer, pass arguments:
      "nfRendererD3D12 1".
    
    Change-Id: I0b80c30b69103310870af301f995e158baf6e4d0

--- nfEngine/Renderers/RendererInterface/Device.hpp
+++ nfEngine/Renderers/RendererInterface/Device.hpp
@@ -103,7 +103,22 @@ public:
     virtual bool DownloadTexture(ITexture* tex, void* data, int mipmap = 0, int layer = 0) = 0;
 };
 
-typedef IDevice* (*RendererInitFunc)();
+
+/**
+ * Structure containing rendering device initialization parameters.
+ */
+struct DeviceInitParams
+{
+    int preferedCardId;
+    const char* preferedCardName;
+
+    DeviceInitParams()
+        : preferedCardId(-1)
+        , preferedCardName(nullptr)
+    {}
+};
+
+typedef IDevice* (*RendererInitFunc)(const DeviceInitParams*);
 typedef void (*RendererReleaseFunc)();
 
 } // namespace Renderer
