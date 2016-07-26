37bca932159ae5ce9946e4bb2a58b1e78d036f86
mkkulagowski <mk.kulagowski@gmail.com>
2016-07-26 16:31:05 +0200

    nfCommon: Implement asynchronous queue managing class
    Introduces AsyncQueueManager class that holds dedicated thread for
    asynchronous jobs and their callbacks.
    
    Verification: Build.
    
    Change-Id: I925130b5ae5e7bf71d42021f4baab959c864852c

---
+++ nfEngine/nfCommon/AsyncQueueManager.hpp
@@ -0,0 +1,91 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  AsyncQueueManager utility declarations.
+ */
+
+#pragma once
+#include "nfCommon.hpp"
+
+#if defined(WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+
+#elif defined(__LINUX__) | defined(__linux__)
+#include <functional>
+#include <thread>
+#include <unordered_map>
+#endif
+
+#define NUM_EVENTS 128
+
+namespace NFE
+{
+namespace Common
+{
+
+class NFCOMMON_API AsyncQueueManager
+{
+private:
+    AsyncQueueManager();
+    ~AsyncQueueManager();
+    AsyncQueueManager(const AsyncQueueManager&) = delete;
+    AsyncQueueManager(AsyncQueueManager&&) = delete;
+
+public:
+    bool mIsDestroyed;
+    /**
+     * Get instance of AsyncQueueManager
+     */
+    static AsyncQueueManager& GetInstance();
+
+#if defined(WIN32)
+private:
+    HANDLE mQueueThread;
+    HANDLE mQuitEvent;
+
+    static DWORD CALLBACK JobQueue(LPVOID param);
+    using JobProcedure = PAPCFUNC;
+
+public:
+    /**
+     * Enqueue asynchronous job.
+     * The callback function will be called from an internal worker thread.
+     *
+     * @param callback Funtion to be called upon end of jobs execution
+     * @param data     Pointer to the user-defined data that will be passed to the callback function.
+     */
+    bool EnqueueJob(JobProcedure& callback, void* data);
+
+
+#elif defined(__LINUX__) | defined(__linux__)
+private:
+    using JobProcedure = std::function<void(void*)>;
+    std::thread mQueueThread;
+    int mEventPollFD;
+    std::unordered_map<int, JobProcedure> mFdMap;
+
+    static void JobQueue();
+public:
+    /**
+     * Enqueue FD to be watched for POLLIN event.
+     * The callback function will be called from an internal worker thread, when FD produces event.
+     *
+     * @param callback Funtion to be called upon POLLIN event of @FD
+     * @param FD       Descriptor to be watched
+     * @param data     Pointer to the user-defined data that will be passed to the callback function.
+     *
+     * @remarks Calling this method second time with the same @FD will cease watching given descriptor.
+     */
+    bool EnqueueJob(JobProcedure& callback, int FD, void* data);
+
+    /**
+     * Get queue thread to enqueue asynchronous jobs on.
+     */
+    const std::thread& GetQueueThread();
+#endif
+};
+
+} // namespace Common
+} // namespace NFE
---
+++ nfEngine/nfCommon/Win/AsyncQueueManager.cpp
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  Windows implementation of AsyncQueueManager class
+ */
+
+#include "../PCH.hpp"
+#include "../AsyncQueueManager.hpp"
+#include "../Logger.hpp"
+#include "Common.hpp"
+
+#include <assert.h>
+
+
+namespace NFE {
+namespace Common {
+
+AsyncQueueManager::AsyncQueueManager()
+    : mIsDestroyed(false)
+{
+    mQueueThread = ::CreateThread(0, 0, AsyncQueueManager::JobQueue, nullptr, 0, 0);
+    mQuitEvent = ::CreateEvent(nullptr, FALSE, FALSE, L"JobQueue QuitEvent");
+}
+
+AsyncQueueManager::~AsyncQueueManager()
+{
+    mIsDestroyed = true;
+    ::SetEvent(mQuitEvent);
+    ::WaitForSingleObject(mQueueThread, INFINITE);
+}
+
+AsyncQueueManager& AsyncQueueManager::GetInstance()
+{
+    static AsyncQueueManager instance;
+    return instance;
+}
+
+bool AsyncQueueManager::EnqueueJob(JobProcedure& callback, void* data)
+{
+    if (0 == ::QueueUserAPC(callback, mQueueThread, reinterpret_cast<ULONG_PTR>(data)))
+    {
+        LOG_ERROR("QueueUserAPC() failed for AsyncQueueManager: %s", GetLastErrorString().c_str());
+        return false;
+    }
+
+    return true;
+}
+
+DWORD AsyncQueueManager::JobQueue(LPVOID param)
+{
+    UNUSED(param);
+    AsyncQueueManager* const instance = &AsyncQueueManager::GetInstance();
+
+    // Drift in the abyss of idleness waiting for the moment to become useful
+    while (!instance->mIsDestroyed)
+        ::WaitForSingleObjectEx(instance->mQuitEvent, INFINITE, true);
+
+    return 0;
+}
+
+} // namespace Common
+} // namespace NFE
