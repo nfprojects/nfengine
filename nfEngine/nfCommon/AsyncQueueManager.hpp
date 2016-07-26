74083041cf0f5dab19c83e9a3ded344a4d8e4b04
mkkulagowski <mk.kulagowski@gmail.com>
2016-07-26 16:31:05 +0200

    nfCommon: Implement asynchronous queue managing class
    Introduces AsyncQueueManager class that holds dedicated thread for
    asynchronous jobs and their callbacks.
    
    Verification: Build.
    
    Change-Id: I925130b5ae5e7bf71d42021f4baab959c864852c

---
+++ nfEngine/nfCommon/AsyncQueueManager.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file
+ * @author mkulagowski (mkkulagowski(at)gmail.com)
+ * @brief  AsyncQueueManager utility declarations.
+ */
+
+#pragma once
+
+#if defined(WIN32)
+#include "Win/AsyncQueueManager.hpp"
+#elif defined(__LINUX__) | defined(__linux__)
+#include "Linux/AsyncQueueManager.hpp"
+#endif
