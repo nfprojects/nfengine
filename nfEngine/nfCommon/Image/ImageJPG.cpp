a2f90e58e4a45fa5bb6d41f09f04730f976676d1
mkkulagowski <mk.kulagowski@gmail.com>
2016-07-28 11:23:36 +0200

    nfCommon: Implement Image::SaveJPG method
    Introduces a way to save an object of the Image class to an OutputStream -
    which can be either buffer or file. Also introduces tests for Save*()
    methods.
    Changes number of channels when loading JPG files to 3 - jpg format
    doesn't use alpha channel.
    
    Verification: Build and run nfCommonTest. Check for files in
    nfEngine\TestResources\ImageSamples ending with "saved.jpg" and compare
    them to originals.
    
    Change-Id: I64003060acf48b4cc859b528b6dfa68eb6dce640

--- nfEngine/nfCommon/Image/ImageJPG.cpp
+++ nfEngine/nfCommon/Image/ImageJPG.cpp
@@ -10,21 +10,22 @@
 #include "Image.hpp"
 #include "ImageJPG.hpp"
 #include "jpeg/jpgd.h"
+#include "jpeg/jpge.h"
 
 
 namespace NFE {
 namespace Common {
 
-namespace {
 
-// bridge between InputStream and jpgd::decompress_jpeg_image_from_stream function
-class CustomJpegStream : public jpgd::jpeg_decoder_stream
+namespace {
+// Custom class to allow reading with our InputStream class
+class CustomJpegIStream : public jpgd::jpeg_decoder_stream
 {
 private:
     InputStream* mStream;
 
 public:
-    CustomJpegStream(InputStream* stream)
+    CustomJpegIStream(InputStream* stream)
     {
         mStream = stream;
     }
@@ -39,7 +40,26 @@ public:
     }
 };
 
-}
+// Custom class to allow writing with our OutputStream class
+class CustomJpegOStream : public jpge::output_stream
+{
+private:
+    OutputStream* mStream;
+
+public:
+    CustomJpegOStream(OutputStream* stream)
+    {
+        mStream = stream;
+    }
+
+    bool put_buf(const void* Pbuf, int len) override
+    {
+        size_t toWrite = static_cast<size_t>(len);
+        return toWrite == mStream->Write(Pbuf, toWrite);
+    }
+};
+
+} // namespace
 
 // Register JPG image type
 bool gImageJPGRegistered = ImageType::RegisterImageType("JPG", std::make_unique<ImageJPG>());
@@ -61,20 +81,63 @@ bool ImageJPG::Check(InputStream* stream)
 
 bool ImageJPG::Load(Image* img, InputStream* stream)
 {
-    CustomJpegStream jpegStream(stream);
+    CustomJpegIStream jpegStream(stream);
 
     int comps, width, height;
-    void* data = jpgd::decompress_jpeg_image_from_stream(&jpegStream, &width, &height, &comps, 4);
+    void* data = jpgd::decompress_jpeg_image_from_stream(&jpegStream, &width, &height, &comps, 3);
 
-    bool result = img->SetData(data, width, height, ImageFormat::RGBA_UByte);
+    bool result = img->SetData(data, width, height, ImageFormat::RGB_UByte);
     free(data);
 
     return result;
 }
 
-bool ImageJPG::Save(Image*, OutputStream*)
+bool ImageJPG::Save(Image* img, OutputStream* stream)
 {
-    return false;
+    if (img->GetFormat() != ImageFormat::RGBA_UByte)
+    {
+        LOG_ERROR("To save image as JPG, RGBA_UByte format is needed.");
+        return false;
+    }
+
+    CustomJpegOStream jpegStream(stream);
+    jpge::jpeg_encoder jpegEncoder;
+    jpge::params jpegParams;
+    jpegParams.m_quality = 100;
+
+    uint32 width = img->GetWidth();
+    uint32 height = img->GetHeight();
+
+    size_t bitsPerPix = BitsPerPixel(img->GetFormat());
+
+    if (!jpegEncoder.init(&jpegStream, width, height, static_cast<int>(bitsPerPix / 8), jpegParams))
+    {
+        LOG_ERROR("Error while initializing encoder");
+        jpegEncoder.deinit();
+        return false;
+    }
+
+    size_t lineSize = bitsPerPix / 8 * width * sizeof(uint8);
+    size_t streamSize = img->GetMipmap()->GetDataSize() / sizeof(uint8);
+    const uint8* linePtr = reinterpret_cast<const uint8*>(img->GetMipmap()->GetData());
+    for (size_t i = 0; i < streamSize; i += lineSize)
+        if (!jpegEncoder.process_scanline(linePtr + i))
+        {
+            LOG_ERROR("Error while processing line no. %i", i / lineSize);
+            jpegEncoder.deinit();
+            return false;
+        }
+
+    if (!jpegEncoder.process_scanline(nullptr))
+    {
+        LOG_ERROR("Error while finishing compression");
+        jpegEncoder.deinit();
+        return false;
+
+    }
+
+    jpegEncoder.deinit();
+    return true;
 }
 
 } // namespace Common
