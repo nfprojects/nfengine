3925cb8d5d0e0f0d523f0a4d2829e0806207fe50
Michal Witanowski <witek902@gmail.com>
2016-08-26 23:06:10 +0200

    nfCore: Simple config integration
    * Introduced ConfigVariable and ConfigManager.
    * Built-in config variables can be declared with ConfigVariable class.
    * Simple config file created.
    
    Verification:
        Build and run nfEngineDemo. Play with engineConfig.cfg
        (only "renderer/debug" and "physics" work for now).
    
    Change-Id: I4da2a110e88657a0310290d809288a5edac8dff1

---
+++ nfEngine/nfCore/Utils/ConfigManager.cpp
@@ -0,0 +1,158 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Engine's config mangager definition
+ */
+
+#include "../PCH.hpp"
+
+#include "ConfigManager.hpp"
+#include "ConfigVariable.hpp"
+
+#include "nfCommon/Config.hpp"
+#include "nfCommon/File.hpp"
+#include "nfCommon/Logger.hpp"
+
+namespace NFE {
+
+using namespace Common;
+
+const char* MAIN_CONFIG_FILE_PATH = "engineConfig.cfg";
+
+ConfigManager::ConfigManager()
+    : mNumVariables(0)
+    , mConfig(new Common::Config)
+{
+}
+
+ConfigManager& ConfigManager::GetInstance()
+{
+    static ConfigManager gConfigManager;
+    return gConfigManager;
+}
+
+bool ConfigManager::Initialize()
+{
+    LOG_INFO("%u config variables registered", mNumVariables);
+
+    // TODO: load config from multiple config files
+    return LoadConfiguration();
+}
+
+void ConfigManager::RegisterVariable(IConfigVariable* variable)
+{
+    const char* path = variable->GetPath();
+
+    std::stringstream sstream(path);
+    std::string segment;
+    std::vector<std::string> segments;
+    while (std::getline(sstream, segment, '/'))
+    {
+        segments.push_back(segment);
+    }
+
+    size_t numSegments = segments.size();
+    NFE_ASSERT(numSegments > 0, "Invalid config variable path");
+
+    // find/create node for the variable
+    Node* node = &mRootNode;
+    if (numSegments > 1)
+    {
+        for (size_t i = 0; i < numSegments - 1; ++i)
+        {
+            const auto it = node->subNodes.find(segments[i]);
+            if (it == node->subNodes.end())
+            {
+                // create new node
+                NodePtr newNode(new Node);
+                Node* nextNode = newNode.get();
+                node->subNodes[segments[i]] = std::move(newNode);
+                node = nextNode;
+            }
+            else
+            {
+                // pass existing node
+                node = it->second.get();
+            }
+        }
+    }
+
+    if (node->variables.find(segments.back()) != node->variables.end())
+    {
+        LOG_ERROR("Variable '%s' declared twice", path);
+    }
+
+    // place variable in the target node
+    node->variables[segments.back()] = variable;
+    mNumVariables++;
+}
+
+bool ConfigManager::LoadConfiguration()
+{
+    LOG_INFO("Loading engine's configuration...");
+
+    std::vector<char> configFileStr;
+    size_t configFileSize = 0;
+
+    // load config file to memory
+    File file(MAIN_CONFIG_FILE_PATH, AccessMode::Read);
+    configFileSize = static_cast<size_t>(file.GetSize());
+    configFileStr.resize(configFileSize + 1);
+    if (file.Read(configFileStr.data(), configFileSize) != configFileSize)
+    {
+        LOG_ERROR("Failed to read config file");
+        return false;
+    }
+    configFileStr[configFileSize] = '\0';
+
+    // parse
+    if (!mConfig->Parse(configFileStr.data()))
+    {
+        LOG_ERROR("Failed to parse engine's config file");
+        return false;
+    }
+
+    // find config variables
+    if (!mRootNode.Parse(*mConfig, mConfig->GetRootNode()))
+    {
+        LOG_ERROR("Failed to load config variables");
+        return false;
+    }
+
+    LOG_SUCCESS("Engine's configuration loaded");
+    return true;
+}
+
+bool ConfigManager::Node::Parse(const Common::Config& config, ConfigObjectNodePtr node)
+{
+    auto callback = [&](const char* key, const ConfigValue& value)
+    {
+        // find matching subnodes (objects)
+        for (auto& subNode : subNodes)
+        {
+            if (subNode.first == key && value.IsObject())
+            {
+                subNode.second->Parse(config, value.GetObj());
+            }
+        }
+
+        // find matching variables (values)
+        for (auto& variable : variables)
+        {
+            if (variable.first == key && !value.IsObject())
+            {
+                if (variable.second->ParseConfigValue(value))
+                {
+                    LOG_DEBUG("'%s' = %s", variable.second->GetPath(), variable.second->ToString().c_str());
+                }
+            }
+        }
+
+        return true;
+    };
+
+    config.Iterate(callback, node);
+    return true;
+}
+
+} // namespace NFE
---
+++ nfEngine/nfCore/Utils/ConfigVariable.cpp
@@ -0,0 +1,134 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Config variables definitions
+ */
+
+#include "../PCH.hpp"
+#include "ConfigVariable.hpp"
+
+#include "nfCommon/Config.hpp"
+#include "nfCommon/File.hpp"
+#include "nfCommon/Logger.hpp"
+#include "nfCommon/Math/Math.hpp"
+
+namespace NFE {
+
+bool IConfigVariable::ParseConfigValue(const Common::ConfigValue& value)
+{
+    if (value.IsInt() && mType == ConfigVarType::Integer)
+    {
+        auto var = static_cast<ConfigVariable<int>*>(this);
+        var->mValue = value.GetInt();
+        return true;
+    }
+    else if (value.IsBool() && mType == ConfigVarType::Bool)
+    {
+        auto var = static_cast<ConfigVariable<bool>*>(this);
+        var->mValue = value.GetBool();
+        return true;
+    }
+    else if (value.IsString() && mType == ConfigVarType::String)
+    {
+        auto var = static_cast<ConfigVariable<const char*>*>(this);
+        var->mValue = value.GetString();
+        return true;
+    }
+    else if (value.IsFloat() && mType == ConfigVarType::Float)
+    {
+        auto var = static_cast<ConfigVariable<float>*>(this);
+        var->mValue = value.GetFloat();
+        return true;
+    }
+
+    return false;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<>
+void ConfigVariable<int>::SetType()
+{
+    mType = ConfigVarType::Integer;
+}
+
+template<>
+void ConfigVariable<bool>::SetType()
+{
+    mType = ConfigVarType::Bool;
+}
+
+template<>
+void ConfigVariable<const char*>::SetType()
+{
+    mType = ConfigVarType::String;
+}
+
+template<>
+void ConfigVariable<float>::SetType()
+{
+    mType = ConfigVarType::Float;
+}
+
+template<>
+void ConfigVariable<Math::Float2>::SetType()
+{
+    mType = ConfigVarType::Float2;
+}
+
+template<>
+void ConfigVariable<Math::Float3>::SetType()
+{
+    mType = ConfigVarType::Float3;
+}
+
+template<>
+void ConfigVariable<Math::Float4>::SetType()
+{
+    mType = ConfigVarType::Float4;
+}
+
+
+template<>
+std::string ConfigVariable<bool>::ToString() const
+{
+    return mValue ? "true" : "false";
+}
+
+template<>
+std::string ConfigVariable<int>::ToString() const
+{
+    return std::to_string(mValue);
+}
+
+template<>
+std::string ConfigVariable<float>::ToString() const
+{
+    return std::to_string(mValue);
+}
+
+template<>
+std::string ConfigVariable<const char*>::ToString() const
+{
+    return '\"' + std::string(mValue) + '\"';
+}
+
+template<>
+std::string ConfigVariable<Math::Float2>::ToString() const
+{
+    return '[' + std::to_string(mValue.x) + ", " + std::to_string(mValue.y) + ']';
+}
+
+template<>
+std::string ConfigVariable<Math::Float3>::ToString() const
+{
+    return '[' + std::to_string(mValue.x) + ", " + std::to_string(mValue.y) + ", " + std::to_string(mValue.z) + ']';
+}
+
+template<>
+std::string ConfigVariable<Math::Float4>::ToString() const
+{
+    return '[' + std::to_string(mValue.x) + ", " + std::to_string(mValue.y) + ", " + std::to_string(mValue.z) + ", " + std::to_string(mValue.w) + ']';
+}
+
+} // namespace NFE
