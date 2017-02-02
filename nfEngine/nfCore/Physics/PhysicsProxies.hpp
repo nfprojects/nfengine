818541db92160f6e19a00dd5cb2c6ce11184c095
Michal Witanowski <witek902@gmail.com>
2017-02-03 00:05:11 +0100

    nfCore: Scene mamagement rewritten
    * "Entity" is a normal class holding list of child entities and
      components.
    * EntityManager was transformed to EntitySystem.
    * TransformSystem was removed - EntitySystem is managing entities
      transforms.
    * All the Scene Systems inherit from Scene::ISystem.
    * Introduced concept of "GameObject". Game Object is a new resource
      representing a structure of nodes and components.
      Game Object can be spawned on a scene via GameObjectComponent.
      GameObjectSystem manages all spawned Game Object Instances.
    
    RendererSystem and PhysicsSystem were refactored. They are now more
    isolated from the rest of the code (scene and components), so they
    can be moved to separate project in the future. Each of this
    systems provides an interface to create "proxies" - the objects
    managed by the system. They won't keep track of resources states -
    it's components and resource manager job. For example, when a
    MeshComponent is attached to entity, it will create MeshProxy
    only when the Mesh resource is fully loaded. This will apply to
    other systems as well.
    NOTE: the proxy management interface is a draft - there will be more
    features and proxy types required in the future.
    
    Verification:
        1. Rebuild all.
        2. Run nfCoreTest and nfEngineDemo in all configurations.
    
    Change-Id: I87209e0028bd06279076e7bd31f097e291966aba

---
+++ nfEngine/nfCore/Physics/PhysicsProxies.hpp
@@ -0,0 +1,66 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Declarations of physics system.
+ */
+
+#pragma once
+
+#include "../Core.hpp"
+
+#include "nfCommon/Math/Vector.hpp"
+#include "nfCommon/Math/Matrix.hpp"
+
+#include <functional>
+
+
+namespace NFE {
+namespace Physics {
+
+
+// TODO translation + orientation
+using BodyProxyTransformUpdateCallback = std::function<void(const Math::Matrix&)>;
+using BodyProxyVelocityUpdateCallback = std::function<void(const Math::Vector&, const Math::Vector&)>;
+using ProxyID = uint32;
+
+static const ProxyID INVALID_PHYSICS_PROXY = std::numeric_limits<ProxyID>::max();
+
+
+/**
+ * Physics body proxy properties.
+ */
+struct NFE_ALIGN(16) BodyProxyInfo
+{
+    // body transformation matrix
+    Math::Matrix transform;
+
+    // moment of inertia vector
+    Math::Vector inertia;
+
+    Math::Vector velocity;
+    Math::Vector angularVelocity;
+
+    // collision shape resource, can be null
+    Resource::CollisionShape* collisionShape;
+
+    // this callback will be called when body was moved
+    BodyProxyTransformUpdateCallback transformUpdateCallback;
+
+    // this callback will be called when body's velocity has changed
+    BodyProxyVelocityUpdateCallback velocityUpdateCallback;
+
+    // Mass in kg. Zero means infinite mass
+    float mass;
+
+    // TODO material properties
+    // TODO drag/friction parameters
+    // TODO CCD
+
+    BodyProxyInfo()
+        : collisionShape(nullptr)
+        , mass(0.0f)
+    { }
+};
+
+} // namespace Physics
+} // namespace NFE
