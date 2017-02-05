0be518122856a9024b9d7572f48bb1f0159d2211
Michal Witanowski <witek902@gmail.com>
2017-02-05 15:38:37 +0100

    nfCommon: Better assertion
    Change-Id: I879ca7db273af10f6b052b2a36e0300c60bd5762

---
+++ nfEngine/nfCommon/Assertion.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Fatal assertion declarations
+ */
+
+#pragma once
+
+#include "nfCommon.hpp"
+#include "Logger.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+void NFCOMMON_API HandleFatalAssertion(const char* expressionStr, const char* functionStr, const char* srcFile,
+                                       int line, const char* str, ...);
+
+} // namespace Common
+} // namespace NFE
+
+
+// TODO assertions should be disable in "Final" build
+#define NFE_ASSERT(expression, ...)                                                                     \
+do {                                                                                                    \
+    if (!(expression))                                                                                  \
+    {                                                                                                   \
+        LOG_FATAL(__VA_ARGS__);                                                                         \
+        NFE::Common::HandleFatalAssertion(#expression, __FUNCTION__, __FILE__, __LINE__, __VA_ARGS__);  \
+    }                                                                                                   \
+} while (0)
