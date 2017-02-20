b9c781cd1bad74968e447f614297ebd089a04124
Michal Witanowski <witek902@gmail.com>
2017-02-21 00:29:15 +0100

    nfRendererDemo: Simple multithreaded scene
    Change-Id: If08f8bee7680e598ece14333bd81bb035a477382

---
+++ nfEngine/nfRendererDemo/Scenes/Multithreaded.cpp
@@ -0,0 +1,433 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Source file with multithreaded test scene definition
+ */
+
+#include "../PCH.hpp"
+
+#include "Multithreaded.hpp"
+#include "../Common.hpp"
+
+#include "nfCommon/Math/Math.hpp"
+#include "nfCommon/Math/Matrix.hpp"
+#include "nfCommon/Logger.hpp"
+
+#include <vector>
+#include <functional>
+
+
+using namespace NFE;
+using namespace NFE::Math;
+using namespace NFE::Renderer;
+
+namespace {
+
+struct VertexCBuffer
+{
+    Matrix viewMatrix;
+};
+
+struct PixelCBuffer
+{
+    Matrix viewMatrix;
+};
+
+} // namespace
+
+/// Helper creators for the Scene
+
+bool MultithreadedScene::CreateCommandRecorders()
+{
+    size_t num = mThreadPool.GetThreadsNumber();
+    mCommandRecorders.resize(num);
+
+    for (size_t i = 0; i < num; ++i)
+    {
+        mCommandRecorders[i] = mRendererDevice->CreateCommandRecorder();
+        if (!mCommandRecorders[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool MultithreadedScene::CreateShaders(BufferMode cbufferMode)
+{
+    mCBufferSlot = -1;
+
+    ShaderMacro vsMacro[] = { { "USE_CBUFFER", "1" } };
+    std::string vsPath = gShaderPathPrefix + "TestVS" + gShaderPathExt;
+    mVertexShader = CompileShader(vsPath.c_str(), ShaderType::Vertex, vsMacro, 1);
+    if (!mVertexShader)
+        return false;
+
+    ShaderMacro psMacro[] = { { "USE_TEXTURE", "0" } };
+    std::string psPath = gShaderPathPrefix + "TestPS" + gShaderPathExt;
+    mPixelShader = CompileShader(psPath.c_str(), ShaderType::Pixel, psMacro, 1);
+    if (!mPixelShader)
+        return false;
+
+    int numBindingSets = 0;
+    ResourceBindingSetPtr bindingSets[2];
+
+    // create binding set
+
+    mCBufferSlot = mVertexShader->GetResourceSlotByName("TestCBuffer");
+    if (mCBufferSlot < 0)
+        return false;
+
+    if (cbufferMode == BufferMode::Static || cbufferMode == BufferMode::Dynamic)
+    {
+        ResourceBindingDesc vertexShaderBinding(ShaderResourceType::CBuffer, mCBufferSlot);
+        mVSBindingSet = mRendererDevice->CreateResourceBindingSet(ResourceBindingSetDesc(&vertexShaderBinding, 1, ShaderType::Vertex));
+        if (!mVSBindingSet)
+            return false;
+
+        mVSBindingSlot = numBindingSets++;
+        bindingSets[mVSBindingSlot] = mVSBindingSet;
+    }
+
+    bool useVolatileCBufferBinding = cbufferMode == BufferMode::Volatile;
+    VolatileCBufferBinding volatileCBufferBinding(ShaderType::Vertex,
+                                                  ShaderResourceType::CBuffer,
+                                                  mCBufferSlot);
+
+    // create binding layout
+    mResBindingLayout = mRendererDevice->CreateResourceBindingLayout(
+        ResourceBindingLayoutDesc(bindingSets, numBindingSets, &volatileCBufferBinding, useVolatileCBufferBinding ? 1 : 0));
+    if (!mResBindingLayout)
+        return false;
+
+    return true;
+}
+
+bool MultithreadedScene::CreateVertexBuffer()
+{
+    // create vertex buffer
+    float vbDataExtra[] =
+    {
+        /// Vertex structure: pos.xyz, texCoord.uv, color.rgba
+
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+        -0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+        0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+    };
+
+    BufferDesc vbDesc;
+    vbDesc.type = BufferType::Vertex;
+    vbDesc.mode = BufferMode::Static;
+    vbDesc.size = sizeof(vbDataExtra);
+    vbDesc.initialData = vbDataExtra;
+    mVertexBuffer = mRendererDevice->CreateBuffer(vbDesc);
+    if (!mVertexBuffer)
+        return false;
+
+    VertexLayoutElement vertexLayoutElements[] =
+    {
+        { ElementFormat::R32G32B32_Float,       0, 0, false, 0 }, // position
+        { ElementFormat::R32G32_Float,          12, 0, false, 0 }, // tex-coords
+        { ElementFormat::R32G32B32A32_Float,    20, 0, false, 0 }, // color
+    };
+
+    VertexLayoutDesc vertexLayoutDesc;
+    vertexLayoutDesc.elements = vertexLayoutElements;
+    vertexLayoutDesc.numElements = 3;
+    mVertexLayout = mRendererDevice->CreateVertexLayout(vertexLayoutDesc);
+    if (!mVertexLayout)
+        return false;
+
+    PipelineStateDesc pipelineStateDesc;
+    pipelineStateDesc.rtFormats[0] = mBackbufferFormat;
+    pipelineStateDesc.vertexShader = mVertexShader;
+    pipelineStateDesc.pixelShader = mPixelShader;
+    pipelineStateDesc.blendState.independent = false;
+    pipelineStateDesc.blendState.rtDescs[0].enable = true;
+    pipelineStateDesc.primitiveType = PrimitiveType::Triangles;
+    pipelineStateDesc.vertexLayout = mVertexLayout;
+    pipelineStateDesc.resBindingLayout = mResBindingLayout;
+    mPipelineState = mRendererDevice->CreatePipelineState(pipelineStateDesc);
+    if (!mPipelineState)
+        return false;
+
+    return true;
+}
+
+bool MultithreadedScene::CreateIndexBuffer()
+{
+    // create index buffer
+    uint16 ibData[] =
+    {
+        0, 1, 2,
+        1, 3, 2
+    };
+
+    BufferDesc ibDesc;
+    ibDesc.type = BufferType::Index;
+    ibDesc.mode = BufferMode::Static;
+    ibDesc.size = sizeof(ibData);
+    ibDesc.initialData = ibData;
+    mIndexBuffer = mRendererDevice->CreateBuffer(ibDesc);
+    if (!mIndexBuffer)
+        return false;
+
+    return true;
+}
+
+bool MultithreadedScene::CreateConstantBuffer(BufferMode cbufferMode)
+{
+    const Matrix rotMatrix = MatrixRotationNormal(Vector(0.0f, 0.0f, 1.0f), NFE_MATH_PI);
+    mAngle = 0.0f;
+    mCBufferMode = cbufferMode;
+
+    BufferDesc cbufferDesc;
+    cbufferDesc.type = BufferType::Constant;
+    cbufferDesc.mode = cbufferMode;
+    cbufferDesc.size = sizeof(VertexCBuffer);
+
+    if (cbufferMode == BufferMode::Static)
+    {
+        cbufferDesc.initialData = &rotMatrix;
+    }
+
+    mConstantBuffer = mRendererDevice->CreateBuffer(cbufferDesc);
+    if (!mConstantBuffer)
+        return false;
+
+    if (cbufferMode == BufferMode::Static || cbufferMode == BufferMode::Dynamic)
+    {
+        // create and fill binding set instance
+        mVSBindingInstance = mRendererDevice->CreateResourceBindingInstance(mVSBindingSet);
+        if (!mVSBindingInstance)
+            return false;
+        if (!mVSBindingInstance->WriteCBufferView(0, mConstantBuffer))
+            return false;
+    }
+
+    return true;
+}
+
+
+/////////////////
+/// Subscenes ///
+/////////////////
+
+// Basic initialization, additionally to RT & BackBuffer shaders are compiled
+// Empty window should be visible
+bool MultithreadedScene::CreateSubSceneEmpty()
+{
+    mGridSize = 0;
+
+    if (!CreateCommandRecorders())
+        return false;
+
+    return CreateShaders(BufferMode::Static);
+}
+
+bool MultithreadedScene::CreateSubSceneNormal(BufferMode cbufferMode, int gridSize)
+{
+    mGridSize = gridSize;
+
+    if (!CreateCommandRecorders())
+        return false;
+
+    if (!CreateShaders(cbufferMode))
+        return false;
+
+    if (!CreateVertexBuffer())
+        return false;
+
+    if (!CreateIndexBuffer())
+        return false;
+
+    if (!CreateConstantBuffer(cbufferMode))
+        return false;
+
+    return true;
+}
+
+/////////////////////////////////////////////////
+/// MultithreadedScene methods and virtuals overridden ///
+/////////////////////////////////////////////////
+
+MultithreadedScene::MultithreadedScene()
+    : Scene("Basic")
+{
+    mCollectedCommandLists.resize(mThreadPool.GetThreadsNumber());
+
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneEmpty, this), "Empty");
+
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Dynamic, 1), "1x1, dynamic cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Dynamic, 4), "4x4, dynamic cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Dynamic, 10), "10x10, dynamic cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Dynamic, 30), "30x30, dynamic cbuffer");
+
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Volatile, 1), "1x1, volatile cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Volatile, 4), "4x4, volatile cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Volatile, 10), "10x10, volatile cbuffer");
+    RegisterSubScene(std::bind(&MultithreadedScene::CreateSubSceneNormal, this, BufferMode::Volatile, 30), "30x30, volatile cbuffer");
+}
+
+MultithreadedScene::~MultithreadedScene()
+{
+    Release();
+}
+
+void MultithreadedScene::ReleaseSubsceneResources()
+{
+    // clear resources
+    mConstantBuffer.reset();
+    mIndexBuffer.reset();
+    mVertexLayout.reset();
+    mVertexBuffer.reset();
+    mPixelShader.reset();
+    mVertexShader.reset();
+    mPipelineState.reset();
+
+    mVSBindingInstance.reset();
+    mResBindingLayout.reset();
+    mVSBindingSet.reset();
+
+    mCommandRecorders.clear();
+}
+
+bool MultithreadedScene::OnInit(void* winHandle)
+{
+    // create backbuffer connected with the window
+    BackbufferDesc bbDesc;
+    bbDesc.width = WINDOW_WIDTH;
+    bbDesc.height = WINDOW_HEIGHT;
+    bbDesc.format = mBackbufferFormat;
+    bbDesc.windowHandle = winHandle;
+    bbDesc.vSync = false;
+    mWindowBackbuffer = mRendererDevice->CreateBackbuffer(bbDesc);
+    if (!mWindowBackbuffer)
+        return false;
+
+    // create rendertarget that will render to the window's backbuffer
+    RenderTargetElement rtTarget;
+    rtTarget.texture = mWindowBackbuffer;
+    RenderTargetDesc rtDesc;
+    rtDesc.numTargets = 1;
+    rtDesc.targets = &rtTarget;
+    mWindowRenderTarget = mRendererDevice->CreateRenderTarget(rtDesc);
+    if (!mWindowRenderTarget)
+        return false;
+
+    return true;
+}
+
+void MultithreadedScene::DrawTask(const Common::TaskContext& ctx, int i, int j)
+{
+    // NOTE:
+    // this is extremely inefficient to draw single square in a separate task
+    // and submit it as a single command list, but it's not performance test
+
+    NFE_ASSERT(ctx.threadId < mCommandRecorders.size(), "Invalid thread ID");
+    const CommandRecorderPtr& recorder = mCommandRecorders[ctx.threadId];
+
+    // reset bound resources and set them once again
+    recorder->Begin();
+    recorder->SetViewport(0.0f, (float)WINDOW_WIDTH, 0.0f, (float)WINDOW_HEIGHT, 0.0f, 1.0f);
+    recorder->SetScissors(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    recorder->SetRenderTarget(mWindowRenderTarget);
+
+    int stride = 9 * sizeof(float);
+    int offset = 0;
+    const BufferPtr& vb = mVertexBuffer;
+    recorder->SetVertexBuffers(1, &vb, &stride, &offset);
+    recorder->SetIndexBuffer(mIndexBuffer, IndexBufferFormat::Uint16);
+
+    recorder->SetResourceBindingLayout(mResBindingLayout);
+    recorder->SetPipelineState(mPipelineState);
+
+    if (mCBufferMode == BufferMode::Static || mCBufferMode == BufferMode::Dynamic)
+        recorder->BindResources(mVSBindingSlot, mVSBindingInstance);
+    else if (mCBufferMode == BufferMode::Volatile)
+        recorder->BindVolatileCBuffer(0, mConstantBuffer);
+
+
+    const float scaleCoeff = 1.0f / static_cast<float>(mGridSize);
+    if (mConstantBuffer && (mCBufferMode == BufferMode::Dynamic || mCBufferMode == BufferMode::Volatile))
+    {
+        float xOffset = 2.0f * (static_cast<float>(i) + 0.5f) * scaleCoeff - 1.0f;
+        float yOffset = 2.0f * (static_cast<float>(j) + 0.5f) * scaleCoeff - 1.0f;
+        const float angle = mAngle + 5.0f * i + 7.0f * j;
+
+        const Matrix rotMatrix = MatrixRotationNormal(Vector(0.0f, 0.0f, 1.0f), angle);
+        const Matrix translationMatrix = MatrixTranslation3(Vector(xOffset, yOffset, 0.0f));
+        const Matrix scaleMatrix = MatrixScaling(Vector(scaleCoeff, scaleCoeff, 0.0f));
+
+        VertexCBuffer vertexCBufferData;
+        vertexCBufferData.viewMatrix = scaleMatrix * rotMatrix * translationMatrix;
+        recorder->WriteBuffer(mConstantBuffer, 0, sizeof(VertexCBuffer), &vertexCBufferData);
+    }
+
+    // draw
+    recorder->DrawIndexed(6, 1);
+
+    CommandListID commandList = recorder->Finish();
+    mCollectedCommandLists[ctx.threadId].push_back(commandList);
+}
+
+void MultithreadedScene::Draw(float dt)
+{
+    // apply rotation
+    mAngle += 2.0f * dt;
+    if (mAngle > NFE_MATH_2PI)
+        mAngle -= NFE_MATH_2PI;
+
+    // clear only once
+    CommandListID clearCommandList;
+    {
+        mCommandBuffer->Begin();
+        mCommandBuffer->SetViewport(0.0f, (float)WINDOW_WIDTH, 0.0f, (float)WINDOW_HEIGHT, 0.0f, 1.0f);
+        mCommandBuffer->SetScissors(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        mCommandBuffer->SetRenderTarget(mWindowRenderTarget);
+
+        // clear target
+        const Float4 color(0.0f, 0.0f, 0.0f, 1.0f);
+        mCommandBuffer->Clear(ClearFlagsColor, 1, nullptr, &color);
+
+        clearCommandList = mCommandBuffer->Finish();
+    }
+
+    for (int i = 0; i < mGridSize; ++i)
+    {
+        for (int j = 0; j < mGridSize; ++j)
+        {
+            using namespace std::placeholders;
+            mThreadPool.CreateTask(std::bind(&MultithreadedScene::DrawTask, this, _1, i, j));
+        }
+    }
+
+    mRendererDevice->Execute(clearCommandList);
+
+    // TODO execute some more command lists while recording
+
+    mThreadPool.WaitForAllTasks();
+
+    // execute recorded command lists
+    for (CollectedCommandLists& collectedCommandLists : mCollectedCommandLists)
+    {
+        for (CommandListID commandList : collectedCommandLists)
+        {
+            mRendererDevice->Execute(commandList);
+        }
+
+        collectedCommandLists.clear();
+    }
+
+    mWindowBackbuffer->Present();
+    mRendererDevice->FinishFrame();
+}
+
+void MultithreadedScene::Release()
+{
+    ReleaseSubsceneResources();
+    mWindowRenderTarget.reset();
+    mWindowBackbuffer.reset();
+    mCommandBuffer = nullptr;
+    mRendererDevice = nullptr;
+}
---
+++ nfEngine/nfRendererDemo/Scenes/Multithreaded.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Header file with multithreaded test scene declaration
+ */
+
+#pragma once
+
+#include "Scene.hpp"
+
+#include "nfCommon/ThreadPool.hpp"
+
+
+class MultithreadedScene : public Scene
+{
+    using CollectedCommandLists = std::vector<NFE::Renderer::CommandListID>;
+
+    std::vector<CollectedCommandLists> mCollectedCommandLists; // separate for each thread
+    std::vector<NFE::Renderer::CommandRecorderPtr> mCommandRecorders;
+
+    NFE::Renderer::BackbufferPtr mWindowBackbuffer;
+    NFE::Renderer::RenderTargetPtr mWindowRenderTarget;
+    NFE::Renderer::PipelineStatePtr mPipelineState;
+    NFE::Renderer::ShaderPtr mVertexShader;
+    NFE::Renderer::ShaderPtr mPixelShader;
+    NFE::Renderer::BufferPtr mVertexBuffer;
+    NFE::Renderer::VertexLayoutPtr mVertexLayout;
+    NFE::Renderer::BufferPtr mIndexBuffer;
+    NFE::Renderer::BufferPtr mConstantBuffer;
+
+    NFE::Renderer::ResourceBindingSetPtr mVSBindingSet;
+    NFE::Renderer::ResourceBindingLayoutPtr mResBindingLayout;
+    NFE::Renderer::ResourceBindingInstancePtr mVSBindingInstance;
+
+    int mCBufferSlot;
+
+    // Used for objects rotation in Constant Buffer scenes and onward
+    float mAngle;
+
+    // how many instances will be drawn?
+    int mGridSize;
+
+    int mVSBindingSlot;
+
+    // cbuffer mode
+    NFE::Renderer::BufferMode mCBufferMode;
+
+    NFE::Common::ThreadPool mThreadPool;
+
+    // Releases only subscene-related resources. Backbuffer, RT and BlendState stay intact.
+    void ReleaseSubsceneResources();
+
+    // Resource creators for subscenes
+    bool CreateCommandRecorders();
+    bool CreateShaders(NFE::Renderer::BufferMode cbufferMode);
+    bool CreateVertexBuffer();
+    bool CreateIndexBuffer();
+    bool CreateConstantBuffer(NFE::Renderer::BufferMode cbufferMode);
+
+    // Subscenes
+    bool CreateSubSceneEmpty();
+    bool CreateSubSceneNormal(NFE::Renderer::BufferMode cbufferMode, int gridSize);
+
+    // drawing task (will be called from thread pool)
+    void DrawTask(const NFE::Common::TaskContext& ctx, int i, int j);
+
+public:
+    MultithreadedScene();
+    ~MultithreadedScene();
+
+    bool OnInit(void* winHandle) override;
+    void Draw(float dt) override;
+    void Release() override;
+};
