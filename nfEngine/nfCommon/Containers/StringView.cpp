0ebd4227ab448423b44f526afe0f372cd5b01b04
Michal Witanowski <witek902@gmail.com>
2017-03-09 21:26:00 +0100

    nfCommon: Introduce StringView class
    Change-Id: Ic497cf2504d09f0d9a6c4030e3ae320d8fe9488e

---
+++ nfEngine/nfCommon/Containers/StringView.cpp
@@ -0,0 +1,214 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  StringView definitions.
+ */
+
+#include "PCH.hpp"
+#include "StringView.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+uint32 StringView::FindFirst(const StringView& subString) const
+{
+    if (subString.mLength > mLength || subString.mLength == 0)
+        return END;
+
+    // TODO: KMP algorithm
+
+    // naive O(N * M) algorithm
+    for (uint32 i = 0; i < mLength - subString.mLength + 1; ++i)
+    {
+        bool found = true;
+        for (uint32 j = 0; j < subString.mLength; ++j)
+        {
+            if (mData[i + j] != subString[j])
+            {
+                found = false;
+                break;
+            }
+        }
+
+        if (found)
+            return i;
+    }
+
+    // not found
+    return END;
+}
+
+uint32 StringView::FindLast(const StringView& subString) const
+{
+    if (subString.mLength > mLength || subString.mLength == 0)
+        return END;
+
+    // TODO: KMP algorithm
+
+    // naive O(N * M) algorithm
+    for (uint32 i = mLength - subString.mLength + 1; i-- > 0; )
+    {
+        bool found = true;
+        for (uint32 j = 0; j < subString.mLength; ++j)
+        {
+            if (mData[i + j] != subString[j])
+            {
+                found = false;
+                break;
+            }
+        }
+
+        if (found)
+            return i;
+    }
+
+    // not found
+    return END;
+}
+
+uint32 StringView::FindFirst(const char c) const
+{
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] == c)
+            return i;
+    }
+
+    // not found
+    return END;
+}
+
+uint32 StringView::FindLast(const char c) const
+{
+    for (uint32 i = mLength; i-- > 0; )
+    {
+        if (mData[i] == c)
+            return i;
+    }
+
+    // not found
+    return END;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+bool StringView::operator == (const StringView& other) const
+{
+    if (mLength != other.mLength)
+        return false;
+
+    if (mData == other.mData)
+        return true;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] != other.mData[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool StringView::operator != (const StringView& other) const
+{
+    if (mLength != other.mLength)
+        return true;
+
+    if (mData == other.mData)
+        return false;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] != other.mData[i])
+            return true;
+    }
+
+    return false;
+}
+
+bool StringView::operator < (const StringView& other) const
+{
+    if (mLength < other.mLength)
+        return true;
+
+    if (mLength > other.mLength)
+        return false;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] < other.mData[i])
+            return true;
+        else if (mData[i] > other.mData[i])
+            return false;
+    }
+
+    // string are equal
+    return false;
+}
+
+bool StringView::operator > (const StringView& other) const
+{
+    if (mLength > other.mLength)
+        return true;
+
+    if (mLength < other.mLength)
+        return false;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] > other.mData[i])
+            return true;
+        else if (mData[i] < other.mData[i])
+            return false;
+    }
+
+    // string are equal
+    return false;
+}
+
+bool StringView::operator <= (const StringView& other) const
+{
+    if (mLength < other.mLength)
+        return true;
+
+    if (mLength > other.mLength)
+        return false;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] < other.mData[i])
+            return true;
+        else if (mData[i] > other.mData[i])
+            return false;
+    }
+
+    // string are equal
+    return true;
+}
+
+bool StringView::operator >= (const StringView& other) const
+{
+    if (mLength > other.mLength)
+        return true;
+
+    if (mLength < other.mLength)
+        return false;
+
+    for (uint32 i = 0; i < mLength; ++i)
+    {
+        if (mData[i] > other.mData[i])
+            return true;
+        else if (mData[i] < other.mData[i])
+            return false;
+    }
+
+    // string are equal
+    return true;
+}
+
+
+} // namespace Common
+} // namespace NFE
