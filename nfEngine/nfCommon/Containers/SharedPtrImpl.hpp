6a4f82ef6928eb8f82a35283dad70244c016fd28
Michal Witanowski <witek902@gmail.com>
2017-05-14 01:07:09 +0200

    nfCommon: Introduce SharedPtr and WeakPtr
    * SharedPtr, WeakPtr and SharedPtrData template classes created.
    * Features:
        * custom deleter supported,
        * atomic reference counters (not all cases are handled properly),
        * implicit conversion is supported (from base type to parent),
        * casting functions are included (StaticCast and DynamicCast).
    * Unit tests written.
    
    Verification: Rebuild and run nfCommonTest.
    
    Change-Id: Idc79377183c012311785b9f72753b6810dfac4f7

---
+++ nfEngine/nfCommon/Containers/SharedPtrImpl.hpp
@@ -0,0 +1,248 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Shared pointer declaration
+ */
+
+#pragma once
+
+#include "SharedPtr.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+template<typename T>
+SharedPtr<T>::SharedPtr(T* ptr, const DeleterFunc& deleter)
+    : SharedPtrBase<T>(ptr, nullptr)
+{
+    if (this->mPointer)
+    {
+        this->mData = new SharedBlockType(ptr, deleter);
+    }
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(const SharedPtr& rhs)
+    : SharedPtrBase<T>(rhs.mPointer, rhs.mData)
+{
+    if (this->mData)
+    {
+        this->mData->AddWeakRef();
+        this->mData->AddStrongRef();
+    }
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(SharedPtr&& rhs)
+{
+    this->mPointer = rhs.mPointer;
+    this->mData = rhs.mData;
+
+    rhs.mPointer = nullptr;
+    rhs.mData = nullptr;
+}
+
+template<typename T>
+SharedPtr<T>::SharedPtr(UniquePtr<T>&& rhs)
+    : SharedPtr(rhs.Release()) // TODO copy deleter from UniquePtr
+{ }
+
+template<typename T>
+template<typename SourceType>
+SharedPtr<T>::SharedPtr(const SharedPtr<SourceType>& rhs)
+    : SharedPtrBase<T>(rhs.mPointer, rhs.mData)
+{
+    if (this->mData)
+    {
+        this->mData->AddWeakRef();
+        this->mData->AddStrongRef();
+    }
+}
+
+template<typename T>
+SharedPtr<T>::~SharedPtr()
+{
+    Reset();
+}
+
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator = (const SharedPtr& rhs)
+{
+    if (&rhs != this)
+    {
+        Reset();
+
+        this->mData = rhs.mData;
+        this->mPointer = rhs.mPointer;
+
+        if (this->mData)
+        {
+            this->mData->AddStrongRef();
+            this->mData->AddWeakRef();
+        }
+    }
+
+    return *this;
+}
+
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator = (SharedPtr&& rhs)
+{
+    Reset();
+
+    this->mPointer = rhs.mPointer;
+    this->mData = rhs.mData;
+
+    rhs.mPointer = nullptr;
+    rhs.mData = nullptr;
+
+    return *this;
+}
+
+template<typename T>
+SharedPtr<T>& SharedPtr<T>::operator = (UniquePtr<T>&& rhs)
+{
+    // TODO copy deleter from UniquePtr
+    Reset(rhs.Release());
+    return *this;
+}
+
+template<typename T>
+template<typename SourceType>
+SharedPtr<T>& SharedPtr<T>::operator = (const SharedPtr<SourceType>& rhs)
+{
+    *this = StaticCast<T>(rhs); // TODO
+    return *this;
+}
+
+template<typename T>
+void SharedPtr<T>::Reset(T* newPtr, const DeleterFunc& deleter)
+{
+    if (this->mData)
+    {
+        SharedBlockType* data = static_cast<SharedBlockType*>(this->mData);
+        T* ptr = data->GetPointer();
+        this->mData = nullptr;
+        this->mPointer = nullptr;
+
+        if (data->DelStrongRef())
+        {
+            const auto& oldObjectDeleter = data->GetDeleter();
+            oldObjectDeleter(ptr);
+        }
+
+        if (data->DelWeakRef())
+        {
+            delete data;
+        }
+    }
+
+    if (newPtr)
+    {
+        this->mPointer = newPtr;
+        this->mData = new SharedBlockType(newPtr, deleter);
+        // TODO possible optimization: reuse SharedPtrData
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+T** SharedPtr<T>::operator&()
+{
+    return &(this->mPointer);
+}
+
+template<typename T>
+T* SharedPtr<T>::operator->() const
+{
+    return this->mPointer;
+}
+
+template<typename T>
+T& SharedPtr<T>::operator*() const
+{
+    return *(this->mPointer);
+}
+
+template<typename T>
+T* SharedPtr<T>::Get() const
+{
+    return this->mPointer;
+}
+
+template<typename T>
+SharedPtr<T>::operator bool() const
+{
+    return this->mPointer != nullptr;
+}
+
+template<typename T>
+bool SharedPtr<T>::operator == (const SharedPtr& other) const
+{
+    return this->mPointer == other.mPointer;
+}
+
+template<typename T>
+bool SharedPtr<T>::operator != (const SharedPtr& other) const
+{
+    return this->mPointer != other.mPointer;
+}
+
+template<typename T>
+bool SharedPtr<T>::operator == (const T* other) const
+{
+    return this->mPointer == other;
+}
+
+template<typename T>
+bool SharedPtr<T>::operator != (const T* other) const
+{
+    return this->mPointer != other;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+template<typename T, typename ... Args>
+SharedPtr<T> MakeSharedPtr(Args&& ... args)
+{
+    // TODO single allocation for both object and control block
+    return SharedPtr<T>(new T(std::forward<Args>(args) ...));
+}
+
+template<typename TargetType, typename SourceType>
+SharedPtr<TargetType> StaticCast(const SharedPtr<SourceType>& source)
+{
+    SharedPtr<TargetType> result;
+    result.mData = source.mData;
+    result.mPointer = static_cast<TargetType*>(source.mPointer);
+
+    if (result.mData)
+    {
+        result.mData->AddWeakRef();
+        result.mData->AddStrongRef();
+    }
+
+    return result;
+}
+
+template<typename TargetType, typename SourceType>
+SharedPtr<TargetType> DynamicCast(const SharedPtr<SourceType>& source)
+{
+    SharedPtr<TargetType> result;
+    result.mData = source.mData;
+    result.mPointer = dynamic_cast<TargetType*>(source.mPointer);
+
+    if (result.mData)
+    {
+        result.mData->AddWeakRef();
+        result.mData->AddStrongRef();
+    }
+
+    return result;
+}
+
+} // namespace Common
+} // namespace NFE
---
+++ nfEngine/nfCommon/Containers/WeakPtrImpl.hpp
@@ -0,0 +1,187 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Weak pointer declaration
+ */
+
+#pragma once
+
+#include "WeakPtr.hpp"
+#include "../System/Assertion.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+template<typename T>
+WeakPtr<T>::WeakPtr(WeakPtr<T>&& rhs)
+{
+    this->mData = rhs.mData;
+    rhs.mData = nullptr;
+}
+
+template<typename T>
+WeakPtr<T>::WeakPtr(const WeakPtr<T>& rhs)
+    : SharedPtrBase<T>(rhs.mPointer, rhs.mData)
+{
+    if (this->mData)
+    {
+        this->mData->AddWeakRef();
+    }
+}
+
+template<typename T>
+WeakPtr<T>::WeakPtr(const SharedPtr<T>& rhs)
+    : SharedPtrBase<T>(rhs.mPointer, rhs.mData)
+{
+    if (this->mData)
+    {
+        this->mData->AddWeakRef();
+    }
+}
+
+template<typename T>
+WeakPtr<T>::~WeakPtr()
+{
+    Reset();
+}
+
+template<typename T>
+WeakPtr<T>& WeakPtr<T>::operator = (WeakPtr<T>&& rhs)
+{
+    Reset();
+    this->mData = rhs.mData;
+    rhs.mData = nullptr;
+    return *this;
+}
+
+template<typename T>
+WeakPtr<T>& WeakPtr<T>::operator = (const SharedPtr<T>& rhs)
+{
+    if (this->mData != rhs.mData)
+    {
+        Reset();
+
+        this->mData = rhs.mData;
+        if (this->mData)
+        {
+            this->mData->AddWeakRef();
+        }
+    }
+
+    return *this;
+}
+
+template<typename T>
+WeakPtr<T>& WeakPtr<T>::operator = (const WeakPtr<T>& rhs)
+{
+    if (&rhs != this)
+    {
+        Reset();
+
+        this->mData = rhs.mData;
+        if (this->mData)
+        {
+            this->mData->AddWeakRef();
+        }
+    }
+
+    return *this;
+}
+
+template<typename T>
+template<typename SourceType>
+WeakPtr<T>::WeakPtr(const WeakPtr<SourceType>& rhs)
+    : SharedPtrBase<T>(rhs.mPointer, rhs.mData)
+{
+    if (this->mData)
+    {
+        this->mData->AddWeakRef();
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+template<typename T>
+void WeakPtr<T>::Reset()
+{
+    if (this->mData)
+    {
+        if (this->mData->DelWeakRef())
+        {
+            delete this->mData;
+        }
+
+        this->mData = nullptr;
+    }
+}
+
+template<typename T>
+SharedPtr<T> WeakPtr<T>::Lock() const
+{
+    SharedPtr<T> result;
+
+    // TODO there's race condition here
+    if (this->mData && this->mData->GetNumStrongRefs() > 0)
+    {
+        this->mData->AddStrongRef();
+        this->mData->AddWeakRef();
+
+        result.mData = this->mData;
+        result.mPointer = this->mPointer;
+    }
+
+    return result;
+}
+
+template<typename T>
+bool WeakPtr<T>::operator == (const SharedPtr<T>& other) const
+{
+    const bool thisValid = Valid();
+    const bool otherValid = other;
+
+    if (!thisValid || !otherValid)
+    {
+        // if weak pointer is invalid, other pointer must be null or invalid too
+        return thisValid == otherValid;
+    }
+
+    return this->mData == other.mData;
+}
+
+template<typename T>
+bool WeakPtr<T>::operator == (const WeakPtr<T>& other) const
+{
+    const bool thisValid = Valid();
+    const bool otherValid = other.Valid();
+
+    if (!thisValid || !otherValid)
+    {
+        return thisValid == otherValid;
+    }
+
+    return this->mData == other.mData;
+}
+
+template<typename T>
+bool operator == (const SharedPtr<T>& lhs, const WeakPtr<T>& rhs)
+{
+    // reuse WeakPtr<T>::operator ==
+    return rhs == lhs;
+}
+
+template<typename T>
+bool WeakPtr<T>::Valid() const
+{
+    if (this->mData)
+    {
+        return this->mData->GetNumStrongRefs() > 0;
+    }
+
+    return false;
+}
+
+
+} // namespace Common
+} // namespace NFE
