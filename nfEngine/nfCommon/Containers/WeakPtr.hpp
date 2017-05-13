99c0afb37368e9448e81101f6a17232fdab6cae7
Michal Witanowski <witek902@gmail.com>
2017-05-14 01:07:09 +0200

    nfCommon: Introduce SharedPtr and WeakPtr
    Change-Id: Idc79377183c012311785b9f72753b6810dfac4f7

---
+++ nfEngine/nfCommon/Containers/WeakPtr.hpp
@@ -0,0 +1,79 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Weak pointer declaration
+ */
+
+#pragma once
+
+#include "WeakPtr.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+template<typename T>
+class WeakPtr
+{
+public:
+    WeakPtr();
+    WeakPtr(nullptr_t);
+
+    WeakPtr(const SharedPtr<T>& rhs);
+    WeakPtr(const WeakPtr<T>& rhs);
+    WeakPtr(WeakPtr<T>&& rhs);
+
+    WeakPtr& operator = (const SharedPtr<T>& rhs);
+    WeakPtr& operator = (const WeakPtr<T>& rhs);
+    WeakPtr& operator = (WeakPtr<T>&& rhs);
+
+    ~WeakPtr();
+
+    /**
+     * Check if the pointed object is still valid.
+     */
+    bool Valid() const;
+
+    /**
+     * Turn the weak pointer into a shared pointer.
+     */
+    SharedPtr<T> Lock() const;
+
+    /**
+     * Get number of object references.
+     */
+    uint32 RefCount() const;
+
+    /**
+     * Compare pointers.
+     */
+    bool operator == (const SharedPtr<T>& other) const;
+    bool operator == (const WeakPtr<T>& other) const;
+
+private:
+    using SharedBlock = SharedPtrData<T>;
+
+    // shared control block
+    SharedBlock* mData;
+
+    void Reset();
+
+    template<typename U>
+    friend bool operator == (const SharedPtr<U>& lhs, const WeakPtr<U>& rhs);
+};
+
+
+/**
+ * Compare shared pointer with weak pointer.
+ */
+template<typename T>
+bool operator == (const SharedPtr<T>& lhs, const WeakPtr<T>& rhs);
+
+
+} // namespace Common
+} // namespace NFE
+
+
+// Definitions go here:
+#include "WeakPtrImpl.hpp"
