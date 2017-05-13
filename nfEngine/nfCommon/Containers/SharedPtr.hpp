c53b3ae76164d2cfa177c8433e5de1a4048c24a2
Michal Witanowski <witek902@gmail.com>
2017-05-14 01:07:09 +0200

    nfCommon: Introduce SharedPtr and WeakPtr
    Change-Id: Idc79377183c012311785b9f72753b6810dfac4f7

---
+++ nfEngine/nfCommon/Containers/SharedPtr.hpp
@@ -0,0 +1,111 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Shared pointer declaration
+ */
+
+#pragma once
+
+#include "UniquePtr.hpp"
+#include "SharedPtrData.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+template<typename T>
+class WeakPtr;
+
+template<typename T>
+class SharedPtr
+{
+public:
+    using DeleterFunc = typename SharedPtrData<T>::DeleterFunc;
+
+    SharedPtr();
+    explicit SharedPtr(nullptr_t);
+    SharedPtr(T* ptr, const DeleterFunc& deleter = SharedPtrData<T>::GetDefaultDeleter());
+    SharedPtr(const SharedPtr& rhs);
+    SharedPtr(SharedPtr&& rhs);
+    ~SharedPtr();
+    SharedPtr& operator = (const SharedPtr& rhs);
+    SharedPtr& operator = (SharedPtr&& rhs);
+
+    template<typename U>
+    operator SharedPtr<U>() const
+    {
+        static_assert(std::is_base_of<U, T>::value, "Cannot downcast shared pointer");
+        return StaticCast<U>(*this);
+    }
+
+    /**
+     * Access pointed object.
+     */
+    T** operator&();
+    T* operator->() const;
+    T& operator*() const;
+    T* Get() const;
+
+    /**
+     * Set a new object.
+     */
+    void Reset(T* newPtr = nullptr, const DeleterFunc& deleter = SharedPtrData<T>::GetDefaultDeleter());
+
+    /**
+     * Check if pointer is not null.
+     */
+    operator bool() const;
+
+    /**
+     * Compare pointers.
+     */
+    bool operator == (const SharedPtr& other) const;
+    bool operator != (const SharedPtr& other) const;
+    bool operator == (const T* other) const;
+    bool operator != (const T* other) const;
+
+    /**
+     * Get number of object references (number SharedPtr objects pointing to the target).
+     */
+    uint32 RefCount() const;
+
+private:
+    using SharedBlock = SharedPtrData<T>;
+
+    // pointed object
+    T* mPointer;
+
+    // shared control block
+    SharedBlock* mData;
+
+    template<typename T, typename U>
+    friend SharedPtr<T> StaticCast(const SharedPtr<U>&);
+
+    template<typename T>
+    friend bool operator == (const SharedPtr<T>& lhs, const WeakPtr<T>& rhs);
+
+    template<typename T>
+    friend class WeakPtr;
+};
+
+
+/**
+ * Create shared pointer.
+ */
+template<typename T, typename ... Args>
+SharedPtr<T> MakeSharedPtr(Args&& ... args);
+
+/**
+ * Static cast a shared pointer.
+ */
+template<typename T, typename U>
+SharedPtr<T> StaticCast(const SharedPtr<U>& source);
+
+
+} // namespace Common
+} // namespace NFE
+
+
+// Definitions go here:
+#include "SharedPtrImpl.hpp"
