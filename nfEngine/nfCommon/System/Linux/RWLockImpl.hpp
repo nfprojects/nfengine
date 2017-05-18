a3e9d860e76892cd976a793d3919b326e92e962b
Michal Witanowski <witek902@gmail.com>
2017-05-18 22:24:08 +0200

    nfCommon: Introduce RWLock and ScopedLock classes
    * RWLock introduced (implemeted as SRWLOCK on Windows and as
      pthread_rwlock_t on Linux).
    * Generic ScopedLock written - works like std::unique_lock,
      but also allows for specifying locking policy (shared/exclusive).
    * Simple unit tests written - they are not meant to test RWLock
      in every aspect, but check if there is no deadlock in common
      scenarios.
    
    Verification: Build and run nfCommonTest
    
    Change-Id: If1c1dbd323356cecad9b14b0cba484225bdad61d

---
+++ nfEngine/nfCommon/System/Linux/RWLockImpl.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Reader/writer lock (mutex) Linux implementation.
+ */
+
+#pragma once
+
+#include "../RWLock.hpp"
+#include "../Asserion.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+RWLock::RWLock()
+{
+    int result = ::pthread_rwlock_init(&mLockObject, nullptr);
+    NFE_ASSERT(result == 0, "RW lock initialization failed");
+}
+
+RWLock::~RWLock()
+{
+    int result = ::pthread_rwlock_destroy(&mLockObject, nullptr);
+    NFE_ASSERT(result == 0, "RW lock destruction failed");
+}
+
+void RWLock::AcquireExclusive()
+{
+    int result = ::pthread_rwlock_wrlock(&mLockObject);
+    NFE_ASSERT(result == 0, "Acquiring exclusive lock failed");
+}
+
+void RWLock::ReleaseExclusive()
+{
+    int result = ::pthread_rwlock_unlock(&mLockObject);
+    NFE_ASSERT(result == 0, "Releasing exclusive lock failed");
+}
+
+void RWLock::AcquireShared()
+{
+    int result = ::pthread_rwlock_rdlock(&mLockObject);
+    NFE_ASSERT(result == 0, "Acquiring shared lock failed");
+}
+
+void RWLock::ReleaseShared()
+{
+    int result = ::pthread_rwlock_unlock(&mLockObject);
+    NFE_ASSERT(result == 0, "Releasing shared lock failed");
+}
+
+
+} // namespace Common
+} // namespace NFE
