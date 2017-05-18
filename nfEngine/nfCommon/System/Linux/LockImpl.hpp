bbc7867f3b07654e5d00c65777d68513542739aa
Michal Witanowski <witek902@gmail.com>
2017-05-18 22:24:08 +0200

    nfCommon: Introduce RWLock and ScopedLock classes
    * RWLock introduced (implemeted as SRWLOCK on Windows and as
      pthread_rwlock_t on Linux).
    * Generic ScopedLock written - works like std::unique_lock,
      but also allows for specifying locking policy (shared/exclusive).
    * Simple unit tests written - they are not meant to test RWLock
      in every aspect, but check if there is no deadlock in common
      scenarios.
    
    Verification: Build and run nfCommonTest
    
    Change-Id: If1c1dbd323356cecad9b14b0cba484225bdad61d

---
+++ nfEngine/nfCommon/System/Linux/LockImpl.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Reader/writer lock (mutex) Linux implementation.
+ */
+
+#pragma once
+
+#include "../Lock.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+RWLock::RWLock()
+{
+    InitializeSRWLock(&mLockObject);
+}
+
+void RWLock::AcquireExclusive()
+{
+    AcquireSRWLockExclusive(&mLockObject);
+}
+
+void RWLock::ReleaseExclusive()
+{
+    ReleaseSRWLockExclusive(&mLockObject);
+}
+
+void RWLock::AcquireShared()
+{
+    AcquireSRWLockShared(&mLockObject);
+}
+
+void RWLock::ReleaseShared()
+{
+    ReleaseSRWLockShared(&mLockObject);
+}
+
+} // namespace Common
+} // namespace NFE
