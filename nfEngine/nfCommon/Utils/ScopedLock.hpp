9d7fbb5750e9bc560a68b500b804c09f542333b3
Michal Witanowski <witek902@gmail.com>
2017-05-18 22:24:08 +0200

    nfCommon: Introduce RWLock and ScopedLock classes
    * RWLock introduced (implemeted as SRWLOCK on Windows and as
      pthread_rwlock_t on Linux).
    * Generic ScopedLock written - works like std::unique_lock,
      but also allows for specifying locking policy (shared/exclusive).
    * Simple unit tests written - they are not meant to test RWLock
      in every aspect, but check if there is no deadlock in common
      scenarios.
    
    Verification: Build and run nfCommonTest
    
    Change-Id: If1c1dbd323356cecad9b14b0cba484225bdad61d

---
+++ nfEngine/nfCommon/Utils/ScopedLock.hpp
@@ -0,0 +1,109 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Scoped lock helper class definitions.
+ */
+
+#pragma once
+
+#include "../nfCommon.hpp"
+#include "../System/RWLock.hpp"
+
+
+namespace NFE {
+namespace Common {
+
+
+/**
+ * Helper traits class for acquiring/releasing an generic exclusive lock.
+ */
+template<typename LockType>
+class ExclusiveLockPolicy
+{
+public:
+    static void Acquire(LockType& lock) { lock.AcquireExclusive(); }
+    static void Release(LockType& lock) { lock.ReleaseExclusive(); }
+};
+
+
+/**
+ * Helper traits class for acquiring/releasing a generic shared lock.
+ */
+template<typename LockType>
+class SharedLockPolicy
+{
+public:
+    static void Acquire(LockType& lock) { lock.AcquireShared(); }
+    static void Release(LockType& lock) { lock.ReleaseShared(); }
+};
+
+
+/**
+ * Scoped lock - helper class for wrapping a generic lock object (e.g. RWLock),
+ * that acquires a lock in the constructor and released the lock in the destructor.
+ * Locking policy (shared / exclusive) is given in the template argument.
+ */
+template <typename LockType, typename LockingPolicy>
+class ScopedLock final
+{
+public:
+    ScopedLock(LockType& lock, bool createLocked = true)
+        : mLock(lock)
+        , mIsLocked(createLocked)
+    {
+        if (createLocked)
+        {
+            LockingPolicy::Acquire(mLock);
+        }
+    }
+
+    ~ScopedLock()
+    {
+        if (mIsLocked)
+        {
+            LockingPolicy::Release(mLock);
+        }
+    }
+
+    void Lock()
+    {
+        if (!mIsLocked)
+        {
+            LockingPolicy::Acquire(mLock);
+            mIsLocked = true;
+        }
+    }
+
+    void Unlock()
+    {
+        if (mIsLocked)
+        {
+            LockingPolicy::Release(mLock);
+            mIsLocked = false;
+        }
+    }
+
+    constexpr bool IsLocked() const
+    {
+        return mIsLocked;
+    }
+
+    ScopedLock& operator = (const ScopedLock&) = default;
+    ScopedLock& operator = (ScopedLock&&) = default;
+
+private:
+    // ScopedLock cannot be copied
+    ScopedLock(const ScopedLock&) = delete;
+    ScopedLock(ScopedLock&&) = delete;
+
+    LockType& mLock;
+    bool mIsLocked;
+};
+
+
+using ScopedSharedLock = ScopedLock<RWLock, SharedLockPolicy<RWLock>>;
+using ScopedExclusiveLock = ScopedLock<RWLock, ExclusiveLockPolicy<RWLock>>;
+
+
+} // namespace Common
+} // namespace NFE
