aea3e1088c63a993acd613fbad7e52b840be72a1
Michal Witanowski <witek902@gmail.com>
2017-05-18 22:24:08 +0200

    nfCommon: Introduce RWLock and ScopedLock classes
    * RWLock introduced (implemeted as SRWLOCK on Windows and as
      pthread_rwlock_t on Linux).
    * Generic ScopedLock written - works like std::unique_lock,
      but also allows for specifying locking policy (shared/exclusive).
    * Simple unit tests written - they are not meant to test RWLock
      in every aspect, but check if there is no deadlock in common
      scenarios.
    
    Verification: Build and run nfCommonTest
    
    Change-Id: If1c1dbd323356cecad9b14b0cba484225bdad61d

---
+++ nfEngine/nfCommon/System/RWLock.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file
+ * @author Witek902 (witek902@gmail.com)
+ * @brief  Reader/writer lock (mutex) declaration.
+ */
+
+#pragma once
+
+#include "../nfCommon.hpp"
+
+#if defined(WIN32)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <Windows.h>
+#endif // defined(WIN32)
+
+
+namespace NFE {
+namespace Common {
+
+/**
+ * Reader/writer lock (mutex).
+ * There can be multiple shared locks or only one exclusive lock acquired at the same time.
+ */
+class RWLock final
+{
+public:
+    NFE_INLINE RWLock();
+    NFE_INLINE ~RWLock();
+
+    /**
+     * Acquire a lock in exclusive mode.
+     */
+    NFE_INLINE void AcquireExclusive();
+
+    /**
+     * Release a lock that was acquired in exclusive mode.
+     */
+    NFE_INLINE void ReleaseExclusive();
+
+    /**
+     * Acquire a lock in shared mode.
+     */
+    NFE_INLINE void AcquireShared();
+
+    /**
+     * Release a lock that was acquired in shared mode.
+     */
+    NFE_INLINE void ReleaseShared();
+
+private:
+    // RWLock cannot be copied nor moved
+    RWLock(const RWLock&) = delete;
+    RWLock(RWLock&&) = delete;
+    RWLock& operator = (const RWLock&) = delete;
+    RWLock& operator = (RWLock&&) = delete;
+
+#if defined(WIN32)
+    ::SRWLOCK mLockObject;
+#elif defined(__LINUX__) | defined(__linux__)
+    ::pthread_rwlock_t mLockObject;
+#endif
+};
+
+
+} // namespace Common
+} // namespace NFE
+
+
+#if defined(WIN32)
+#include "Win/RWLockImpl.hpp"
+#elif defined(__LINUX__) | defined(__linux__)
+#include "Linux/RWLockImpl.hpp"
+#endif
---
+++ nfEngine/nfCommonTest/TestCases/LockTest.cpp
@@ -0,0 +1,141 @@
+/**
+ * @file
+ * @author  Witek902 (witek902@gmail.com)
+ * @brief   Unit tests for ScopedLock and RWLock classes.
+ */
+
+#include "PCH.hpp"
+#include "nfCommon/Utils/ScopedLock.hpp"
+#include "nfCommon/System/Thread.hpp"
+#include "nfCommon/Utils/Latch.hpp"
+
+
+using namespace NFE;
+using namespace NFE::Common;
+
+
+TEST(LockTest, SimpleSharedLock)
+{
+    RWLock lock;
+    {
+        ScopedSharedLock scopedLock(lock);
+        ASSERT_TRUE(scopedLock.IsLocked());
+    }
+}
+
+TEST(LockTest, SimpleExclusiveLock)
+{
+    RWLock lock;
+    {
+        ScopedExclusiveLock scopedLock(lock);
+        ASSERT_TRUE(scopedLock.IsLocked());
+    }
+}
+
+TEST(LockTest, ExlusiveLock_Multithreaded)
+{
+    const uint32 maxIterations = 10000;
+
+    uint32 sharedCounter = 0; // non atomic counter incremented from multiple threads
+    RWLock lock; // for synchronizing counter access
+
+    const auto func = [&sharedCounter, &lock, maxIterations]()
+    {
+        for (uint32 i = 0; i < maxIterations; ++i)
+        {
+            ScopedExclusiveLock scopedLock(lock);
+            sharedCounter++;
+        }
+    };
+
+    const size_t numThreads = std::min<size_t>(1024, std::thread::hardware_concurrency());
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads.emplace_back(func);
+    }
+
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads[i].join();
+    }
+
+    ASSERT_EQ(sharedCounter, static_cast<uint32>(numThreads) * maxIterations);
+}
+
+TEST(LockTest, SharedLock_Multithreaded)
+{
+    RWLock testLock; // the lock being tested
+
+    using LockType = std::unique_lock<std::mutex>;
+
+    uint32 counter;
+    std::condition_variable hasEnteredCV;
+    std::mutex hasEnteredMutex;
+
+    bool canContinue;
+    std::condition_variable canContinueCV;
+    std::mutex canContinueMutex;
+
+
+    const auto func = [&]()
+    {
+        ScopedSharedLock scopedLock(testLock);
+
+        // notify main thread about reaching synchronization point
+        {
+            LockType lock(hasEnteredMutex);
+            counter++;
+            hasEnteredCV.notify_one();
+        }
+
+        // if we are here, it means that all the threads are inside shared lock
+
+        // exit the shared lock only when notified by the main thread
+        {
+            LockType lock(canContinueMutex);
+            while (!canContinue)
+            {
+                canContinueCV.wait(lock);
+            }
+        }
+    };
+
+    counter = 0;
+    canContinue = false;
+
+    // spawn threads
+    const size_t numThreads = std::min<size_t>(1024, std::thread::hardware_concurrency());
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads.emplace_back(func);
+    }
+
+    // wait until all the threads reach synchronization point
+    {
+        LockType lock(hasEnteredMutex);
+        while (counter < static_cast<uint32>(numThreads))
+        {
+            hasEnteredCV.wait(lock);
+        }
+    }
+
+    // when program execution reaches this place, this means
+    // that counter has been incremented to maximum value
+    ASSERT_EQ(counter, static_cast<uint32>(numThreads));
+
+    // notify threads to continue
+    {
+        LockType lock(canContinueMutex);
+        canContinue = true;
+        canContinueCV.notify_all();
+    }
+
+    for (size_t i = 0; i < numThreads; ++i)
+    {
+        threads[i].join();
+    }
+
+    ASSERT_EQ(counter, static_cast<uint32>(numThreads));
+}
