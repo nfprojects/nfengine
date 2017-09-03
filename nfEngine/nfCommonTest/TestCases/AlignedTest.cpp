7383f42fb47819c0bb4f99d8efe602e6a94de336
Michal Witanowski <witek902@gmail.com>
2017-09-03 16:51:59 +0200

    nfCommon: Improve Aligned class
    - Added support for arrays
    - Removed unnecessary virtual destructor which forced all child classes
      to be polymorphic
    
    Verification: Build all and run tests.
    
    Change-Id: I1f06fe25d1e94184159b600c8ca4b894e25bc105

--- nfEngine/nfCommonTest/TestCases/AlignedTest.cpp
+++ nfEngine/nfCommonTest/TestCases/AlignedTest.cpp
@@ -14,28 +14,47 @@ using namespace NFE::Common;
 
 namespace {
 
-struct TestClass16 : public Aligned<16>
+struct NFE_ALIGN(16) TestClass16 : public Aligned<16>
 {
     int a;
+
+    TestClass16() : a(0) { }
+    explicit TestClass16(int val) : a(val) { }
 };
 
-struct TestClass64 : public Aligned<64>
+struct NFE_ALIGN(64) TestClass64 : public Aligned<64>
 {
     int a;
+
+    TestClass64() : a(0) { }
+    explicit TestClass64(int val) : a(val) { }
 };
 
-struct TestClass4096 : public Aligned<4096>
+struct NFE_ALIGN(4096) TestClass4096 : public Aligned<4096>
 {
     int a;
+
+    TestClass4096() : a(0) { }
+    explicit TestClass4096(int val) : a(val) { }
 };
 
 } // namespace
+
+
+static_assert(alignof(TestClass16) == 16, "Invalid alignment of test class");
+static_assert(alignof(TestClass64) == 64, "Invalid alignment of test class");
+static_assert(alignof(TestClass4096) == 4096, "Invalid alignment of test class");
+
 
 TEST(Aligned, AlignedObject)
 {
     std::unique_ptr<TestClass16> testObject16(new TestClass16);
     std::unique_ptr<TestClass64> testObject64(new TestClass64);
     std::unique_ptr<TestClass4096> testObject4096(new TestClass4096);
+
+    *testObject16 = TestClass16(123);
+    *testObject64 = TestClass64(123);
+    *testObject4096 = TestClass4096(123);
 
     EXPECT_EQ(0, reinterpret_cast<size_t>(testObject16.get()) % 16);
     EXPECT_EQ(0, reinterpret_cast<size_t>(testObject64.get()) % 64);
@@ -55,4 +74,24 @@ TEST(Aligned, AlignedContainer)
     EXPECT_EQ(0, reinterpret_cast<size_t>(vector16.data()) % 16);
     EXPECT_EQ(0, reinterpret_cast<size_t>(vector64.data()) % 64);
     EXPECT_EQ(0, reinterpret_cast<size_t>(vector4096.data()) % 4096);
+}
+
+TEST(Aligned, AlignedArray)
+{
+    const size_t numElements = 100;
+
+    std::unique_ptr<TestClass16[]> testObject16(new TestClass16[numElements]);
+    std::unique_ptr<TestClass64[]> testObject64(new TestClass64[numElements]);
+    std::unique_ptr<TestClass4096[]> testObject4096(new TestClass4096[numElements]);
+
+    for (size_t i = 0; i < numElements; ++i)
+    {
+        testObject16[i] = TestClass16(123);
+        testObject64[i] = TestClass64(123);
+        testObject4096[i] = TestClass4096(123);
+    }
+
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject16.get()) % 16);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject64.get()) % 64);
+    EXPECT_EQ(0, reinterpret_cast<size_t>(testObject4096.get()) % 4096);
 }
