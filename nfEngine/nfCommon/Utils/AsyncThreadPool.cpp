b41470b1bbe05f40085a81f8bf2868e3b35895eb
Michal Witanowski <witek902@gmail.com>
2017-09-27 23:25:32 +0200

    nfCommon: Refactor AsyncThreadPool
    Change-Id: Ib25bdb0a65057a26d7c86a308acc1d3bd9cca9c7

--- nfEngine/nfCommon/Utils/AsyncThreadPool.cpp
+++ nfEngine/nfCommon/Utils/AsyncThreadPool.cpp
@@ -6,24 +6,156 @@
 
 #include "PCH.hpp"
 #include "AsyncThreadPool.hpp"
-
+#include "ScopedLock.hpp"
+#include "Logger/Logger.hpp"
 
 namespace NFE {
 namespace Common {
 
-AsyncFunc::AsyncFunc(AsyncFuncCallback callback)
-    : ptr(0)
+
+IAsyncDependency::IAsyncDependency(AsyncThreadPool& pool)
+    : mPool(pool)
+    , mFullfilled(false)
+{
+}
+
+void IAsyncDependency::OnDepenencyFulfilled()
+{
+    const bool fulfilledBefore = mFullfilled.exchange(true);
+    NFE_ASSERT(!fulfilledBefore, "Dependency cannot be fulfilled twice");
+
+    for (const AsyncTaskPtr& childTask : mReverseDependencies)
+    {
+        childTask->Enqueue();
+    }
+
+    // clear the list to break the cycle
+    mReverseDependencies.Clear();
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+AsyncTask::AsyncTask(AsyncThreadPool& pool, const AsyncFuncCallback& callback, const AsyncTaskSetup& setup)
+    : IAsyncDependency(pool)
+    , mParent(setup.parent)
+    , mDependency(setup.dependency)
     , mCallback(callback)
+    , mState(State::Pending)
 {}
+
+AsyncTask::~AsyncTask()
+{
+    NFE_ASSERT(mState != State::Executing, "Async tasks has 'executing' state while destruction. This should be not possible");
+}
+
+void AsyncTask::Schedule()
+{
+    const State stateBefore = mState.exchange(State::Sheduled);
+    NFE_ASSERT(stateBefore == State::Pending, "Schedule() must be called on a non-scheduled task only");
+
+    if (mDependency)
+    {
+        ScopedMutexLock lock(GetPool().mTasksQueueMutex);
+
+        // check if the dependency is already fulfilled
+        // if so, enqueue immediately
+        if (mDependency->IsFulfilled())
+        {
+            Enqueue();
+        }
+
+        // clear the pointer to break the cycle
+        mDependency.Reset();
+    }
+    else
+    {
+        Enqueue();
+    }
+}
+
+void AsyncTask::Enqueue()
+{
+    const State stateBefore = mState.exchange(State::Enqueued);
+    NFE_ASSERT(stateBefore == State::Sheduled, "Enqueue() must be called on a scheduled task only");
+
+    // TODO SharedFromThis?
+    GetPool().EnqueueTask(this);
+}
+
+void AsyncTask::OnExecute()
+{
+    {
+        const State stateBefore = mState.exchange(State::Executing);
+        NFE_ASSERT(stateBefore == State::Enqueued, "Execute() must be called on a scheduled task only");
+    }
+
+    if (mCallback)
+    {
+        const AsyncTaskContext context =
+        {
+            *this,
+            0,      // TODO thread ID
+        };
+
+        mCallback(context);
+    }
+
+    {
+        const State stateBefore = mState.exchange(State::Executed);
+        NFE_ASSERT(stateBefore == State::Executing, "Task state has been changed externally during execution");
+    }
+
+    OnFinished();
+}
+
+void AsyncTask::OnFinished()
+{
+    if (--mTasksLeft > 0)
+    {
+        // there are still some child tasks running
+        return;
+    }
+
+    // TODO notify the pool about the finished task
+
+    // propagate the update to parent
+    if (mParent)
+    {
+        mParent->OnFinished();
+
+        // cleanup the pointer to break the cycle
+        mParent.Reset();
+    }
+
+    // notify waiting tasks
+    OnDepenencyFulfilled();
+}
+
+void AsyncTask::Wait() const
+{
+    // TODO
+}
+
+void AsyncTask::UpdateState(const State newState)
+{
+    // TODO verify state transition
+    mState = newState;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
 
 AsyncThreadPool::AsyncThreadPool()
 {
-    mLastTaskId = 0;
     mStarted = true;
 
     // spawn worker threads
     for (size_t i = 0; i < std::thread::hardware_concurrency(); ++i)
+    {
         mWorkerThreads.PushBack(std::thread(&AsyncThreadPool::SchedulerCallback, this));
+    }
 }
 
 AsyncThreadPool::~AsyncThreadPool()
@@ -31,22 +163,20 @@ AsyncThreadPool::~AsyncThreadPool()
     mStarted = false;
     {
         ScopedMutexLock lock(mTasksQueueMutex);
-        mTaskQueueTask.SignalAll();
+        mTaskQueueCV.SignalAll();
     }
 
     // cleanup worker threads
     for (auto& thread : mWorkerThreads)
+    {
         thread.join();
+    }
     mWorkerThreads.Clear();
-
-    // cleanup tasks
-    for (auto taskPair : mTasks)
-        delete taskPair.second;
 }
 
 void AsyncThreadPool::SchedulerCallback()
 {
-    AsyncFunc* currTask = nullptr;
+    AsyncTaskPtr currTask;
 
     for (;;)
     {
@@ -55,10 +185,14 @@ void AsyncThreadPool::SchedulerCallback()
 
             // wait for new task
             while (mStarted && mTasksQueue.empty())
-                mTaskQueueTask.Wait(lock);
+            {
+                mTaskQueueCV.Wait(lock);
+            }
 
             if (!mStarted)
+            {
                 return;
+            }
 
             // pop a task from the queue
             currTask = mTasksQueue.front();
@@ -66,75 +200,31 @@ void AsyncThreadPool::SchedulerCallback()
         }
 
         // execute
-        currTask->mCallback();
-
-        // mark as done and notify waiting threads
-        {
-            ScopedMutexLock lock(mTasksMutex);
-            mTasks.Erase(currTask->ptr);
-            mTasksMutexCV.SignalAll();
-
-            // cleanup
-            delete currTask;
-            currTask = nullptr;
-        }
+        currTask->OnExecute();
     }
 }
 
-AsyncFuncID AsyncThreadPool::Enqueue(AsyncFuncCallback function)
+AsyncTaskPtr AsyncThreadPool::CreateTask(const AsyncFuncCallback& function, const AsyncTaskSetup& setup)
 {
-    AsyncFuncID taskPtr = mLastTaskId++; //< generate task id
-    AsyncFunc* task = new AsyncFunc(function);
-    task->ptr = taskPtr;
-
+    if (setup.parent)
     {
-        ScopedMutexLock lock(mTasksMutex);
-        mTasks.Insert(taskPtr, task);
+        NFE_ASSERT(setup.parent->GetState() == AsyncTask::State::Pending, "Parent task must be in pending state");
+        setup.parent->mTasksLeft++;
     }
 
-    {
-        ScopedMutexLock lock(mTasksQueueMutex);
-        mTasksQueue.push(task);
-        mTaskQueueTask.SignalAll();
-    }
+    AsyncTaskPtr task = MakeSharedPtr<AsyncTask>(*this, function, setup);
 
-    return taskPtr;
+    return task;
 }
 
-AsyncFunc* AsyncThreadPool::GetTask(const AsyncFuncID& taskID) const
+void AsyncThreadPool::EnqueueTask(const AsyncTaskPtr& task)
 {
-    // assume called in mTasksMutex lock
+    ScopedMutexLock lock(mTasksQueueMutex);
 
-    auto it = mTasks.Find(taskID);
-    if (it != mTasks.End())
-        return it->second;
-
-    return nullptr;
+    mTasksQueue.push(task);
+    mTaskQueueCV.SignalAll();
 }
 
-bool AsyncThreadPool::IsTaskFinished(const AsyncFuncID& taskID)
-{
-    ScopedMutexLock lock(mTasksMutex);
-    return GetTask(taskID) == nullptr;
-}
-
-void AsyncThreadPool::WaitForTask(const AsyncFuncID& taskID)
-{
-    ScopedMutexLock lock(mTasksMutex);
-    while (GetTask(taskID) != nullptr)
-    {
-        mTasksMutexCV.Wait(lock);
-    }
-}
-
-void AsyncThreadPool::WaitForAllTasks()
-{
-    ScopedMutexLock lock(mTasksMutex);
-    while (!mTasks.Empty())
-    {
-        mTasksMutexCV.Wait(lock);
-    }
-}
 
 } // namespace Common
 } // namespace NFE
