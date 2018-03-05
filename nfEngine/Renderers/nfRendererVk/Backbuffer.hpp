6cf70b71aafc1f7265d0665a1fdb116dc959954a
Lukasz Kostyra <costyrra.xl@gmail.com>
2018-03-05 09:35:23 +0100

    nfRendererVk: Overhauling resources and defines
    * CHECK_VKRESULT became more descriptive VK_RETURN_FALSE_IF_FAILED.
    * Added Tools and VkRAII template for simple Vulkan object management.
    * Shader's set-binding pair is now an union.
    * Certain parts rewritten using NFE Common library to get rid of STL
      and exception-throwing elements.
    
    Change-Id: I12ab136c413891388d455cd25a7a1cacafeb6265

--- nfEngine/Renderers/nfRendererVk/Backbuffer.hpp
+++ nfEngine/Renderers/nfRendererVk/Backbuffer.hpp
@@ -8,6 +8,7 @@
 
 #include "../RendererInterface/Backbuffer.hpp"
 #include "Texture.hpp"
+#include "VkRAII.hpp"
 
 namespace NFE {
 namespace Renderer {
@@ -33,8 +34,7 @@ class Backbuffer : public IBackbuffer, public Texture
     VkSurfaceCapabilitiesKHR mSurfaceCapabilities;
     VkSwapchainKHR mSwapchain;
     VkPresentModeKHR mSwapPresentMode;
-    Common::DynArray<VkCommandBuffer> mPresentCommandBuffers;
-    Common::DynArray<VkCommandBuffer> mPostPresentCommandBuffers;
+    VkRAII<VkFence> mImageAcquiredFence;
 
     // platform-specific surface creator
     bool CreateSurface(const BackbufferDesc& desc);
@@ -48,14 +48,13 @@ class Backbuffer : public IBackbuffer, public Texture
     bool SelectBufferCount();
     bool CreateSwapchain(const BackbufferDesc& desc);
     bool CreateSwapchainImageViews();
-    bool BuildPresentCommandBuffers();
-    bool AcquireNextImage();
 
 public:
     Backbuffer();
     ~Backbuffer();
 
     bool Init(const BackbufferDesc& desc);
+    bool AcquireNextImage();
 
     bool Resize(int newWidth, int newHeight) override;
     bool Present() override;
