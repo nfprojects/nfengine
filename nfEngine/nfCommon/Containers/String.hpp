5bb8a86b5719bca797608b3a94e3c2d493ef8c87
Lukasz Kostyra <costyrra.xl@gmail.com>
2018-03-06 15:07:06 +0100

    nfCommon: Add utils useful for String parsing
    Added String::SubStr() and SplitString() util function.
    
    Change-Id: I37783e2c087136b7169dbf07f129adc26dfc5338

--- nfEngine/nfCommon/Containers/String.hpp
+++ nfEngine/nfCommon/Containers/String.hpp
@@ -35,6 +35,7 @@ public:
     NFE_INLINE String(const String& string);
     NFE_INLINE String(const char* string);
     String(const char* str, uint32 length);
+    String(uint32 length);
 
     // construct a string from a fixed-sized array
     // array size must be less than MaxInternalLength
@@ -98,6 +99,15 @@ public:
     String& Replace(uint32 index, uint32 numCharacters, const StringView& other);
     NFE_INLINE String& Replace(uint32 index, uint32 numCharacters, const String& string);
     NFE_INLINE String& Replace(uint32 index, uint32 numCharacters, const char* string);
+
+    /**
+     * Returns substring from position A to B.
+     * @param   index           Location of first character to be included in substring.
+     *                          If it's out of range, assertion will be called.
+     * @param   numCharacters   Length of the substring. If exceeds string size,
+     *                          returns substring from @p index to the end.
+     */
+    String SubStr(uint32 index, uint32 numCharacters = UINT32_MAX);
 
     /**
      * Reserve memory for given string length (in characters, without null-termination).
