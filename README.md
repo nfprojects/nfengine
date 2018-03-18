3eb1129b394b48e45d8e377ba665c59b750be600
Lukasz Kostyra <costyrra.xl@gmail.com>
2018-03-18 13:41:15 +0100

    nfRendererVk: Link to Vulkan library
    Vulkan renderer now properly integrates with Vulkan SDK, which is
    installed on developer's OS.
    
    Extensions code was left as a placeholder in case we'll need to use
    any extensions, which are distributed outside of Vulkan Core spec.
    
    Change-Id: I81acda898b2e7544cda83c31fc080ff15eef883c

--- README.md
+++ README.md
@@ -10,37 +10,36 @@ Building the project - Windows
 
 To make the code compilable, the following requirements have to be met:
 
-1. Installed Visual Studio 2015 with Windows SDK.
-2. Pulled external dependencies from [here](http://www.github.com/nfprojects/nfenginedeps)
+1. Installed Visual Studio 2017 with Windows SDK.
+2. Installed Vulkan SDK (if you want to build Vulkan renderer; otherwise, it can be excluded from building)
+3. Pulled external dependencies from [here](http://www.github.com/nfprojects/nfenginedeps)
     * **"nfEngineDeps"** directory will be created by using git submodules inside repo - fetch its contents by using `git submodule update --init` command
     * **NOTE:** Dependencies need to be built separately from engine. See README.md inside nfEngineDeps repo for more info.
-3. Acquired additional resources from our server with _syncher.py_ script.
+4. Acquired additional resources from our server with _syncher.py_ script.
     * Download resources with `Scripts/syncher.py init` command - all needed data will automatically download into correct directory.
-4. Project is now buildable, however resources still need to be processed to NFE-compatible format.
+5. Project is now buildable, however resources still need to be processed to NFE-compatible format.
     * Build ResourceManager project (it's available inside Tools directory)
     * Run built _ResourceManager.exe_ binary. The program will automatically gather and process files downloaded by _syncher.py_ script.
-
-**NOTE:** In current state, not all projects are right now buildable inside Visual Studio. Following projects are guaranteed to fail the build:
-* nfRendererOGL4
-* PackerTool
-
-It is unsure whether we will make them buildable again, or remove them - for now it is recommended to manually unload them inside Visual Studio solution.
 
 
 Building the project - Linux
 ----------------------------
 
-Currently, buildable parts of nfEngine are: nfCommon, nfCommonTest, nfCommonPerfTest and nfRendererDemo. Requirements:
+Currently buildable parts of nfEngine are: nfCommon, nfCommonTest, nfCommonPerfTest, nfRendererVk and nfRendererDemo. Requirements:
 
 1. Installed a C++11-compatible compiler (right now the only compiler tested to work is GCC 4.7 and higher versions).
 2. Installed CMake 2.6 or higher.
-3. Pulled external dependencies from [here](http://www.github.com/nfprojects/nfenginedeps):
+3. Installed Vulkan SDK
+    * **NOTE:** Make sure that `VULKAN_SDK` env variable is set in the system and points to dir x86\_64 inside Vulkan SDK.
+4. Pulled external dependencies from [here](http://www.github.com/nfprojects/nfenginedeps):
     * **"nfEngineDeps"** directory will be created by using git submodules inside repo - fetch its contents by using `git submodule update --init`
     * **NOTE:** Dependencies need to be built separately from engine. See README.md inside nfEngineDeps repo for more info.
 
-To rebuild the entire project in one go, _Scripts/rebuild-all.sh_ script is available, which works similarly to "Batch Build" feature in Visual Studio. The script will call cleaning script and build the engine in all configurations possible. Unlike Windows platform, due to conflict between 32-bit and 64-bit versions of some packages, the Engine builds only in version which conforms Distros version.
+To rebuild the entire project in one go, _Scripts/rebuild-all.sh_ script is available, which works similarly to "Batch Build" feature in Visual Studio. The script will call cleaning script and build the engine in all configurations possible. Unlike Windows platform, due to conflicts between 32-bit and 64-bit versions of some packages, the Engine builds only in version which conforms Distros version.
 
-Building without _Scripts/rebuild-all.sh_ script is done by explicitly calling CMake and Make. To build nfEngine with default settings (Release build, 64-bit platform), simply call on nfEngine repo root directory:
+If you need to manually build (without cleaning) single configuration, _Scripts/build.sh_ script can be used. It's only argument is a build configuration and can be **Debug** or **Release**. Calling without argument assumes Release build.
+
+Building without _Scripts/rebuild-all.sh_ script is done by explicitly calling CMake and Make. To build nfEngine with default settings (Release build), simply call on nfEngine repo root directory:
 
 ```
 cmake .
--- nfEngine/Renderers/nfRendererVk/Extensions.hpp
+++ nfEngine/Renderers/nfRendererVk/Extensions.hpp
@@ -7,7 +7,7 @@
 #pragma once
 
 #ifdef WIN32
-#include "Win/Win32Extensions.hpp"
+#include "Win/WinExtensions.hpp"
 #elif defined(__linux__) | defined(__LINUX__)
 #include "Linux/XcbExtensions.hpp"
 #else
@@ -18,23 +18,8 @@
 namespace NFE {
 namespace Renderer {
 
-// Instance extractor
-extern PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr;
-
 // Instance extensions
-extern PFN_vkEnumeratePhysicalDevices vkEnumeratePhysicalDevices;
-extern PFN_vkGetPhysicalDeviceProperties vkGetPhysicalDeviceProperties;
-extern PFN_vkGetPhysicalDeviceMemoryProperties vkGetPhysicalDeviceMemoryProperties;
-extern PFN_vkGetPhysicalDeviceFeatures vkGetPhysicalDeviceFeatures;
-extern PFN_vkGetPhysicalDeviceQueueFamilyProperties vkGetPhysicalDeviceQueueFamilyProperties;
-extern PFN_vkCreateDevice vkCreateDevice;
-extern PFN_vkDestroyDevice vkDestroyDevice;
-extern PFN_vkGetDeviceProcAddr vkGetDeviceProcAddr;
-extern PFN_vkDestroySurfaceKHR vkDestroySurfaceKHR;
-extern PFN_vkGetPhysicalDeviceSurfaceFormatsKHR vkGetPhysicalDeviceSurfaceFormatsKHR;
-extern PFN_vkGetPhysicalDeviceSurfaceCapabilitiesKHR vkGetPhysicalDeviceSurfaceCapabilitiesKHR;
-extern PFN_vkGetPhysicalDeviceSurfacePresentModesKHR vkGetPhysicalDeviceSurfacePresentModesKHR;
-extern PFN_vkGetPhysicalDeviceSurfaceSupportKHR vkGetPhysicalDeviceSurfaceSupportKHR;
+// If any useful Device-related extensions show up outside of VK Core, they should go in here
 
 /**
  * Initializes Vulkan Instance extensions used by nfRendererVk.
@@ -46,101 +31,7 @@ bool nfvkInstanceExtensionsInit(VkInstance instance);
 
 
 // Device extensions
-
-// Command Pool
-extern PFN_vkCreateCommandPool vkCreateCommandPool;
-extern PFN_vkDestroyCommandPool vkDestroyCommandPool;
-
-// Command Buffers
-extern PFN_vkAllocateCommandBuffers vkAllocateCommandBuffers;
-extern PFN_vkBeginCommandBuffer vkBeginCommandBuffer;
-extern PFN_vkEndCommandBuffer vkEndCommandBuffer;
-extern PFN_vkFreeCommandBuffers vkFreeCommandBuffers;
-extern PFN_vkResetCommandBuffer vkResetCommandBuffer;
-
-// Render Passes and Framebuffers
-extern PFN_vkCmdBeginRenderPass vkCmdBeginRenderPass;
-extern PFN_vkCmdEndRenderPass vkCmdEndRenderPass;
-extern PFN_vkCreateFramebuffer vkCreateFramebuffer;
-extern PFN_vkCreateRenderPass vkCreateRenderPass;
-extern PFN_vkDestroyFramebuffer vkDestroyFramebuffer;
-extern PFN_vkDestroyRenderPass vkDestroyRenderPass;
-
-// Other Commands
-extern PFN_vkCmdBindDescriptorSets vkCmdBindDescriptorSets;
-extern PFN_vkCmdBindIndexBuffer vkCmdBindIndexBuffer;
-extern PFN_vkCmdBindPipeline vkCmdBindPipeline;
-extern PFN_vkCmdBindVertexBuffers vkCmdBindVertexBuffers;
-extern PFN_vkCmdClearAttachments vkCmdClearAttachments;
-extern PFN_vkCmdCopyBuffer vkCmdCopyBuffer;
-extern PFN_vkCmdCopyBufferToImage vkCmdCopyBufferToImage;
-extern PFN_vkCmdDraw vkCmdDraw;
-extern PFN_vkCmdDrawIndexed vkCmdDrawIndexed;
-extern PFN_vkCmdSetScissor vkCmdSetScissor;
-extern PFN_vkCmdSetViewport vkCmdSetViewport;
-
-// Swapchain
-extern PFN_vkAcquireNextImageKHR vkAcquireNextImageKHR;
-extern PFN_vkCreateSwapchainKHR vkCreateSwapchainKHR;
-extern PFN_vkDestroySwapchainKHR vkDestroySwapchainKHR;
-extern PFN_vkGetSwapchainImagesKHR vkGetSwapchainImagesKHR;
-
-// Queue
-extern PFN_vkGetDeviceQueue vkGetDeviceQueue;
-extern PFN_vkQueuePresentKHR vkQueuePresentKHR;
-extern PFN_vkQueueSubmit vkQueueSubmit;
-extern PFN_vkQueueWaitIdle vkQueueWaitIdle;
-
-// Memory
-extern PFN_vkAllocateMemory vkAllocateMemory;
-extern PFN_vkBindBufferMemory vkBindBufferMemory;
-extern PFN_vkBindImageMemory vkBindImageMemory;
-extern PFN_vkCreateBuffer vkCreateBuffer;
-extern PFN_vkCreateImage vkCreateImage;
-extern PFN_vkCreateImageView vkCreateImageView;
-extern PFN_vkDestroyBuffer vkDestroyBuffer;
-extern PFN_vkDestroyImage vkDestroyImage;
-extern PFN_vkDestroyImageView vkDestroyImageView;
-extern PFN_vkFreeMemory vkFreeMemory;
-extern PFN_vkGetBufferMemoryRequirements vkGetBufferMemoryRequirements;
-extern PFN_vkGetImageMemoryRequirements vkGetImageMemoryRequirements;
-extern PFN_vkMapMemory vkMapMemory;
-extern PFN_vkUnmapMemory vkUnmapMemory;
-
-// Synchronization
-extern PFN_vkCmdPipelineBarrier vkCmdPipelineBarrier;
-extern PFN_vkCreateFence vkCreateFence;
-extern PFN_vkCreateSemaphore vkCreateSemaphore;
-extern PFN_vkDestroyFence vkDestroyFence;
-extern PFN_vkDestroySemaphore vkDestroySemaphore;
-extern PFN_vkGetFenceStatus vkGetFenceStatus;
-extern PFN_vkResetFences vkResetFences;
-extern PFN_vkWaitForFences vkWaitForFences;
-
-// Shaders
-extern PFN_vkCreateShaderModule vkCreateShaderModule;
-extern PFN_vkDestroyShaderModule vkDestroyShaderModule;
-
-// Pipeline
-extern PFN_vkCreateGraphicsPipelines vkCreateGraphicsPipelines;
-extern PFN_vkCreatePipelineCache vkCreatePipelineCache;
-extern PFN_vkCreatePipelineLayout vkCreatePipelineLayout;
-extern PFN_vkDestroyPipeline vkDestroyPipeline;
-extern PFN_vkDestroyPipelineCache vkDestroyPipelineCache;
-extern PFN_vkDestroyPipelineLayout vkDestroyPipelineLayout;
-
-// Samplers
-extern PFN_vkCreateSampler vkCreateSampler;
-extern PFN_vkDestroySampler vkDestroySampler;
-
-// Descriptors
-extern PFN_vkAllocateDescriptorSets vkAllocateDescriptorSets;
-extern PFN_vkCreateDescriptorPool vkCreateDescriptorPool;
-extern PFN_vkCreateDescriptorSetLayout vkCreateDescriptorSetLayout;
-extern PFN_vkDestroyDescriptorPool vkDestroyDescriptorPool;
-extern PFN_vkDestroyDescriptorSetLayout vkDestroyDescriptorSetLayout;
-extern PFN_vkResetDescriptorPool vkResetDescriptorPool;
-extern PFN_vkUpdateDescriptorSets vkUpdateDescriptorSets;
+// If any useful Device-related extensions show up outside of VK Core, they should go in here
 
 /**
  * Initializes Vulkan Device extensions used by nfRendererVk.
