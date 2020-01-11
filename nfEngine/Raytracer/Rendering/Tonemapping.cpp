c346ffacfc5f73b556c4fa281611a0458f18f1a4
Michal Witanowski <witek902@gmail.com>
2020-01-11 13:53:03 +0100

    Fitted ACES tonemapping

--- nfEngine/Raytracer/Rendering/Tonemapping.cpp
+++ nfEngine/Raytracer/Rendering/Tonemapping.cpp
@@ -38,6 +38,7 @@ NFE_END_DEFINE_CLASS()
 NFE_DEFINE_POLYMORPHIC_CLASS(NFE::RT::ApproxACESTonemapper)
 {
     NFE_CLASS_PARENT(NFE::RT::ITonemapper);
+    NFE_CLASS_MEMBER(fitted);
 }
 NFE_END_DEFINE_CLASS()
 
@@ -83,15 +84,50 @@ const Vector4 FilmicTonemapper::Apply(const Vector4 hdrColor) const
 
 const Vector4 ApproxACESTonemapper::Apply(const Vector4 hdrColor) const
 {
-    // based on https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
-    float a = 2.51f;
-    float b = 0.03f;
-    float c = 2.43f;
-    float d = 0.59f;
-    float e = 0.14f;
-    const Vector4 linearTonemapped = (hdrColor * (a * hdrColor + Vector4(b))) * Vector4::FastReciprocal(hdrColor * (c * hdrColor + Vector4(d)) + Vector4(e));
+    Vector4 color;
 
-    return Convert_Linear_To_sRGB(linearTonemapped);
+    if (fitted)
+    {
+        // based on https://github.com/TheRealMJP/BakingLab/blob/master/BakingLab/ACES.hlsl
+
+        // sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
+        const Vector4 ACESInputMat[] =
+        {
+            { 0.59719f, 0.07600f, 0.02840f, 0.0f },
+            { 0.35458f, 0.90834f, 0.13383f, 0.0f },
+            { 0.04823f, 0.01566f, 0.83777f, 0.0f },
+        };
+
+        // ODT_SAT => XYZ => D60_2_D65 => sRGB
+        const Vector4 ACESOutputMat[] =
+        {
+            {  1.60475f, -0.10208f, -0.00327f, 0.0f },
+            { -0.53108f,  1.10813f, -0.07276f, 0.0f },
+            { -0.07367f, -0.00605f,  1.07602f, 0.0f },
+        };
+
+        const Vector4 v = ACESInputMat[0] * hdrColor.x + ACESInputMat[1] * hdrColor.y + ACESInputMat[2] * hdrColor.z;
+
+        const Vector4 a = v * (v + Vector4(0.0245786f)) - Vector4(0.000090537f);
+        const Vector4 b = v * (0.983729f * v + Vector4(0.4329510f)) + Vector4(0.238081f);
+        color = a * Vector4::FastReciprocal(b);
+
+        color = Vector4::Saturate(ACESOutputMat[0] * color.x + ACESOutputMat[1] * color.y + ACESOutputMat[2] * color.z);
+    }
+    else
+    {
+        // based on https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+
+        color = 0.5f * hdrColor;
+
+        const Vector4 a = color * (2.51f * color + Vector4(0.03f));
+        const Vector4 b = color * (2.43f * color + Vector4(0.59f)) + Vector4(0.14f);
+
+        color = a / b;
+    }
+
+    return Convert_Linear_To_sRGB(color);
+
 }
 
 } // namespace RT
